4d9fbee54594e2b79d2c4f42fda9d7d8bf2ff7d7
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-12 08:34:48 +0000

    license plus asserts

--- trunk/java/picocontainer/src/test/picocontainer/DummiesTestCase.java
+++ trunk/java/picocontainer/src/test/picocontainer/DummiesTestCase.java
@@ -1,3 +1,13 @@
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the license.html file.                                                    *
+ *                                                                           *
+ * Original code by Aslak Hellesoy and Paul Hammant                          *
+ *****************************************************************************/
+
 package picocontainer;
 
 import junit.framework.TestCase;
@@ -6,10 +16,30 @@ public class DummiesTestCase extends TestCase
  {
 
     public void testDummyContainer() {
-        //DummyContainer dc = new DummyContainer();
-        //Object obj = new Object();
-        //Object obj2 = new Object();
-        //dc.
+        DummyContainer dc = new DummyContainer();
+        assertFalse(dc.hasComponent(String.class));
+        assertNull(dc.getComponent(String.class));
+        assertEquals(0,dc.getComponents().length);
     }
+
+    public void testDummyStartableLifecycleManager() throws PicoStartException, PicoStopException {
+        DummyStartableLifecycleManagerImpl ds = new DummyStartableLifecycleManagerImpl();
+        Object o = new Object();
+        ds.startComponent(o);
+        ds.stopComponent(o);
+        //TODO check no methods were called, via proxy ?
+    }
+
+    public void testDummyComponentDecorator() throws PicoStartException, PicoStopException {
+        DummyComponentDecorator dcd = new DummyComponentDecorator();
+        Object o = new Object();
+        Object decorated = dcd.decorateComponent(Object.class, o);
+        assertEquals(o, decorated);
+        //TODO check no methods were called, via proxy ?
+
+    }
+
+
+
 
 }

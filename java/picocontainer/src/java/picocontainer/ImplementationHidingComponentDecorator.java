fcbd9f073aafbb1b4c1f4d26d4b6d00ebc9a44c9
tirsen <tirsen@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-15 15:09:11 +0000

    refactored ComponentDecorator to ComponentFactory

--- java/picocontainer/src/java/picocontainer/ImplementationHidingComponentDecorator.java
+++ java/picocontainer/src/java/picocontainer/ImplementationHidingComponentDecorator.java
@@ -1,12 +1,13 @@
 package picocontainer;
 
-import java.lang.reflect.Proxy;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
-public class ImplementationHidingComponentDecorator implements ComponentDecorator {
-    public Object decorateComponent(Class compType, Object instance) {
-        return Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {compType}, new ImplementationHidingProxy(instance));
+public class ImplementationHidingComponentDecorator extends DefaultComponentFactory {
+    public Object createComponent(Class compType, Constructor constructor, Object[] args)
+            throws InvocationTargetException, IllegalAccessException, InstantiationException
+    {
+        Object component = super.createComponent(compType, constructor, args);
+        return Proxy.newProxyInstance(getClass().getClassLoader(), new Class[] {compType}, new ImplementationHidingProxy(component));
     }
 
     private class ImplementationHidingProxy implements InvocationHandler {

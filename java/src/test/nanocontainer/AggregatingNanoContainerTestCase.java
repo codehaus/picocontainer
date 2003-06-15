b87d8b7cbae81c1ccf17da689b962b19a78d6360
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-15 16:51:55 +0000

    o Set up documentation o Added license

--- java/src/test/nanocontainer/AggregatingNanoContainerTestCase.java
+++ java/src/test/nanocontainer/AggregatingNanoContainerTestCase.java
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------- *
  * The software in this package is published under the terms of the BSD      *
  * style license a copy of which has been included with this distribution in *
- * the license.html file.                                                    *
+ * the LICENSE.txt file.                                                     *
  *                                                                           *
  * Original code by Aslak Hellesoy and Paul Hammant                          *
  *****************************************************************************/
@@ -52,7 +52,6 @@ public class AggregatingNanoContainerTestCase extends TestCase {
         pico.start();
 
         AggregatingNanoContainer aggContainer = new AggregatingNanoContainer(pico, new SequentialInvocationHandler(pico));
-        
         Object proxy = aggContainer.getProxy();
 
         Collection proxyCollection = (Collection) proxy;

7a7ad05913111a33e0284e12dd63cee29b2466f9
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-16 17:18:03 +0000

    refactor of nanning component factory

---
+++ trunk/java/src/java/nanocontainer/nanning/NanningComponentFactory.java
@@ -0,0 +1,49 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Jon Tirsén                                               *
+ *****************************************************************************/
+
+package nanocontainer.nanning;
+
+import picocontainer.DefaultComponentFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import com.tirsen.nanning.AspectInstance;
+import com.tirsen.nanning.MixinInstance;
+import com.tirsen.nanning.config.AspectSystem;
+
+public class NanningComponentFactory extends DefaultComponentFactory {
+    private final AspectSystem aspectSystem;
+
+    public NanningComponentFactory(AspectSystem aspectSystem) {
+        this.aspectSystem = aspectSystem;
+    }
+
+    public Object createComponent(Class compType, Constructor constructor, Object[] args)
+            throws InvocationTargetException, IllegalAccessException, InstantiationException {
+        Object component = super.createComponent(compType, constructor, args);
+        Class[] interfaces = compType.getInterfaces();
+
+        // Nanning will only aspectify stuff that has one and only one interface
+        if (interfaces.length == 1) {
+            Class interfaze = interfaces[0];
+
+            // the trick: set up first mixin manually with the component as target
+            AspectInstance aspectInstance = new AspectInstance();
+            MixinInstance mixin = new MixinInstance(interfaze, component);
+            aspectInstance.addMixin(mixin);
+
+            // let the aspects do it's work
+            aspectSystem.initialize(aspectInstance);
+            component = aspectInstance.getProxy();
+        }
+        return component;
+    }
+}

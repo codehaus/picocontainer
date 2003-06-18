065101005afac9c4f3c9da433761f48a5d181d82
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-18 00:29:37 +0000

    improving coverage

---
+++ trunk/java/picocontainer/src/test/picocontainer/AggregatedContainersContainerTestCase.java
@@ -0,0 +1,49 @@
+package picocontainer;
+
+import junit.framework.TestCase;
+
+public class AggregatedContainersContainerTestCase extends TestCase {
+
+    public void testBasic() {
+
+        final String acomp = "hello";
+        final Integer bcomp = new Integer(123);
+
+        Container a = new Container() {
+            public boolean hasComponent(Class compType) {
+                return compType == String.class;
+            }
+
+            public Object getComponent(Class compType) {
+                return compType == String.class ? acomp : null;
+            }
+
+            public Object[] getComponents() {
+                return new Object[] {acomp};
+            }
+        };
+
+        Container b = new Container() {
+            public boolean hasComponent(Class compType) {
+                return compType == Integer.class;
+            }
+
+            public Object getComponent(Class compType) {
+                return compType == Integer.class ? bcomp : null;
+            }
+
+            public Object[] getComponents() {
+                return new Object[] {bcomp};
+            }
+        };
+
+        AggregatedContainersContainer acc = new AggregatedContainersContainer(new Container[] {a, b});
+
+        assertTrue(acc.hasComponent(String.class));
+        assertTrue(acc.hasComponent(Integer.class));
+        assertTrue(acc.getComponent(String.class) == acomp);
+        assertTrue(acc.getComponent(Integer.class) == bcomp);
+        assertTrue(acc.getComponents().length == 2);
+
+    }
+}

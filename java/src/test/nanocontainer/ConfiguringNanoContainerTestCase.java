e2dae6bb46a0b5d78eb252d39e9c2027510b8d3f
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-18 22:57:48 +0000

    first version of configurinbg nano dom container from Mike

---
+++ java/src/test/nanocontainer/ConfiguringNanoContainerTestCase.java
@@ -0,0 +1,115 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Mike Hogan                                               *
+ *****************************************************************************/
+
+package nanocontainer;
+
+import junit.framework.TestCase;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.StringReader;
+
+import picocontainer.PicoRegistrationException;
+import picocontainer.PicoStartException;
+
+public class ConfiguringNanoContainerTestCase extends TestCase {
+
+    public void testCanDealWithNoConfiguration()
+            throws ParserConfigurationException, PicoRegistrationException, PicoStartException, ClassNotFoundException {
+        final String xml =
+                "<components>" +
+                "      <component class=\"nanocontainer.MockComponentImpl\"/>" +
+                "</components>";
+        final InputSourceRegistrationNanoContainer container = configureContainer(xml);
+        final MockComponent component = (MockComponent) container.getComponents()[0];
+        assertNotNull(component);
+    }
+
+    public void testCanDealWithNonConfiguringXML()
+            throws ParserConfigurationException, PicoRegistrationException, PicoStartException, ClassNotFoundException {
+        final String xml =
+                "<components>" +
+                "      <component class=\"nanocontainer.MockComponentImpl\">" +
+                "            <nonconfiguring/>" +
+                "      </component>" +
+                "</components>";
+        final InputSourceRegistrationNanoContainer container = configureContainer(xml);
+        assertNotNull(container.getComponents()[0]);
+    }
+
+    public void testCanConfigureWithSingleString()
+            throws ParserConfigurationException, PicoRegistrationException, PicoStartException, ClassNotFoundException {
+        final String xml =
+                "<components>" +
+                "      <component class=\"nanocontainer.MockComponentImpl\">" +
+                "            <server>server_name</server>" +
+                "      </component>" +
+                "</components>";
+        final InputSourceRegistrationNanoContainer container = configureContainer(xml);
+        final MockComponent component = (MockComponent) container.getComponents()[0];
+        assertEquals("server_name", component.getServer());
+    }
+
+    public void testCanConfigureWithSingleStringAndTypeSpecification()
+            throws ParserConfigurationException, PicoRegistrationException, PicoStartException, ClassNotFoundException {
+        final String xml =
+                "<components>" +
+                "      <component type=\"nanocontainer.MockComponent\" class=\"nanocontainer.MockComponentImpl\">" +
+                "            <server>server_name</server>" +
+                "      </component>" +
+                "</components>";
+        final InputSourceRegistrationNanoContainer container = configureContainer(xml);
+        final MockComponent component = (MockComponent) container.getComponents()[0];
+        assertEquals("server_name", component.getServer());
+    }
+
+    public void testCanConfigureWithSingleInteger()
+            throws ParserConfigurationException, PicoRegistrationException, PicoStartException, ClassNotFoundException {
+        final String xml =
+                "<components>" +
+                "      <component class=\"nanocontainer.MockComponentImpl\">" +
+                "            <port>12345</port>" +
+                "      </component>" +
+                "</components>";
+        final InputSourceRegistrationNanoContainer container = configureContainer(xml);
+        final MockComponent component = (MockComponent) container.getComponents()[0];
+        assertEquals(12345, component.getPort());
+    }
+
+    public void testCanConfigureWithMultipleIntegers()
+            throws ParserConfigurationException, PicoRegistrationException, PicoStartException, ClassNotFoundException {
+        final String xml =
+                "<components>" +
+                "      <component class=\"nanocontainer.MockComponentImpl\">" +
+                "            <registers>" +
+                "                  <register>1</register>" +
+                "                  <register>2</register>" +
+                "                  <register>3</register>" +
+                "            </registers>" +
+                "      </component>" +
+                "</components>";
+        final InputSourceRegistrationNanoContainer container = configureContainer(xml);
+        final MockComponent component = (MockComponent) container.getComponents()[0];
+        assertEquals(3, component.getNumRegisters());
+        assertTrue(component.hasRegister(1));
+        assertTrue(component.hasRegister(2));
+        assertTrue(component.hasRegister(3));
+    }
+
+    private InputSourceRegistrationNanoContainer configureContainer(final String xml)
+            throws ParserConfigurationException, PicoRegistrationException, ClassNotFoundException, PicoStartException {
+        final InputSourceRegistrationNanoContainer container = new ConfiguringNanoContainerImpl.Default();
+        container.registerComponents(new InputSource(new StringReader(
+                xml)));
+        container.start();
+        return container;
+    }
+
+}

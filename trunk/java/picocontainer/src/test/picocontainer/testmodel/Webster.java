bbfea6ab542ec2209f7e3b6a1d8ab822412ece5e
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-21 19:46:09 +0000

    Dupe compInstances can exist now

--- trunk/java/picocontainer/src/test/picocontainer/testmodel/Webster.java
+++ trunk/java/picocontainer/src/test/picocontainer/testmodel/Webster.java
@@ -1,9 +1,14 @@
 package picocontainer.testmodel;
 
+import java.util.List;
+
 /**
  * 
  * @author Aslak Hellesoy
  * @version $Revision: 0 $
  */
 public class Webster implements Dictionary, Thesaurus {
+    public Webster(List list) {
+        list.add("webster created");
+    }
 }

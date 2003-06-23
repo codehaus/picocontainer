be1641f5f33e4e9f41141b563eccd76ed24b889f
joe <joe@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-23 18:01:23 +0000

    Ensured config files are closed afterwards

--- java/src/java/nanocontainer/servlet/containerfactory/XmlConfiguredNanoFactory.java
+++ java/src/java/nanocontainer/servlet/containerfactory/XmlConfiguredNanoFactory.java
@@ -8,6 +8,8 @@ import org.xml.sax.InputSource;
 import picocontainer.*;
 
 import javax.servlet.ServletContext;
+import java.io.InputStream;
+import java.io.IOException;
 
 public class XmlConfiguredNanoFactory implements ContainerFactory {
     private ServletContext servletContext;
@@ -19,8 +21,7 @@ public class XmlConfiguredNanoFactory implements ContainerFactory {
     public Container buildContainer(String configName) {
         try {
             InputSourceRegistrationNanoContainer container = new DomRegistrationNanoContainer.Default();
-            container.registerComponents(getConfigInputStream(configName));
-            container.start();
+            configureAndStart(configName, container);
             return container;
         } catch (Exception e) {
             // TODO: Better exception
@@ -31,13 +32,22 @@ public class XmlConfiguredNanoFactory implements ContainerFactory {
     public Container buildContainerWithParent(Container parentContainer, String configName) {
         try {
             InputSourceRegistrationNanoContainer container = new DomRegistrationNanoContainer.WithParentContainer(parentContainer);
-            container.registerComponents(getConfigInputStream(configName));
-            container.start();
+            configureAndStart(configName, container);
             return container;
         } catch (Exception e) {
             // TODO: Better exception
             throw new RuntimeException("Cannot build container for config: " + configName, e);
         }
+    }
+
+    private void configureAndStart(String configName, InputSourceRegistrationNanoContainer container) throws PicoRegistrationException, ClassNotFoundException, PicoStartException, IOException {
+        InputStream in = getConfigInputStream(configName);
+        try {
+            container.registerComponents(new InputSource(in));
+        } finally {
+            in.close();
+        }
+        container.start();
     }
 
     public ObjectInstantiater buildInstantiater(final Container parentContainer) {
@@ -65,9 +75,9 @@ public class XmlConfiguredNanoFactory implements ContainerFactory {
         // TODO
     }
 
-    private InputSource getConfigInputStream(String configName) {
+    private InputStream getConfigInputStream(String configName) {
         // TODO: find a way of caching this so the XML does not have to be continually reparsed
-        return new InputSource(servletContext.getResourceAsStream("/WEB-INF/components-" + configName + ".xml"));
+        return servletContext.getResourceAsStream("/WEB-INF/components-" + configName + ".xml");
     }
 
 }

d49863da897fba9c0858fdc41c60eff11ffba3cd
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-06-25 22:47:40 +0000

    licenses

--- trunk/java/src/java/nanocontainer/webwork1/NanoActionFactoryChain.java
+++ trunk/java/src/java/nanocontainer/webwork1/NanoActionFactoryChain.java
@@ -1,3 +1,13 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Joe Walnes                                               *
+ *****************************************************************************/
+
 package nanocontainer.webwork1;
 
 import webwork.action.factory.*;

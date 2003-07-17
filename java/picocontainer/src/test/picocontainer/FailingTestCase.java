33452702908e091be232206235504da2717176ed
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-07-17 01:08:28 +0000

    Test CI compile error

--- java/picocontainer/src/test/picocontainer/FailingTestCase.java
+++ java/picocontainer/src/test/picocontainer/FailingTestCase.java
@@ -2,7 +2,7 @@ package picocontainer;
 
 import junit.framework.TestCase;
 
-public class FailingTestCase extends TestCase {
+public class FailingTestCase extRends TestCase {
 
     public void testFailure() {
         fail("We should get mail about this");

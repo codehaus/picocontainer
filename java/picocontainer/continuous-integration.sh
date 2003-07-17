d950b355bebc24adfab6cb9383487881b00e627c
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-07-17 01:02:18 +0000

    Go live with continuous integration

--- java/picocontainer/continuous-integration.sh
+++ java/picocontainer/continuous-integration.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 # This is a *very* simple continuous integration script.
 # It gets the sources from CVS, builds and uploads the site and binaries.
 #
@@ -18,11 +18,19 @@
 # Finally, to get the full picture, see maven.xml
 #
 
-cd $HOME/cvs/pico
+JAVA_HOME=/usr/local/j2sdk1.4.1
+MAVEN_HOME=$HOME/maven
+ANT_HOME=$HOME/cvs/ant/dist
+PATH=$HOME/bin:$JAVA_HOME/bin:$MAVEN_HOME/bin:$ANT_HOME/bin:$PATH
+CVS_RSH=$HOME/ssh1.sh
 
-# Using my own script because I was stupid enough to upload an old format key
-export CVS_RSH = $HOME/ssh1.sh
-#export CVS_RSH = ssh
+export JAVA_HOME
+export MAVEN_HOME
+export ANT_HOME
+export PATH
+export CVS_RSH
+
+cd $HOME/cvs/pico
 
 cvs -d:ext:$USER@cvs.codehaus.org:/cvsroot/picocontainer update -d -P
 
@@ -30,6 +38,30 @@ cvs -d:ext:$USER@cvs.codehaus.org:/cvsroot/picocontainer update -d -P
 rm -Rf target
 mkdir target
 
-# Run clover. This will compile and test, and also work around a bug in
-# clover requiring it to run twice to get proper coverage. The 2nd time
-# happens during site:deploy
+# clover requiring it to run twice to get proper coverage.
+# The 2nd time happens during site:deploy
+maven clover > target/cleanbuild.log
+
+# See if the "compiling" file is there. If it is, compilation
+# failed.
+if [ -e "target/compiling" ] ; then
+  # Mail Maven's output to the dev list.
+  cat target/cleanbuild.log | mutt -s "[BUILD] Clean build failed" picocontainer-dev@lists.codehaus.org
+else
+  # See if the "testfailure" file is there. If it is, tests failed.
+  if [ -e "target/testfailure" ] ; then
+    # Mail Maven's output to the dev list.
+    cat target/cleanbuild.log | mutt -s "[BUILD] Test failure" picocontainer-dev@lists.codehaus.org
+
+    # We'll deploy the site even if the tests fail. Log currently not used.
+    maven site:deploy > target/sitedeploy.log
+  else
+    # Deploy site only if compile and tests pass. Logs currently not used.
+    # Must be run separately to get the files uploaded in the proper dir
+    # on the server
+    maven jar:deploy > target/jardeploy.log
+    maven dist:deploy > target/distdeploy.log
+  fi
+  # We'll deploy the site even if the tests fail. Log currently not used.
+  maven site:deploy > target/sitedeploy.log
+fi

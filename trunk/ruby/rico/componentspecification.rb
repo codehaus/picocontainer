be84dc667447c42365a62657d5e94ea650f4dab5
dnorth <dnorth@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-07-30 19:36:57 +0000

    Removed start/stop behaviour. Introduced multicast.

--- trunk/ruby/rico/componentspecification.rb
+++ trunk/ruby/rico/componentspecification.rb
@@ -1,6 +1,6 @@
     
 class ComponentSpecification
-  attr_reader :component, :type, :dependencies
+  attr_reader :type, :dependencies
   
   def initialize(type, dependencies, create_method)
     @type, @dependencies, @create_method = type, dependencies, create_method
@@ -8,22 +8,13 @@ class ComponentSpecification
   
   def create_component(container)
       check_access @type, @create_method # calling by reflection doesn't check access
-      @dependencies.each { |dep| container.start_component(dep) }
       args = @dependencies.collect { |dep| container.component(dep) }
       @component = @type.send @create_method, *args
   end
   
-  def set_startable(start_method, stop_method)
-  	@start_method, @stop_method = start_method, stop_method
-  end	
-  
-  def start_component(container)
+  def component(container)
   	create_component container unless @component
-    @component.send @start_method if @start_method
-  end
-  
-  def stop_component
-  	@component.send @stop_method if @stop_method
+  	return @component
   end
   
   private

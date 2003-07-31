0f9d972118c303209735800565ee412068269baa
dnorth <dnorth@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-07-31 10:04:46 +0000

    *** empty log message ***

--- ruby/rico/componentspecification.rb
+++ ruby/rico/componentspecification.rb
@@ -9,7 +9,6 @@ class ComponentSpecification
       check_access @type, @create_method # calling by reflection doesn't check access
       args = @dependencies.collect { |dep| container.component(dep) }
       @component = @type.send(@create_method, *args)
-      return @component
   end
   
   def component(container)

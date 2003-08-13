ad20c5e20fbecf7759f1e837e94d36a14dba98fc
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-08-13 03:16:15 +0000

    more tck work

--- java/picocontainer/src/test/org/picocontainer/defaults/DefaultPicoContainerWithComponentFactoryStringKeyTestCase.java
+++ java/picocontainer/src/test/org/picocontainer/defaults/DefaultPicoContainerWithComponentFactoryStringKeyTestCase.java
@@ -4,26 +4,25 @@ import org.picocontainer.PicoContainer;
 import org.picocontainer.PicoIntrospectionException;
 import org.picocontainer.PicoRegistrationException;
 import org.picocontainer.ComponentFactory;
-import org.picocontainer.ComponentRegistry;
 import org.picocontainer.tck.AbstractBasicStringCompatabilityTestCase;
 import org.picocontainer.tck.DependsOnTouchable;
 import org.picocontainer.tck.SimpleTouchable;
 
-public class DefaultPicoContainerStringWithComponentRegistryTestCase extends AbstractBasicStringCompatabilityTestCase {
+public class DefaultPicoContainerWithComponentFactoryStringKeyTestCase extends AbstractBasicStringCompatabilityTestCase {
 
     public PicoContainer createPicoContainerWithTouchableAndDependancy() throws DuplicateComponentKeyRegistrationException,
         AssignabilityRegistrationException, NotConcreteRegistrationException, PicoIntrospectionException {
 
-        ComponentRegistry componentRegistry = new DefaultComponentRegistry();
-        DefaultPicoContainer defaultPico = new DefaultPicoContainer.WithComponentRegistry(componentRegistry);
+        ComponentFactory componentFactory = new DefaultComponentFactory();
+        DefaultPicoContainer defaultPico = new DefaultPicoContainer.WithComponentFactory(componentFactory);
         defaultPico.registerComponent("touchable", SimpleTouchable.class);
         defaultPico.registerComponent("dependsOnTouchable", DependsOnTouchable.class);
         return defaultPico;
     }
 
     public PicoContainer createPicoContainerWithTouchablesDependancyOnly() throws PicoRegistrationException, PicoIntrospectionException {
-        ComponentRegistry componentRegistry = new DefaultComponentRegistry();
-        DefaultPicoContainer defaultPico = new DefaultPicoContainer.WithComponentRegistry(componentRegistry);
+        ComponentFactory componentFactory = new DefaultComponentFactory();
+        DefaultPicoContainer defaultPico = new DefaultPicoContainer.WithComponentFactory(componentFactory);
         defaultPico.registerComponent("dependsOnTouchable", DependsOnTouchable.class);
         return defaultPico;
     }

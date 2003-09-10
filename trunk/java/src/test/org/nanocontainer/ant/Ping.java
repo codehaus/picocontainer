3d200f22a97afb19c6bc03e305a794bb1f94bdcc
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-09-10 13:45:16 +0000

    Improved the Ant task

--- trunk/java/src/test/org/nanocontainer/ant/Ping.java
+++ trunk/java/src/test/org/nanocontainer/ant/Ping.java
@@ -18,5 +18,7 @@ public class Ping {
     public void execute() {
         Assert.assertEquals("The property should be set", "HELLO", prop);
         wasExecuted = true;
+        // This is only so we can see the output when run from Ant
+        System.out.println("Ping executing :-)");
     }
 }

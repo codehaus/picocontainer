a110d35175162ed9b56096193f4f7db5197f4a25
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-09-10 14:21:57 +0000

    Splitting up various nano containers in smaller sub projects. These can be built by Maven reactor. Scripts not done yet.

--- trunk/java/src/test/org/nanocontainer/ant/Pang.java
+++
@@ -1,9 +0,0 @@
-package org.nanocontainer.ant;
-
-/**
- * 
- * @author Aslak Helles&oslash;y
- * @version $Revision$
- */
-public class Pang {
-}
--- trunk/java/src/test/org/nanocontainer/ant/Pong.java
+++
@@ -1,9 +0,0 @@
-package org.nanocontainer.ant;
-
-/**
- * @author Aslak Helles&oslash;y
- * @version $Revision$
- */
-public class Pong {
-    public Pong(Ping foo) {}
-}

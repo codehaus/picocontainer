5ca489db17083fdc7ac1914afc98b2d918582d2b
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-10-17 09:30:12 +0000

    A testcase for Mike

---
+++ java/picocontainer/src/test/org/picocontainer/testmodel/WashableTouchable.java
@@ -0,0 +1,15 @@
+package org.picocontainer.testmodel;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public class WashableTouchable implements Washable, Touchable {
+    public void wasTouched() {
+
+    }
+
+    public void wash() {
+
+    }
+}

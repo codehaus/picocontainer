6835ae2a828d7c2abfc863a9cac1305f8f5d11b7
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-10-22 19:48:45 +0000

    completion of xmlstream

---
+++ trunk/java/xstream/src/test/org/nanocontainer/xstream/TestPseudoComp.java
@@ -0,0 +1,13 @@
+package org.nanocontainer.xstream;
+
+public class TestPseudoComp {
+    private int foo;
+    private String bar;
+
+    public int getFoo() {
+        return foo;
+    }
+    public String getBar() {
+        return bar;
+    }
+}
---
+++ trunk/java/xstream/src/test/org/nanocontainer/xstream/XStreamXmlPseudoComponentFactoryTestCase.java
@@ -0,0 +1,46 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.nanocontainer.xstream;
+
+import junit.framework.TestCase;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * @author Paul Hammant
+ * @author Marcos Tarruella
+ */
+public class XStreamXmlPseudoComponentFactoryTestCase extends TestCase {
+
+    public void testDeserialization() throws ParserConfigurationException, IOException, SAXException, ClassNotFoundException {
+        XStreamXmlPseudoComponentFactory xsf = new XStreamXmlPseudoComponentFactory();
+
+                StringReader sr = new StringReader("" +
+                        "<org.nanocontainer.xstream.TestPseudoComp>" +
+                          "<foo>10</foo>" +
+                          "<bar>hello</bar>" +
+                        "</org.nanocontainer.xstream.TestPseudoComp>"
+                    );
+        InputSource is = new InputSource(sr);
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = db.parse(is);
+
+        Object o = xsf.makeInstance(doc.getDocumentElement());
+        TestPseudoComp tsc = (TestPseudoComp) o;
+        assertEquals("hello",tsc.getBar());
+        assertEquals(10,tsc.getFoo());
+    }
+}

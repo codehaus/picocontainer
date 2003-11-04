b5c2186ec6a1b2ca93faeb75df8f64c6070328b3
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-11-04 22:52:08 +0000

    Start using spice-cli for command line arg parsing.

--- trunk/java/nanocontainer/src/java/org/nanocontainer/Main.java
+++ trunk/java/nanocontainer/src/java/org/nanocontainer/Main.java
@@ -10,12 +10,43 @@
 package org.nanocontainer;
 
 import org.picocontainer.PicoConfigurationException;
+import org.realityforge.cli.CLArgsParser;
+import org.realityforge.cli.CLOptionDescriptor;
+import org.realityforge.cli.CLOption;
+import org.realityforge.cli.CLUtil;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
+
+    private static final int HELP_OPT = 'h';
+    private static final int VERSION_OPT = 'v';
+    private static final int MONITOR_OPT = 'm';
+    private static final int ASSEMBLY_OPT = 'a';
+
+    private static final CLOptionDescriptor[] OPTIONS = new CLOptionDescriptor[]
+    {
+        new CLOptionDescriptor( "help",
+                                CLOptionDescriptor.ARGUMENT_DISALLOWED,
+                                HELP_OPT,
+                                "print this message and exit" ),
+        new CLOptionDescriptor( "version",
+                                CLOptionDescriptor.ARGUMENT_DISALLOWED,
+                                VERSION_OPT,
+                                "print the version information and exit" ),
+        new CLOptionDescriptor( "monitor",
+                                CLOptionDescriptor.ARGUMENT_REQUIRED,
+                                MONITOR_OPT,
+                                "specify the monitor implemenatation" ),
+        new CLOptionDescriptor( "assembly",
+                                CLOptionDescriptor.ARGUMENT_REQUIRED,
+                                ASSEMBLY_OPT,
+                                "specify the assembly file" )
+
+    };
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, PicoConfigurationException, ParserConfigurationException {
         if (args.length == 0) {
@@ -23,19 +54,66 @@ public class Main {
             System.exit(10);
         }
 
+        CLArgsParser parser = new CLArgsParser(args, OPTIONS);
+
+        //Make sure that there was no errors parsing
+        //arguments
+        if( null != parser.getErrorString() )
+        {
+            System.err.println( "Error: " + parser.getErrorString() );
+            return;
+        }
+
+        // Get a list of parsed options
+        final List options = parser.getArguments();
+        final int size = options.size();
+
+        String monitor = "";
+        String nanoContainerConfig = "";
+
+        for( int i = 0; i < size; i++ )
+        {
+            final CLOption option = (CLOption)options.get( i );
+
+            switch( option.getId() )
+            {
+                case CLOption.TEXT_ARGUMENT:
+                    //This occurs when a user supplies an argument that
+                    //is not an option
+                    System.out.println( "Unknown arg: " + option.getArgument() );
+                    break;
+
+                case HELP_OPT:
+                    printUsage();
+                    break;
+
+                case VERSION_OPT:
+                    printVersion();
+                    break;
+
+                case MONITOR_OPT:
+                    monitor = option.getArgument();
+                    break;
+
+                case ASSEMBLY_OPT:
+                    nanoContainerConfig = option.getArgument();
+                    break;
+
+            }
+        }
+
         // Monitor
         NanoContainerMonitor nanoContainerMonitor = new NullNanoContainerMonitor();
         if (args.length == 2) {
-            if (args[1].equals("CommonsLogging")) {
+            if (monitor.equals("CommonsLogging")) {
                 nanoContainerMonitor = new CommonsLoggingNanoContainerMonitor();
-            } else if (args[1].equals("Log4J")) {
+            } else if (monitor.equals("Log4J")) {
                 nanoContainerMonitor = new Log4JNanoContainerMonitor();
-            } else if (args[1].equals("Console")) {
+            } else if (monitor.equals("Console")) {
                 nanoContainerMonitor = new ConsoleNanoContainerMonitor();
             }
         }
 
-        String nanoContainerConfig = args[0];
         if (nanoContainerConfig.toLowerCase().endsWith(".js")) {
             NanoContainer nano = new JavaScriptAssemblyNanoContainer(new FileReader(nanoContainerConfig), nanoContainerMonitor);
             JavaScriptAssemblyNanoContainer.addShutdownHook(nano);
@@ -47,6 +125,32 @@ public class Main {
             System.exit(10);
         }
     }
-}    
+
+    private static void printVersion()
+    {
+        System.out.println( "1.0" );
+        System.exit( 0 );
+    }
+
+    private static void printUsage()
+    {
+        final String lineSeparator = System.getProperty( "line.separator" );
+
+        final StringBuffer msg = new StringBuffer();
+
+        msg.append( lineSeparator );
+        msg.append( "Foo!" );
+
+        /*
+         * Notice that the next line uses CLUtil.describeOptions to generate the
+         * list of descriptions for each option
+         */
+        msg.append( CLUtil.describeOptions( OPTIONS ).toString() );
+
+        System.out.println( msg.toString() );
+
+        System.exit( 0 );
+    }
+}
     
 

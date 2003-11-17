8f2a2cea70d865f04bcd84075f93618b62d0825f
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-11-17 18:10:17 +0000

    Fixed PICO-48 - Hot Swapping. Check out the docs!

--- trunk/java/picocontainer/src/test/org/picocontainer/extras/ImplementationHidingComponentAdapterFactoryTestCase.java
+++ trunk/java/picocontainer/src/test/org/picocontainer/extras/ImplementationHidingComponentAdapterFactoryTestCase.java
@@ -10,53 +10,112 @@
 
 package org.picocontainer.extras;
 
-import org.picocontainer.PicoInitializationException;
-import org.picocontainer.PicoIntrospectionException;
-import org.picocontainer.ComponentAdapter;
 import org.picocontainer.tck.AbstractComponentAdapterFactoryTestCase;
 import org.picocontainer.defaults.*;
 
 public class ImplementationHidingComponentAdapterFactoryTestCase extends AbstractComponentAdapterFactoryTestCase {
 
-    private static boolean addCalled = false;
+    public static interface Man {
+        Woman getWoman();
 
-    public static class ReferencesSwappables {
-        public final Swappable swappable;
+        void kiss();
+        boolean wasKissed();
+    }
 
-        public ReferencesSwappables(Swappable swappable) {
-            this.swappable = swappable;
+    public static interface Woman {
+        Man getMan();
+    }
+
+    public static class Husband implements Man {
+        public final Woman partner;
+        private boolean wasKissed;
+
+        public Husband(Woman partner) {
+            this.partner = partner;
+        }
+
+        public Woman getWoman() {
+            return partner;
+        }
+
+        public void kiss() {
+            wasKissed = true;
+        }
+
+        public boolean wasKissed() {
+            return wasKissed;
         }
     }
 
-    public static interface Swappable {
-        String getCheese();
-    }
+    public static class Wife implements Woman {
+        public final Man partner;
 
-    public static class ConcreteSwappable implements Swappable {
-        public String getCheese() {
-            return "Edam";
+        public Wife(Man partner) {
+            this.partner = partner;
+        }
+
+        public Man getMan() {
+            return partner;
         }
     }
 
-    public void testCreatedComponentAdapterCreatesInstancesWhereImplementationIsHidden()
-            throws NoSuchMethodException, PicoInitializationException, PicoIntrospectionException, AssignabilityRegistrationException, NotConcreteRegistrationException {
-        ComponentAdapter componentAdapter = createComponentAdapterFactory().createComponentAdapter(Swappable.class, ConcreteSwappable.class, null);
-        Swappable swappable = (Swappable) componentAdapter.getComponentInstance(picoContainer);
-        assertFalse(swappable instanceof ConcreteSwappable);
-        assertEquals("Edam", swappable.getCheese());
+    public void testLowLevelCheating() {
+        ComponentAdapterFactory caf = createComponentAdapterFactory();
+        DefaultPicoContainer pico = new DefaultPicoContainer(caf);
+
+        ImplementationHidingComponentAdapter wifeAdapter = (ImplementationHidingComponentAdapter) caf.createComponentAdapter("wife", Wife.class, null);
+        ImplementationHidingComponentAdapter husbandAdapter = (ImplementationHidingComponentAdapter) caf.createComponentAdapter("husband", Husband.class, null);
+
+        pico.registerComponent(wifeAdapter);
+        pico.registerComponent(husbandAdapter);
+
+        Woman wife = (Woman) wifeAdapter.getComponentInstance(pico);
+        wife.getMan().kiss();
+        Man man = (Man) husbandAdapter.getComponentInstance(pico);
+        assertTrue(man.wasKissed());
+
+        assertSame(man, wife.getMan());
+        assertSame(wife, man.getWoman());
+
+        // Let the wife use another (single) man
+        Man newMan = new Husband(null);
+        Man oldMan = (Man) husbandAdapter.hotSwap(newMan);
+
+        wife.getMan().kiss();
+        assertTrue(newMan.wasKissed());
+
+        assertNotSame(man, oldMan);
+        assertNotSame(oldMan, newMan);
+        assertNotSame(newMan, man);
+
+        assertFalse(man.hashCode() == oldMan.hashCode());
+        assertFalse(oldMan.hashCode() == newMan.hashCode());
+        assertFalse(newMan.hashCode() == man.hashCode());
     }
 
-    public void testHotSwap() {
-        ImplementationHidingComponentAdapterFactory componentAdapterFactory = (ImplementationHidingComponentAdapterFactory) createComponentAdapterFactory();
-        ComponentAdapter componentAdapter = componentAdapterFactory.createComponentAdapter(Swappable.class, ConcreteSwappable.class, null);
-        Swappable swappable = (Swappable) componentAdapter.getComponentInstance(picoContainer);
-        assertFalse(swappable instanceof ConcreteSwappable);
-        assertEquals("Edam", swappable.getCheese());
+    public void testHighLevelCheating() {
+        DefaultPicoContainer pico = new DefaultPicoContainer(createComponentAdapterFactory());
 
-        //TODO - what is this last line for, throws something on errror or should have an assert following ?
-        componentAdapterFactory.hotSwap(Swappable.class);
+        // Register two classes with mutual dependencies in the constructor (!!!)
+        pico.registerComponentImplementation(Wife.class);
+        pico.registerComponentImplementation(Husband.class);
+
+        Woman wife = (Woman) pico.getComponentInstance(Wife.class);
+        Man man = (Man) pico.getComponentInstance(Husband.class);
+
+        assertSame(man, wife.getMan());
+        assertSame(wife, man.getWoman());
+
+        // Let the wife use another (single) man
+        Man newMan = new Husband(null);
+        ImplementationHidingComponentAdapter husbandAdapter = (ImplementationHidingComponentAdapter) pico.findComponentAdapter(Husband.class);
+        Man oldMan = (Man) husbandAdapter.hotSwap(newMan);
+
+        wife.getMan().kiss();
+        assertFalse(oldMan.wasKissed());
+        assertTrue(newMan.wasKissed());
+
     }
-
 
     protected ComponentAdapterFactory createComponentAdapterFactory() {
         return new ImplementationHidingComponentAdapterFactory(new DefaultComponentAdapterFactory());

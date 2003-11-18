d158831ebe11f08deeb426c4341cd2831edbbec7
ahelleso <ahelleso@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-11-18 20:40:38 +0000

    Test skeleton

---
+++ sample/java/test/org/picocontainer/sample/stock/StockTraderTest.java
@@ -0,0 +1,14 @@
+package org.picocontainer.sample.stock;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @author Jon Tirs&eacute;n
+ * @version $Revision$
+ */
+public class StockTraderTest extends TestCase {
+    public void testSellMicrosoftWhenAboveHundred() {
+        StockTrader trader = new StockTrader(ticker, market);
+    }
+}

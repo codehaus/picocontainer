f9c1470ea580432cf697a80ca29dd159b2e6dd77
ahelleso <ahelleso@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-11-18 20:42:59 +0000

    Test skeleton compiles

--- sample/java/test/org/picocontainer/sample/stock/StockTraderTest.java
+++ sample/java/test/org/picocontainer/sample/stock/StockTraderTest.java
@@ -8,7 +8,27 @@ import junit.framework.TestCase;
  * @version $Revision$
  */
 public class StockTraderTest extends TestCase {
+
+    public static interface StockTicker {
+    }
+
+    public static class MockStockTicker implements StockTicker {
+    }
+
+    public static interface StockMarket {
+    }
+
+    public static class MockStockMarket implements StockMarket {
+    }
+
+    public static class StockTrader {
+        public StockTrader(StockTicker ticker,  StockMarket market) {
+        }
+    }
+
     public void testSellMicrosoftWhenAboveHundred() {
+        StockTicker ticker = new MockStockTicker();
+        StockMarket market = new MockStockMarket();
         StockTrader trader = new StockTrader(ticker, market);
     }
 }

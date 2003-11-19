4710728cfe2a54913373812bff47ce5bbc59cd62
tastapod <tastapod@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-11-19 22:40:48 +0000

    Made some tests more explicit - thanks to Marcus

--- trunk/ruby/rico/test/containertest.rb
+++ trunk/ruby/rico/test/containertest.rb
@@ -6,38 +6,51 @@ require 'rico/exceptions'
 class ContainerTest < Test::Unit::TestCase
 
   include Rico # so we don't have to keep typing Rico::
+  
+  # Registering components in the container
 
   def test_new_container_is_empty
     rico = Container.new
     assert_equal true, rico.is_empty?
   end
   
-  def test_get_nonexistent_component_details_fails
-    rico = Container.new
-    assert_raises NonexistentComponentError, "component class for nonexistent key" do
-      rico.component_class :key
-    end
-    assert_raises NonexistentComponentError, "dependencies for nonexistent key" do
-      rico.dependencies :key
-    end
-  end
-  
   def test_registers_component_with_no_dependencies
     rico = Container.new
     rico.register_component :object, Object
-    assert_equal false, rico.is_empty?, "is empty"
     assert_equal 1, rico.component_count, "component count"
     assert_equal Object, rico.component_class(:object), "component class"
     assert_equal [], rico.dependencies(:object), "dependencies"
   end
   
+  def test_container_with_component_is_not_empty
+    rico = Container.new
+    assert_equal true, rico.is_empty?, "before"
+    rico.register_component :object, Object
+    assert_equal false, rico.is_empty?, "after"
+  end
+  
+  def test_get_class_of_nonexistent_component_raises_error
+    rico = Container.new
+    assert_raises NonexistentComponentError, "component class for nonexistent key" do
+      rico.component_class :nonexistent_key
+    end
+  end
+  
+  def test_get_dependencies_for_nonexistent_component_raises_error
+    rico = Container.new
+    assert_raises NonexistentComponentError, "dependencies for nonexistent key" do
+      rico.dependencies :nonexistent_key
+    end
+  end
+  
   class HasDependents
     attr_reader :needed, :also_needed
     
-    def initialize needed, also_needed
+    def initialize(needed, also_needed)
       @needed, @also_needed = needed, also_needed
     end
   end
+  
   class Needed; end
   class AlsoNeeded; end
   
@@ -48,6 +61,7 @@ class ContainerTest < Test::Unit::TestCase
     rico.register_component :has_dependents, HasDependents, [ :needed, :also_needed ]
     return rico
   end
+  private :build_rico_with_dependent_components
 
   def test_registers_component_with_dependencies
     rico = build_rico_with_dependent_components
@@ -56,12 +70,14 @@ class ContainerTest < Test::Unit::TestCase
     assert_equal [ :needed, :also_needed ], rico.dependencies(:has_dependents), "dependencies"
   end
   
-  def test_registering_component_with_missing_dependencies_fails
+  def test_registering_component_with_missing_dependencies_raises_error
     rico = Container.new
     assert_raises NonexistentComponentError do
-       rico.register_component :key, Object, [ :nonexistent ]
+       rico.register_component :object, Object, [ :nonexistent_key ]
     end
   end
+  
+  # Getting things out of the container
   
   def test_gets_component_with_no_dependencies
     rico = Container.new
@@ -70,7 +86,7 @@ class ContainerTest < Test::Unit::TestCase
     assert_instance_of Object, result
   end
   
-  def test_getting_component_twice_gives_same_component
+  def test_getting_same_component_twice_gives_same_component
     rico = Container.new
     rico.register_component :object, Object
     one = rico.component :object
@@ -81,27 +97,29 @@ class ContainerTest < Test::Unit::TestCase
   def test_gets_component_with_dependencies
     rico = build_rico_with_dependent_components
     component = rico.component(:has_dependents)
+    assert_instance_of HasDependents, component
     assert_instance_of Needed, component.needed
     assert_instance_of AlsoNeeded, component.also_needed
   end
   
-  class Higher
+  class HasNestedDependents
     attr_reader :dependent
-    def initialize dependent
+    def initialize(dependent)
       @dependent = dependent
     end
   end
     
   def test_gets_component_with_multiple_levels_of_dependencies
     rico = build_rico_with_dependent_components
-    rico.register_component :higher, Higher, [ :has_dependents ]
+    rico.register_component :has_nested_dependents, HasNestedDependents, [ :has_dependents ]
     
     # check top level has dependent
-    higher = rico.component :higher
-    assert_instance_of HasDependents, higher.dependent
+    component = rico.component(:has_nested_dependents)
+    assert_instance_of HasNestedDependents, component
+    assert_instance_of HasDependents, component.dependent
     
     # check dependent has its own dependents
-    dependent = higher.dependent
+    dependent = component.dependent
     assert_instance_of Needed, dependent.needed
     assert_instance_of AlsoNeeded, dependent.also_needed
   end
@@ -110,12 +128,16 @@ class ContainerTest < Test::Unit::TestCase
   
   class Thing1
   	attr_reader :common
-    def initialize common; @common = common; end
+    def initialize(common)
+      @common = common
+    end
   end
   
   class Thing2
   	attr_reader :common
-    def initialize common; @common = common; end
+    def initialize(common)
+      @common = common
+    end
   end
 
   def test_two_components_get_the_same_instance_for_the_same_tag
@@ -141,26 +163,41 @@ class ContainerTest < Test::Unit::TestCase
   
   class Washable
     attr_accessor :washed # creates washed and washed= methods
-    def initialize; @washed = false; end
+    def initialize
+      @washed = false
+    end
   end
   
   class AlsoWashable
     attr_accessor :washed
-    def initialize; @washed = false; end
+    def initialize
+      @washed = false
+    end
   end
   
-  class DontWashMe; end
-  
-  def test_multicast_delivers_message_to_all_components
+  def test_multicast_delivers_message_to_multiple_components
     rico = Container.new
     rico.register_component :washable, Washable
     rico.register_component :also_washable, AlsoWashable
-    rico.register_component :dont_wash_me, DontWashMe
+    
     assert_equal false, rico.component(:washable).washed
     assert_equal false, rico.component(:also_washable).washed
-    rico.multicaster.washed = true
+    
+    multicaster = rico.multicaster
+    multicaster.washed = true # call the washed= method on anything that understands it
+    
     assert_equal true, rico.component(:washable).washed
     assert_equal true, rico.component(:also_washable).washed
+  end
+      
+  class DontWashMe
+  end
+  
+  def test_multicast_ignores_irrelevant_component
+    rico = Container.new
+    rico.register_component :dont_wash_me, DontWashMe
+    multicaster = rico.multicaster
+    multicaster.washed = true # call the washed= method on anything that understands it
   end
   
   def test_registers_constant_values

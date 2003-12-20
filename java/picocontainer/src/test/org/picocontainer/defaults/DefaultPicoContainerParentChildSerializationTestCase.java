318f6e81c8d405ea8ac4a0987d2c3b50d825dc28
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2003-12-20 15:05:38 +0000

    failing test from Thomas Heller

---
+++ java/picocontainer/src/test/org/picocontainer/defaults/DefaultPicoContainerParentChildSerializationTestCase.java
@@ -0,0 +1,67 @@
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Idea by Rachel Davies, Original code by Aslak Hellesoy and Paul Hammant   *
+ *****************************************************************************/
+
+package org.picocontainer.defaults;
+
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoException;
+import org.picocontainer.PicoInitializationException;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.tck.AbstractPicoContainerTestCase;
+import org.picocontainer.testmodel.DependsOnTouchable;
+import org.picocontainer.testmodel.SimpleTouchable;
+import org.picocontainer.testmodel.Touchable;
+
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ByteArrayInputStream;
+import java.util.Collection;
+
+/**
+ * @author Thomas Heller
+ * @author Paul Hammant
+ */
+public class DefaultPicoContainerParentChildSerializationTestCase  extends AbstractPicoContainerTestCase
+{
+   protected MutablePicoContainer createPicoContainer()
+   {
+      DefaultPicoContainer container = new DefaultPicoContainer();
+      DefaultPicoContainer child = new DefaultPicoContainer();
+
+      child.registerComponentImplementation(Touchable.class, SimpleTouchable.class);
+      child.registerComponentImplementation(DependsOnTouchable.class);
+      container.addChild(child);
+      return container;
+   }
+
+    public void testContainerIsDeserializableWithChildren() throws PicoException, PicoInitializationException,
+            IOException, ClassNotFoundException {
+
+        PicoContainer pico = createPicoContainerWithTouchableAndDependency();
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+
+        oos.writeObject(pico);
+
+        // yeah yeah, is not needed.
+        pico = null;
+
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
+
+        pico = (PicoContainer) ois.readObject();
+
+        Collection children = pico.getChildContainers();
+        assertNotNull(children);
+        assertEquals(1, children.size());
+    }
+}

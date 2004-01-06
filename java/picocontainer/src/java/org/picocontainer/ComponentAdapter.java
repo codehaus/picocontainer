f110fcaf139a5115dd431adf745abda384e80db7
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-01-06 20:35:41 +0000

    javadoc corrections

--- java/picocontainer/src/java/org/picocontainer/ComponentAdapter.java
+++ java/picocontainer/src/java/org/picocontainer/ComponentAdapter.java
@@ -1,48 +1,40 @@
 package org.picocontainer;
 
-import org.picocontainer.defaults.NoSatisfiableConstructorsException;
-
 /**
- * A component adapter is responsible for instantiating and caching
- * a specific component instance. It is used internally by PicoContainer,
- * and is not meant to be used directly by clients of the PicoContainer API.
+ * A component adapter is responsible for providing
+ * a specific component instance.
  *
  * @author Aslak Helles&oslash;y
  * @author Jon Tirs&eacute;n
  */
 public interface ComponentAdapter {
     /**
-     * The component's key.
-     * @return
+     * @return the component's key.
      */
     Object getComponentKey();
 
     /**
-     * The component's implementation class.
-     * @return
+     * @return the component's implementation class.
      */
     Class getComponentImplementation();
 
     /**
-     * Gets the component instance. Subsequent calls to this method
-     * with the same arguments should return the same object (it should
-     * be caching).
-     * <p>
-     * This method should also register the instantiated component with the
-     * pico container.
+     * Gets the component instance. This method will usually create
+     * a new instance for each call (an exception is {@link org.picocontainer.defaults.CachingComponentAdapter}).
      *
      * @param dependencyContainer container where the adapter can look for
-     *  dependent component instances
-     * @return the component instance
+     *  dependent component instances.
+     * @return the component instance.
      * @throws PicoInitializationException if the component couldn't be instantiated
      */
     Object getComponentInstance(MutablePicoContainer dependencyContainer) throws PicoInitializationException, PicoIntrospectionException;
 
     /**
-     *  Verify that all dependencies for this adapter can be satisifed given picoConatiner
-     * @param picoContainer container where the dependencied fot this adapter will be resolved
-     * @throws NoSatisfiableConstructorsException 
+     * Verify that all dependencies for this adapter can be satisifed.
+     * 
+     * @param picoContainer container where the dependencied fot this adapter will be resolved.
+     * @throws PicoIntrospectionException if the dependencies cannot be resolved.
      */
-    void verify(PicoContainer picoContainer) throws NoSatisfiableConstructorsException;
+    void verify(PicoContainer picoContainer) throws PicoIntrospectionException;
 
 }

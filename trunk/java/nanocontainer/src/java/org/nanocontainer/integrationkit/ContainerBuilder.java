89103bf0c38bc5927b13739c800e1cefef85779d
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-01-23 23:49:48 +0000

    Readded. Was accidentally deleted.

---
+++ trunk/java/nanocontainer/src/java/org/nanocontainer/integrationkit/ContainerBuilder.java
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.picoextras.integrationkit;
+
+import org.picocontainer.defaults.ObjectReference;
+
+/**
+ * The responsability of a ContainerBuilder is to <em>instantiate</em> containers.
+ * It delegates the responsability of the <em>assembly</em> to a
+ * {@link ContainerAssembler}.
+ *
+ * @author <a href="mailto:joe@thoughtworks.net">Joe Walnes</a>
+ */
+public interface ContainerBuilder {
+
+    /**
+     * Create, assemble, init and start a new PicoContainer and store it
+     * at a given reference.
+     *
+     * @param containerRef        Where to store the new container.
+     * @param parentContainerRef  reference to a container that may be used as a parent to the new container (may be null).
+     * @param assemblyScope       Argument to be passed to ContainerAssembler.
+     */
+    void buildContainer(ObjectReference containerRef, ObjectReference parentContainerRef, Object assemblyScope);
+
+    /**
+     * Locate a container at the given reference so it can be stopped,
+     * destroyed and removed.
+     *
+     * @param containerRef        Where the container is stored.
+     */
+    void killContainer(ObjectReference containerRef);
+
+}
+

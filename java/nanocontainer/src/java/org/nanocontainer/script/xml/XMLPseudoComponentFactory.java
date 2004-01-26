816355c310013677a6eafad9c9f68eb4c5e2d009
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-01-26 19:01:27 +0000

    renames

---
+++ java/nanocontainer/src/java/org/nanocontainer/script/xml/XMLPseudoComponentFactory.java
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Aslak Hellesoy and Paul Hammant                          *
+ *****************************************************************************/
+
+package org.picoextras.script.xml;
+
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+public interface XMLPseudoComponentFactory {
+    Object makeInstance(Element elem) throws SAXException, ClassNotFoundException;
+}

8f6f5c86dc22fcfcb9179fd800d2aeb03d4b754a
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-01-26 19:01:27 +0000

    renames

---
+++ trunk/java/nanocontainer/src/java/org/nanocontainer/script/xml/XStreamXMLPseudoComponentFactory.java
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.picoextras.script.xml;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.xml.dom.DomXMLReader;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Paul Hammant
+ * @author Marcos Tarruella
+ */
+public class XStreamXMLPseudoComponentFactory implements XMLPseudoComponentFactory {
+    public Object makeInstance(Element elem) throws SAXException, ClassNotFoundException {
+        XStream xs = new XStream();
+        Object result = xs.fromXML(new DomXMLReader(elem));
+        return result;
+    }
+}

32d3de585c735e5e23db7e28d0435dc6de8135fd
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-02-10 22:36:51 +0000

    NanoContainer is back

---
+++ java/nanocontainer/src/java/org/nanocontainer/NanoContainer.java
@@ -0,0 +1,101 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+
+package org.nanocontainer;
+
+import org.picocontainer.defaults.DefaultPicoContainer;
+import org.picocontainer.defaults.ObjectReference;
+import org.picocontainer.defaults.SimpleReference;
+import org.picocontainer.defaults.NullPicoContainer;
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.PicoContainer;
+import org.nanocontainer.reflection.DefaultReflectionContainerAdapter;
+import org.nanocontainer.integrationkit.ContainerBuilder;
+import org.nanocontainer.script.ScriptedComposingLifecycleContainerBuilder;
+
+import java.io.Reader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+
+public class NanoContainer {
+
+    public static final String JAVASCRIPT = ".js";
+    public static final String JYTHON = ".py";
+    public static final String GROOVY = ".groovy";
+    public static final String XML = ".xml";
+    public static final String BEANSHELL = ".bsh";
+
+    protected static final Map extensionToComposers = new HashMap();
+
+    static {
+        extensionToComposers.put(JAVASCRIPT, "org.nanocontainer.script.rhino.JavascriptContainerBuilder");
+        extensionToComposers.put(XML, "org.nanocontainer.script.xml.XMLContainerBuilder");
+        extensionToComposers.put(JYTHON, "org.nanocontainer.script.jython.JythonContainerBuilder");
+        extensionToComposers.put(GROOVY, "org.nanocontainer.script.groovy.GroovyContainerBuilder");
+        extensionToComposers.put(BEANSHELL, "org.nanocontainer.script.bsh.BeanShellContainerBuilder");
+    }
+
+    protected ScriptedComposingLifecycleContainerBuilder containerBuilder;
+
+    protected ObjectReference containerRef;
+
+    public NanoContainer(File compositionFile, PicoContainer parent, ClassLoader classLoader) throws IOException, ClassNotFoundException {
+        this(new FileReader(compositionFile), compositionFile.getCanonicalPath().substring(compositionFile.getCanonicalPath().indexOf(".")) , parent, classLoader);
+    }
+
+    public NanoContainer(File compositionFile, PicoContainer parent) throws IOException, ClassNotFoundException {
+        this(new FileReader(compositionFile), compositionFile.getCanonicalPath().substring(compositionFile.getCanonicalPath().indexOf(".")) , parent, NanoContainer.class.getClassLoader());
+    }
+
+    public NanoContainer(File compositionFile) throws IOException, ClassNotFoundException {
+        this(new FileReader(compositionFile), compositionFile.getCanonicalPath().substring(compositionFile.getCanonicalPath().indexOf(".")), new NullPicoContainer(), NanoContainer.class.getClassLoader());
+    }
+
+    public NanoContainer(Reader composition, String language, ClassLoader classLoader) throws ClassNotFoundException {
+        this(composition, language, new NullPicoContainer(), classLoader);
+    }
+
+    public NanoContainer(Reader composition, String language) throws ClassNotFoundException {
+        this(composition, language, new NullPicoContainer(), NanoContainer.class.getClassLoader());
+    }
+
+    public NanoContainer(Reader composition, String language, PicoContainer parent, ClassLoader classLoader) throws ClassNotFoundException {
+
+        String containerAssemblerClassName = (String) extensionToComposers.get(language);
+        DefaultReflectionContainerAdapter defaultReflectionContainerAdapter;
+        {
+            // disposable.
+            DefaultPicoContainer dpc = new DefaultPicoContainer();
+            dpc.registerComponentInstance(composition);
+            dpc.registerComponentInstance(NanoContainer.class.getClassLoader());
+            dpc.registerComponentInstance(classLoader);
+            defaultReflectionContainerAdapter = new DefaultReflectionContainerAdapter(dpc);
+        }
+        ComponentAdapter componentAdapter = defaultReflectionContainerAdapter.registerComponentImplementation(containerAssemblerClassName);
+        containerBuilder = (ScriptedComposingLifecycleContainerBuilder) componentAdapter.getComponentInstance();
+        containerRef = new SimpleReference();
+        final ObjectReference parentRef = new SimpleReference();
+        parentRef.set(parent);
+
+        // build and start the container
+        containerBuilder.buildContainer(containerRef, parentRef, null);
+    }
+
+    public void killContainer() {
+        containerBuilder.killContainer(containerRef);
+    }
+
+    public ScriptedComposingLifecycleContainerBuilder getContainerBuilder() {
+        return containerBuilder;
+    }
+
+}

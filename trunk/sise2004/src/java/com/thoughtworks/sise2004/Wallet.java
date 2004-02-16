9eb592a17db3a96be102269d90d34b60bad62a1b
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-02-16 15:54:33 +0000

    import of files

---
+++ trunk/sise2004/src/java/com/thoughtworks/sise2004/Wallet.java
@@ -0,0 +1,8 @@
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+package com.thoughtworks.sise2004;
+
+public class Wallet {
+}
---
+++ trunk/sise2004/src/test/com/thoughtworks/sise2004/WalletTestCase.java
@@ -0,0 +1,15 @@
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+package com.thoughtworks.sise2004;
+
+import junit.framework.TestCase;
+
+public class WalletTestCase extends TestCase {
+
+    public void testNewWalletShouldBeEmpty() {
+        Wallet wallet = new Wallet();
+    }
+
+}

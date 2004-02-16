d804001bd0963172b211f64a4769e227c364d028
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-02-16 17:25:23 +0000

    Done with iteration I

--- sise2004/src/java/com/thoughtworks/sise2004/Wallet.java
+++ sise2004/src/java/com/thoughtworks/sise2004/Wallet.java
@@ -5,11 +5,27 @@
 package com.thoughtworks.sise2004;
 
 public class Wallet {
+    private long money;
 
-    public Wallet()
-    {
+    public long getMoney() {
+        long result = money;
+        money = 0;
+        return result;
     }
 
+    public void putMoney(int someMoney) {
+        money = someMoney;
+    }
 
-    
+    public long viewMoney() {
+        return money;
+    }
+
+    public long getMoney(long amount) throws NotEnoughMoneyException {
+        if(amount > money) {
+            throw new NotEnoughMoneyException();
+        }
+        money -= amount;
+        return amount;
+    }
 }

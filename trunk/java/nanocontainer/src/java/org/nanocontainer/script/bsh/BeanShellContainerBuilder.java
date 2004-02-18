927e34b0ddddc9cc8b787bf50bcba5c89c9854e0
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-02-18 21:33:23 +0000

    Added Bsh support

---
+++ trunk/java/nanocontainer/src/java/org/nanocontainer/script/bsh/BeanShellContainerBuilder.java
@@ -0,0 +1,33 @@
+package org.nanocontainer.script.bsh;
+
+import org.nanocontainer.script.ScriptedContainerBuilder;
+import org.nanocontainer.integrationkit.PicoCompositionException;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoContainer;
+
+import java.io.Reader;
+
+import bsh.Interpreter;
+import bsh.EvalError;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public class BeanShellContainerBuilder extends ScriptedContainerBuilder {
+    public BeanShellContainerBuilder(Reader script, ClassLoader classLoader) {
+        super(script, classLoader);
+    }
+
+    protected MutablePicoContainer createContainer(PicoContainer parentContainer, Object assemblyScope) {
+        Interpreter i = new Interpreter();
+        try {
+            i.set("parent", parentContainer);
+            i.set("assemblyScope", assemblyScope);
+            i.eval(script, i.getNameSpace(), "nanocontainer.bsh");
+            return (MutablePicoContainer) i.get("pico");
+        } catch (EvalError e) {
+            throw new PicoCompositionException(e);
+        }
+    }
+}

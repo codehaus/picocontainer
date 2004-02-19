b002c70dcc9ec88f08c3ed8082262b2d0e7000e6
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-02-19 22:47:33 +0000

    Code to be used in docs

---
+++ trunk/java/picocontainer/src/test/org/picocontainer/doc/Peeler.java
@@ -0,0 +1,19 @@
+package org.picocontainer.doc;
+
+import org.picocontainer.Startable;
+
+public class Peeler implements Startable {
+    private final Peelable peelable;
+
+    public Peeler(Peelable peelable) {
+        this.peelable = peelable;
+    }
+
+    public void start() {
+        peelable.peel();
+    }
+
+    public void stop() {
+
+    }
+}

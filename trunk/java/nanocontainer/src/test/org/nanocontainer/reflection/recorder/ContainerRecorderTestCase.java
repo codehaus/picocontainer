ebbacd90e01d9b54f50c0acd55128fb397d2959e
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-02-21 16:33:52 +0000

    Added Konstantin's ContainerRecorder (PICO-137) with some additions. See JIRA for details.

---
+++ trunk/java/nanocontainer/src/test/org/nanocontainer/reflection/recorder/ContainerRecorderTestCase.java
@@ -0,0 +1,67 @@
+package org.nanocontainer.reflection.recorder;
+
+import junit.framework.TestCase;
+import org.nanocontainer.testmodel.ThingThatTakesParamsInConstructor;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.Parameter;
+import org.picocontainer.defaults.ComponentParameter;
+import org.picocontainer.defaults.DefaultPicoContainer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author Konstantin Pribluda ( konstantin.pribluda(at)infodesire.com )
+ * @author Aslak Helles&oslash;y
+ */
+public class ContainerRecorderTestCase extends TestCase {
+	public void testInvocationsCanBeRecordedAndReplayedOnADifferentContainerInstance() throws Exception {
+		ContainerRecorder recorder = new ContainerRecorder(new DefaultPicoContainer());
+		MutablePicoContainer recorded = recorder.getContainerProxy();
+
+		recorded.registerComponentInstance("fruit", "apple");
+		recorded.registerComponentInstance("int",new Integer(239));
+		recorded.registerComponentImplementation("thing",
+			ThingThatTakesParamsInConstructor.class,
+			new Parameter[] {
+					new ComponentParameter(),
+					new ComponentParameter()
+			});
+
+        MutablePicoContainer slave = new DefaultPicoContainer();
+		recorder.replay(slave);
+		assertEquals("apple",slave.getComponentInstance("fruit"));
+		assertEquals("apple239",((ThingThatTakesParamsInConstructor)slave.getComponentInstance("thing")).getValue());
+
+		// test that we can replay once more
+		MutablePicoContainer anotherSlave = new DefaultPicoContainer();
+		recorder.replay(anotherSlave);
+		assertEquals("apple",anotherSlave.getComponentInstance("fruit"));
+		assertEquals("apple239",((ThingThatTakesParamsInConstructor)anotherSlave.getComponentInstance("thing")).getValue());
+	}
+
+    public void testRecorderWorksAfterSerialization() throws IOException, ClassNotFoundException, IllegalAccessException, InvocationTargetException {
+        ContainerRecorder recorder = new ContainerRecorder(new DefaultPicoContainer());
+        MutablePicoContainer recorded = recorder.getContainerProxy();
+        recorded.registerComponentInstance("fruit", "apple");
+
+        ContainerRecorder serializedRecorder = (ContainerRecorder) serializeAndDeserialize(recorder);
+        MutablePicoContainer slave = new DefaultPicoContainer();
+		serializedRecorder.replay(slave);
+		assertEquals("apple",slave.getComponentInstance("fruit"));
+    }
+
+    private Object serializeAndDeserialize(Object o) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+
+        oos.writeObject(o);
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()));
+
+        return ois.readObject();
+    }
+}

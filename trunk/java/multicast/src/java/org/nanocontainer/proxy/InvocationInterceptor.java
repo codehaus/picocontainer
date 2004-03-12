3ae5227a4a49bbdc44d605fd7cfb9a72784603b6
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-03-12 13:23:21 +0000

    Small improvements

--- trunk/java/multicast/src/java/org/nanocontainer/proxy/InvocationInterceptor.java
+++ trunk/java/multicast/src/java/org/nanocontainer/proxy/InvocationInterceptor.java
@@ -1,22 +1,8 @@
-/*****************************************************************************
- * Copyright (C) NanoContainer Organization. All rights reserved.            *
- * ------------------------------------------------------------------------- *
- * The software in this package is published under the terms of the BSD      *
- * style license a copy of which has been included with this distribution in *
- * the LICENSE.txt file.                                                     *
- *                                                                           *
- * Original code by                                                          *
- *****************************************************************************/
-/*
- * Created by IntelliJ IDEA.
- * User: ahelleso
- * Date: 26-Jan-2004
- * Time: 21:14:38
- */
 package org.nanocontainer.proxy;
 
 import java.lang.reflect.Method;
+import java.io.Serializable;
 
-public interface InvocationInterceptor {
+public interface InvocationInterceptor extends Serializable {
     Object intercept(Object proxy, Method method, Object[] args) throws Throwable;
 }
---
+++ trunk/java/multicast/src/java/org/nanocontainer/proxy/StandardInvocationInterceptorAdapter.java
@@ -0,0 +1,21 @@
+package org.nanocontainer.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.io.Serializable;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+class StandardInvocationInterceptorAdapter implements InvocationHandler, Serializable {
+    private final InvocationInterceptor invocationInterceptor;
+
+    public StandardInvocationInterceptorAdapter(InvocationInterceptor invocationInterceptor) {
+        this.invocationInterceptor = invocationInterceptor;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return invocationInterceptor.intercept(proxy, method, args);
+    }
+}

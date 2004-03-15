adb35fda45621b786e902789eebb2e4161fad7f0
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-03-15 20:17:32 +0000

    Upgraded to use more uptodate velocity servlet Added integration test that runs on Jetty

---
+++ trunk/java/sample-nanoweb/src/test/org/nanocontainer/sample/nanoweb/IntegrationTestCase.java
@@ -0,0 +1,47 @@
+package org.nanocontainer.sample.nanoweb;
+
+import org.custommonkey.xmlunit.XMLTestCase;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.mortbay.http.SocketListener;
+import org.mortbay.jetty.Server;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public class IntegrationTestCase extends XMLTestCase {
+    static final int PORT = 12345;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.getControlDocumentBuilderFactory().setIgnoringComments(true);
+        XMLUnit.getTestDocumentBuilderFactory().setIgnoringComments(true);
+    }
+
+    public void testWithJetty() throws Exception {
+        Server server = new Server();
+        try {
+            SocketListener listener = new SocketListener();
+            listener.setPort(PORT);
+            server.addListener(listener);
+
+            String webappDir = "target/nanocontainer-sample-nanoweb";
+            server.addWebApplication("/nanocontainer-sample-nanoweb/*", webappDir);
+
+            server.start();
+
+            URL url = new URL("http://localhost:" + PORT + "/nanocontainer-sample-nanoweb/demo.nano");
+            InputStream expected = getClass().getResourceAsStream("expected.xhtml");
+
+            InputStream actual = url.openStream();
+            assertXMLEqual(new InputStreamReader(expected), new InputStreamReader(actual));
+        } finally {
+            server.stop();
+        }
+    }
+}

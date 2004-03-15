a76f2ed603bbe58e1a3f611ac93ef0cb26700fa6
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-03-15 20:59:11 +0000

    o Optimisations

--- trunk/java/piccolo/src/java/org/nanocontainer/piccolo/ContainerView.java
+++ trunk/java/piccolo/src/java/org/nanocontainer/piccolo/ContainerView.java
@@ -9,12 +9,6 @@
  *****************************************************************************/
 package org.nanocontainer.piccolo;
 
-import java.awt.Dimension;
-import java.awt.geom.Point2D;
-
-import org.picocontainer.PicoContainer;
-import org.nanocontainer.piccolo.Constants;
-
 import edu.umd.cs.piccolo.PCamera;
 import edu.umd.cs.piccolo.PCanvas;
 import edu.umd.cs.piccolo.PNode;
@@ -23,86 +17,91 @@ import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.nodes.PPath;
 import edu.umd.cs.piccolo.nodes.PText;
 import edu.umd.cs.piccolo.util.PBounds;
+import org.picocontainer.PicoContainer;
+
+import java.awt.Dimension;
+import java.awt.geom.Point2D;
 
 public class ContainerView extends PCanvas {
 
-	private PicoContainer container;
-	private PNode containerNode;
-	private PNode lastSelection;
-	private PText tooltipNode;
+    private PicoContainer container;
+    private PNode containerNode;
+    private PNode lastSelection;
+    private PText tooltipNode;
 
-	public ContainerView(PicoContainer container) {
-		this.container = container;
+    public ContainerView(PicoContainer container) {
+        this.container = container;
 
-		this.tooltipNode = new PText();
-		this.tooltipNode.setPickable(false);
-		getCamera().addChild(tooltipNode);
+        this.tooltipNode = new PText();
+        this.tooltipNode.setPickable(false);
+        getCamera().addChild(tooltipNode);
 
-		// Create the path for the root container
-		this.containerNode = PiccoloHelper.createNodeForContainer(this.container, 0, 0, 300);
-		this.getLayer().addChild(this.containerNode);
+        // Create the path for the root container
+        this.containerNode = PiccoloHelper.createNodeForContainer(this.container, 0, 0, 300);
+        this.getLayer().addChild(this.containerNode);
 
-		// Auto Pan/Zoom
-		this.getCamera().addInputEventListener(new PBasicInputEventHandler() {
-			public void mousePressed(PInputEvent event) {
-				PNode node = event.getPickedNode();
-				if (node instanceof PCamera) {
-				} else {
-					// Simulate selection by changing the stroke paint
-					if (lastSelection instanceof PPath) {
-						((PPath) lastSelection).setStrokePaint(Constants.PICO_STROKE);
-					}
-					lastSelection = node;
-					if (lastSelection instanceof PPath) {
-						((PPath) lastSelection).setStrokePaint(Constants.PICO_GREEN);
-					}
+        // Auto Pan/Zoom
+        this.getCamera().addInputEventListener(new PBasicInputEventHandler() {
+            public void mousePressed(PInputEvent event) {
+                PNode node = event.getPickedNode();
+                if (node instanceof PCamera) {
+                } else {
+                    // Simulate selection by changing the stroke paint
+                    if (lastSelection instanceof PPath) {
+                        ((PPath) lastSelection).setStrokePaint(Constants.PICO_STROKE);
+                    }
+                    lastSelection = node;
+                    if (lastSelection instanceof PPath) {
+                        ((PPath) lastSelection).setStrokePaint(Constants.PICO_GREEN);
+                    }
 
-					// Tag the event
-					//event.setHandled(true);
+                    // Tag the event
+                    //event.setHandled(true);
 
-					if (event.getClickCount() == 2) {
-						// Get the bounds
-						PBounds b = node.getGlobalFullBounds();
-						double offset = b.getWidth() / 20;
-						PBounds n = new PBounds(b.getX() - offset, b.getY() - offset, b.getWidth() + 2 * offset, b.getHeight() + 2 * offset);
+                    if (event.getClickCount() == 2) {
+                        // Get the bounds
+                        PBounds b = node.getGlobalFullBounds();
+                        double offset = b.getWidth() / 20;
+                        PBounds n = new PBounds(b.getX() - offset, b.getY() - offset, b.getWidth() + 2 * offset, b.getHeight() + 2 * offset);
 
-						// Animate to the bounds
+                        // Animate to the bounds
 // (AH - doesn't compile)                       getCamera().animateViewToCenterBounds(n, true, 500);
                         getCamera().animateViewToBounds(n, true, 500);
-					}
-				}
-			}
-			public void mouseMoved(PInputEvent event) {
-				updateToolTip(event);
-			}
+                    }
+                }
+            }
 
-			public void mouseDragged(PInputEvent event) {
-				updateToolTip(event);
-			}
+            public void mouseMoved(PInputEvent event) {
+                updateToolTip(event);
+            }
 
-			public void updateToolTip(PInputEvent event) {
-				PNode n = event.getInputManager().getMouseOver().getPickedNode();
-				String tooltipString = (String) n.getClientProperty(Constants.TOOL_TIP);
-				Point2D p = event.getCanvasPosition();
+            public void mouseDragged(PInputEvent event) {
+                updateToolTip(event);
+            }
 
-				event.getPath().canvasToLocal(p, getCamera());
+            public void updateToolTip(PInputEvent event) {
+                PNode n = event.getInputManager().getMouseOver().getPickedNode();
+                String tooltipString = (String) n.getClientProperty(Constants.TOOL_TIP);
+                Point2D p = event.getCanvasPosition();
 
-				tooltipNode.setText(tooltipString);
-				tooltipNode.setOffset(p.getX() + 8, p.getY() - 8);
-			}
-		});
+                event.getPath().canvasToLocal(p, getCamera());
 
-		// No zoom handler
-		this.removeInputEventListener(getZoomEventHandler());
-		// No pan handler
-		this.removeInputEventListener(getPanEventHandler());
-	}
+                tooltipNode.setText(tooltipString);
+                tooltipNode.setOffset(p.getX() + 8, p.getY() - 8);
+            }
+        });
 
-	public Dimension getMinimumSize() {
-		return new Dimension((int) getLayer().getFullBounds().getWidth(), (int) getLayer().getFullBounds().getHeight());
-	}
+        // No zoom handler
+        this.removeInputEventListener(getZoomEventHandler());
+        // No pan handler
+        this.removeInputEventListener(getPanEventHandler());
+    }
 
-	public Dimension getPreferredSize() {
-		return getMinimumSize();
-	}
+    public Dimension getMinimumSize() {
+        return new Dimension((int) getLayer().getFullBounds().getWidth(), (int) getLayer().getFullBounds().getHeight());
+    }
+
+    public Dimension getPreferredSize() {
+        return getMinimumSize();
+    }
 }

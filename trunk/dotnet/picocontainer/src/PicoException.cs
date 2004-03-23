f465f3d37ee13b03b226607364478f3b4874df05
maarten <maarten@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-03-23 17:35:27 +0000

    Added comment and constructors to make it compliant with FxCop rules

--- trunk/dotnet/picocontainer/src/PicoException.cs
+++ trunk/dotnet/picocontainer/src/PicoException.cs
@@ -10,13 +10,75 @@
  *****************************************************************************/
 
 using System;
+using System.Runtime.Serialization;
 
-namespace PicoContainer
-{
+namespace PicoContainer {
 
-	[Serializable]
-  public class PicoException : Exception
+  /// <summary>
+  /// The PicoException is thrown when an error in the PicoContainer occurs.
+  /// </summary>
+  /// <remarks>
+  /// This is the base exception for all exceptions thrown in PicoContainer. 
+  /// Derive all PicoContainer specific exceptions in PicoContainer from this exception.
+  /// </remarks>
+  [Serializable]
+  public class PicoException : ApplicationException {
 
-  {
-	}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PicoContainer.PicoException"/> class.
+    /// </summary>
+    /// <remarks>
+    /// This constructor initializes the Message property of the new instance to a system-supplied message 
+    /// that describes the error, such as "PicoContainer caused an exception." 
+    /// </remarks>
+    public PicoException() : base ("PicoContainer caused an exception.") {}
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PicoContainer.PicoException"/> class with a specified error message.
+    /// </summary>
+    /// <remarks>
+    /// This constructor initializes the Message property of the new instance using the message parameter.
+    /// </remarks>
+    /// <param name="ex">The exception that is the cause of the current exception. 
+    /// If the innerException parameter is not a null reference (Nothing in Visual Basic), 
+    /// the current exception is raised in a catch block that handles the inner exception.</param>
+    public PicoException(Exception ex) : base("PicoContainer caused an exception.",ex) {  }
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PicoContainer.PicoException"/> class with a 
+    /// reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <remarks>
+    /// This constructor initializes the Message property of the new instance to the Message property of the 
+    /// passed in exception. 
+    /// </remarks>
+    /// <param name="message">The message that describes the error.</param>
+    public PicoException(string message) : base(message) {}
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PicoContainer.PicoException"/> class with a specified error message 
+    /// and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <remarks>
+    /// An exception that is thrown as a direct result of a previous exception should include a reference to the previous 
+    /// exception in the InnerException property. 
+    /// The InnerException property returns the same value that is passed into the constructor, or a null reference 
+    /// (Nothing in Visual Basic) if the InnerException property does not supply the inner exception value to the constructor.
+    /// </remarks>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="ex">The exception that caused the error</param>
+    public PicoException(string message, Exception ex) : base(message,ex) {}
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PicoContainer.PicoException"/> class with serialized data.
+    /// </summary>
+    /// <remarks>
+    /// This constructor is called during deserialization to reconstitute the exception object transmitted over a stream.
+    /// </remarks>
+    /// <param name="info">The <see cref="System.Runtime.Serialization.SerializationInfo"/> that holds the serialized 
+    /// object data about the exception being thrown.</param>
+    /// <param name="context">The <see cref="System.Runtime.Serialization.StreamingContext"/> that contains contextual 
+    /// information about the source or destination. </param>
+    protected PicoException(SerializationInfo info, StreamingContext context) : base (info, context) {}
+  }
 }

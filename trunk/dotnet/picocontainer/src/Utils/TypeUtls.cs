0e350148b426d9174dae1b755841429624137bf3
maarten <maarten@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-03-28 15:45:12 +0000

    Added method to show constructors in exceptions

--- trunk/dotnet/picocontainer/src/Utils/TypeUtls.cs
+++ trunk/dotnet/picocontainer/src/Utils/TypeUtls.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.Text;
 
 namespace PicoContainer.Utils
 {
@@ -29,5 +30,21 @@ namespace PicoContainer.Utils
       return GetParameterTypes(ci.GetParameters());
     }
 
+    public static string ConstructorAsString(ConstructorInfo ci)
+    {
+      StringBuilder sb = new StringBuilder(ci.DeclaringType.FullName).Append("(");
+      ParameterInfo []pi = ci.GetParameters();
+      for(int x =0 ; x < pi.Length;x++)
+      {
+        sb.Append(pi[x].ParameterType);
+        if (x+1 < pi.Length)
+        {
+          sb.Append(" ,");
+        }
+      }
+
+      sb.Append(")");
+      return sb.ToString();
+    }
   }
 }

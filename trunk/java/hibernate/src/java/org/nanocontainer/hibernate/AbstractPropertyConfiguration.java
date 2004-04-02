9813ad94a9616bca6083eee0cf56cf8a9206ed97
ko5tik <ko5tik@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-04-02 15:01:41 +0000

    initial release of pico hibernate integration

---
+++ trunk/java/hibernate/src/java/org/nanocontainer/hibernate/AbstractPropertyConfiguration.java
@@ -0,0 +1,80 @@
+/*****************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the license.html file.                                                    *
+ *                                                                           *
+ * Idea by Rachel Davies, Original code by Aslak Hellesoy and Paul Hammant   *
+ *****************************************************************************/
+package org.nanocontainer.hibernate;
+
+import net.sf.hibernate.cfg.Configuration;
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.MappingException;
+
+import java.util.Properties;
+
+import java.io.IOException;
+
+/**
+ * abstract base class for property based hibernate configuration. concrete implementation 
+ * shall register persistent classes. 
+ *
+ * @author    Konstantin Pribluda ( konstantin.pribluda[at]infodesire.com )
+ * @version   $Revision$
+ */
+public abstract class AbstractPropertyConfiguration extends Configuration {
+    /**
+     * configure from properties contained on class path
+     *
+     * @param properties              path to your properties on classpath
+     * @exception HibernateException  if hibernate barfs
+     * @exception IOException         if your properties path is suboptimal 
+     */
+    protected AbstractPropertyConfiguration(String properties) throws HibernateException, IOException {
+        addProperties(loadProperties(properties));
+        registerClasses();
+    }
+
+    /**
+     * configure from properties obtained elswhere
+     *
+     * @param properties              properties obtained elswhere
+     * @exception HibernateException  if hibernate barfs...
+     */
+    protected AbstractPropertyConfiguration(Properties properties) throws HibernateException {
+        addProperties(properties);
+        registerClasses();
+    }
+
+
+    /**
+     * we do not allow to instantiate us without proper properties
+     */
+    private AbstractPropertyConfiguration() {
+    }
+
+
+    /**
+     * load properties from classpath
+     *
+     * @param name             path to properties
+     * @return                 properties we have loaded
+     * @exception IOException  if properties can not be loaded
+     */
+     Properties loadProperties(String name) throws IOException {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(name));
+        return hibernateProperties;
+    }
+
+
+    /**
+     * register application classes. implementors have to override this. 
+     *
+     * @exception MappingException  if hibernate finds your mappings unworthy
+     */
+    protected abstract void registerClasses() throws MappingException;
+
+}

e7daf0ef9ed65eb58e1082a15ffb3ce8c0ae6cd6
lsimons <lsimons@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-05-05 20:17:28 +0000

    start adding more end-user-friendly javadocs. No other changes.

--- java/picocontainer/src/java/org/picocontainer/defaults/Swappable.java
+++ java/picocontainer/src/java/org/picocontainer/defaults/Swappable.java
@@ -10,17 +10,20 @@
 package org.picocontainer.defaults;
 
 /**
- * Interface implemented by all proxy instances created by
- * {@link ImplementationHidingComponentAdapter}.
+ * This interface is implemented by delegate proxies which support replacing the delegate object
+ * behind that proxy with a different instance. It is implemented by all proxy instances created by
+ * {@link ImplementationHidingComponentAdapter}. Please note that implementations of this method
+ * are not guaranteed to be safe for concurrent use.
  *
  * @author Aslak Helles&oslash;y
  * @version $Revision$
  */
 public interface Swappable {
     /**
-     * Swaps the subject behind the proxy with a new instance.
-     * @param newSubject the new subject the proxy will delegate to.
-     * @return the old subject
+     * Swaps the delegate behind the proxy with a new instance.
+     * 
+     * @param newDelegate the new delegate to which the proxy will defer calls.
+     * @return the old delegate which has been replaced.
      */
-    Object hotswap(Object newSubject);
+    Object hotswap(Object newDelegate);
 }

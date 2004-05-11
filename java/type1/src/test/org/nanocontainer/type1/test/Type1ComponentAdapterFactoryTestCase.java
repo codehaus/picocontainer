eca6e10fe1da0bfeb697be96e37f2702cd850a9e
lsimons <lsimons@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-05-11 18:53:42 +0000

    Initial revision

---
+++ java/type1/src/test/org/nanocontainer/type1/test/Type1ComponentAdapterFactoryTestCase.java
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Leo Simons                                               *
+ *****************************************************************************/
+package org.nanocontainer.type1.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.nanocontainer.type1.Type1ComponentAdapter;
+import org.nanocontainer.type1.Type1ComponentAdapterFactory;
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.Parameter;
+
+/**
+ * @author <a href="lsimons at jicarilla dot org">Leo Simons</a>
+ * @version $Id$
+ */
+public class Type1ComponentAdapterFactoryTestCase extends TestCase {
+    public void testEverything()
+    {
+        final Type1ComponentAdapterFactory f = new Type1ComponentAdapterFactory();
+        final ComponentAdapter adapter = f.createComponentAdapter(this, this.getClass(), new Parameter[0] );
+        assertTrue( adapter instanceof Type1ComponentAdapter );
+        assertEquals( this.getClass(), adapter.getComponentImplementation() );
+        assertEquals( this, adapter.getComponentKey() );
+        assertTrue( adapter.getComponentInstance() instanceof Test );
+    }
+}
---
+++ java/type1/src/test/org/nanocontainer/type1/test/Type1ComponentAdapterTestCase.java
@@ -0,0 +1,169 @@
+/*****************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Leo Simons                                               *
+ *****************************************************************************/
+package org.nanocontainer.type1.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.context.ContextException;
+import org.apache.avalon.framework.service.ServiceException;
+import org.nanocontainer.type1.*;
+import org.picocontainer.defaults.PicoInvocationTargetInitializationException;
+
+/**
+ * @author <a href="lsimons at jicarilla dot org">Leo Simons</a>
+ * @version $Id$
+ */
+public class Type1ComponentAdapterTestCase extends TestCase {
+    
+    public void testConstructor()
+    {
+        new Type1ComponentAdapter(this, this.getClass());
+    }
+    
+    public void testVerify()
+    {
+        new Type1ComponentAdapter(this, this.getClass()).verify();
+    }
+    
+    public void testGetComponentInstance()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter(this, this.getClass());
+        assertTrue( a.getComponentInstance() instanceof Test );
+    }
+
+    public void testGetComponentInstanceInstantiationException()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter("mock1", Mock1.class);
+        try
+        {
+            a.getComponentInstance();
+            fail( "Expected an exception!" );
+        }
+        catch( PicoInvocationTargetInitializationException th )
+        {
+            assertTrue( th.getCause() instanceof InstantiationException );
+        }
+    }
+    
+    public void testGetComponentIllegalAccessException()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter("mock2", Mock2.class);
+        try
+        {
+            a.getComponentInstance();
+            fail( "Expected an exception!" );
+        }
+        catch( PicoInvocationTargetInitializationException th )
+        {
+            assertTrue( th.getCause() instanceof IllegalAccessException );
+        }
+    }
+    
+    public void testGetComponentContextException()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter("mock3", Mock3.class);
+        try
+        {
+            a.getComponentInstance();
+            fail( "Expected an exception!" );
+        }
+        catch( PicoContextException th )
+        {
+            assertTrue( th.getCause() instanceof ContextException );
+        }
+    }
+    
+    public void testGetComponentServiceException()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter("mock4", Mock4.class);
+        try
+        {
+            a.getComponentInstance();
+            fail( "Expected an exception!" );
+        }
+        catch( PicoServiceException th )
+        {
+            assertTrue( th.getCause() instanceof ServiceException );
+        }
+    }
+    
+    public void testGetComponentConfigurationException()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter("mock5", Mock5.class);
+        try
+        {
+            a.getComponentInstance();
+            fail( "Expected an exception!" );
+        }
+        catch( PicoConfigurationException th )
+        {
+            assertTrue( th.getCause() instanceof ConfigurationException );
+        }
+    }
+
+    public void testGetComponentSomeException()
+    {
+        final Type1ComponentAdapter a = new Type1ComponentAdapter("mock6", Mock6.class);
+        try
+        {
+            a.getComponentInstance();
+            fail( "Expected an exception!" );
+        }
+        catch( PicoType1ContractException th )
+        {
+            assertTrue( th.getCause() instanceof SomeException );
+        }
+    }
+    
+    public static class Mock1
+    {
+        public Mock1() throws InstantiationException {
+            throw new InstantiationException();
+        }
+    }
+
+    public static class Mock2
+    {
+        public Mock2() throws IllegalAccessException {
+            throw new IllegalAccessException();
+        }
+    }
+
+    public static class Mock3
+    {
+        public Mock3() throws ContextException {
+            throw new ContextException("blah");
+        }
+    }
+
+    public static class Mock4
+    {
+        public Mock4() throws ServiceException {
+            throw new ServiceException("blah", "blah");
+        }
+    }
+
+    public static class Mock5
+    {
+        public Mock5() throws ConfigurationException {
+            throw new ConfigurationException("blah");
+        }
+    }
+
+    public static class Mock6
+    {
+        public Mock6() throws SomeException {
+            throw new SomeException();
+        }
+    }
+    
+    public static class SomeException extends Exception {}
+}

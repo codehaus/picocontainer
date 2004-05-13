bcd7f546a62a11c59f7d31cb644a0246c38ec028
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-05-13 21:48:58 +0000

    Initial commit for client-side EJB supprt (NANO-77)

---
+++ trunk/java/ejb/src/java/org/nanocontainer/ejb/EJBClientComponentAdapter.java
@@ -0,0 +1,136 @@
+/*****************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Joerg Schaible                                           *
+ *****************************************************************************/
+package org.nanocontainer.ejb;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.ejb.EJBHome;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.rmi.PortableRemoteObject;
+
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.PicoInitializationException;
+import org.picocontainer.PicoIntrospectionException;
+import org.picocontainer.defaults.*;
+import org.picocontainer.defaults.AssignabilityRegistrationException;
+import org.picocontainer.defaults.UnsatisfiableDependenciesException;
+
+/**
+ * {@link ComponentAdapter}, that is able to lookup and instantiate an EJB as client.
+ * <p>If you want to cache the EJB with a {@link org.picocontainer.defaults.CachingComponentAdapter}, 
+ * you have to use a {@link ThreadLocalReference}, since you may not use an instance of the EJB 
+ * in different threads.</p>
+ * <p>Use a EJBClientComponnatAdapterFactory for a completely transparent {@link ThreadLocal} 
+ * support.</p>  
+ * @author J&ouml;rg Schaible
+ */
+public class EJBClientComponentAdapter implements ComponentAdapter {
+
+    private PicoContainer m_picoContainer;
+    private final Object m_home;
+    private final Method m_create;
+    private final String m_ejbName;
+    private final Class m_ejbClass;
+
+    /**
+     * Construct a {@link ComponentAdapter} for an EJB.
+     * @param ejb The EJB's name.
+     * @param homeInterface The home interface of the EJB.
+     * @param context The {@link InitialContext} to use.
+     * @throws PicoIntrospectionException Thrown if lookup of home interface fails.
+     */
+    public EJBClientComponentAdapter(final String ejb, final Class homeInterface, final InitialContext context) {
+        super();
+        try {
+            if (!EJBHome.class.isAssignableFrom(homeInterface)) {
+                throw new AssignabilityRegistrationException(EJBHome.class, homeInterface);
+            }
+            final Object ref = context.lookup(ejb);
+            m_home = PortableRemoteObject.narrow(ref, homeInterface);
+            final Class homeClass = m_home.getClass();
+            m_create = homeClass.getMethod("create", null);
+            m_ejbClass = m_create.getReturnType();
+            m_ejbName = ejb;
+        } catch (SecurityException e) {
+            throw new PicoIntrospectionException("Security Exception occured accessing create method of home interface of " + ejb, e);
+        } catch (NoSuchMethodException e) {
+            throw new PicoIntrospectionException("Home interface of " + ejb + " has no create method", e);
+        } catch (NamingException e) {
+            throw new PicoIntrospectionException("InitialContext has no EJB named " + ejb, e);
+        } catch (ClassCastException e) {
+            throw new PicoIntrospectionException("Home interface for " + ejb + " has wrong type.", e);
+        }
+    }
+
+    /**
+     * Instantiate the EJB.
+     * @see org.picocontainer.ComponentAdapter#getComponentInstance()
+     */
+    public Object getComponentInstance() throws PicoInitializationException, PicoIntrospectionException {
+        Object result = null;
+        try {
+            result = m_create.invoke(m_home, null);
+        } catch (IllegalArgumentException e) {
+            throw new PicoInitializationException(e);
+        } catch (IllegalAccessException e) {
+            throw new PicoInitializationException(e);
+        } catch (InvocationTargetException e) {
+            throw new PicoInitializationException(e);
+        }
+        return result;
+    }
+
+    /**
+     * This implementation has nothing to verify.
+     * @see org.picocontainer.ComponentAdapter#verify()
+     */
+    public void verify() throws UnsatisfiableDependenciesException {
+        // cannot do anything here
+    }
+
+    /**
+     * @return the name of the EJB.
+     * @see org.picocontainer.ComponentAdapter#getComponentKey()
+     */
+    public Object getComponentKey() {
+        return m_ejbName;
+    }
+
+    /**
+     * @see org.picocontainer.ComponentAdapter#getComponentImplementation()
+     */
+    public Class getComponentImplementation() {
+        return m_ejbClass;
+    }
+
+    /**
+     * @see org.picocontainer.ComponentAdapter#getContainer()
+     */
+    public PicoContainer getContainer() {
+        return m_picoContainer;
+    }
+
+    /**
+     * @see org.picocontainer.ComponentAdapter#setContainer(org.picocontainer.PicoContainer)
+     */
+    public void setContainer(final PicoContainer picoContainer) {
+        m_picoContainer = picoContainer;
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return getClass().getName() + "[" + getComponentKey() + "]";
+    }
+}
---
+++ trunk/java/ejb/src/java/org/nanocontainer/ejb/EJBClientComponentAdapterFactory.java
@@ -0,0 +1,79 @@
+/*****************************************************************************
+ * Copyright (c) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Joerg Schaible                                           *
+ *****************************************************************************/
+package org.nanocontainer.ejb;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.Parameter;
+import org.picocontainer.PicoIntrospectionException;
+import org.picocontainer.defaults.*;
+import org.picocontainer.defaults.AssignabilityRegistrationException;
+import org.picocontainer.defaults.ComponentAdapterFactory;
+import org.picocontainer.defaults.NotConcreteRegistrationException;
+
+/**
+ * {@link ComponentAdapterFactory} for EJB components.
+ * The instantiated components are cached for each {@link Thread}.
+ * @author J&ouml;rg Schaible
+ */
+public class EJBClientComponentAdapterFactory
+        implements ComponentAdapterFactory {
+
+    private final InitialContext m_initialContext;
+
+    /**
+     * Construct an EJBClientComponentAdapterFactory using the default {@link InitialContext}.
+     * @throws NamingException
+     */
+    public EJBClientComponentAdapterFactory() throws NamingException {
+        this(new InitialContext());
+    }
+
+    /**
+     * Construct an EJBClientComponentAdapterFactory using a special {@link InitialContext}.
+     * @param initialContext The InitialContext. 
+     */
+    public EJBClientComponentAdapterFactory(final InitialContext initialContext) {
+        super();
+        m_initialContext = initialContext;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see org.picocontainer.defaults.ComponentAdapterFactory#createComponentAdapter(java.lang.Object, java.lang.Class, org.picocontainer.Parameter[])
+     */
+    public ComponentAdapter createComponentAdapter(
+            Object componentKey, Class componentImplementation, Parameter[] parameters)
+            throws PicoIntrospectionException, AssignabilityRegistrationException,
+            NotConcreteRegistrationException {
+        return createComponentAdapter(componentKey.toString(), componentImplementation);
+    }
+
+    /**
+     * Creates a {@link ComponentAdapter} for EJB objects.
+     * @param componentKey The key used to lookup the {@link InitialContext}.
+     * @param componentImplementation The home interface.
+     * @see org.picocontainer.defaults.ComponentAdapterFactory#createComponentAdapter(java.lang.Object, java.lang.Class, org.picocontainer.Parameter[])
+     * @return Returns the created {@link ComponentAdapter}
+     * @throws PicoIntrospectionException Thrown if the home interface of the EJB could not instanciated.
+     * @throws AssignabilityRegistrationException Thrown if the <code>componentImplementation</code> does not extend {@link javax.ejb.EJBHome}.
+     * @throws NotConcreteRegistrationException Should not occur.
+     */
+    public ComponentAdapter createComponentAdapter(
+            String componentKey, Class componentImplementation)
+            throws PicoIntrospectionException, AssignabilityRegistrationException,
+            NotConcreteRegistrationException {
+        return new ThreadLocalComponentAdapter(
+                new EJBClientComponentAdapter(componentKey, componentImplementation, m_initialContext));
+    }
+
+}

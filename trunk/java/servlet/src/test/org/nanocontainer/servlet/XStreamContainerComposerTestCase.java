60a073a193587c3714248f8a42df1a10178dec40
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-05-16 12:55:51 +0000

    optimised imports

--- trunk/java/servlet/src/test/org/nanocontainer/servlet/XStreamContainerComposerTestCase.java
+++ trunk/java/servlet/src/test/org/nanocontainer/servlet/XStreamContainerComposerTestCase.java
@@ -10,11 +10,8 @@
 package org.nanocontainer.servlet;
 
 import junit.framework.TestCase;
-import org.jmock.C;
 import org.jmock.Mock;
-import org.picocontainer.PicoContainer;
 import org.picocontainer.defaults.DefaultPicoContainer;
-
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -22,34 +19,35 @@ import javax.servlet.http.HttpSession;
 
 /**
  * test case for XStreamContainerComposer
- * @author Konstantin Pribluda ( konstantin.pribluda[at]infodesire.com ) 
+ *
+ * @author Konstantin Pribluda ( konstantin.pribluda[at]infodesire.com )
  * @version $Revision$
  */
 public class XStreamContainerComposerTestCase extends TestCase implements KeyConstants {
-	
-	public void testThatProperConfigurationIsRead() throws Exception {
-		XStreamContainerComposer composer = new XStreamContainerComposer();
-		
-		DefaultPicoContainer application = new DefaultPicoContainer();
-		Mock servletContextMock = new Mock(ServletContext.class);
-		
-		composer.composeContainer(application,servletContextMock.proxy());
-		
-		assertNotNull(application.getComponentInstance("applicationScopedInstance"));
-		
-		DefaultPicoContainer session = new DefaultPicoContainer();
-		Mock httpSessionMock = new Mock(HttpSession.class);
-		
-		composer.composeContainer(session,httpSessionMock.proxy());
-		
-		assertNotNull(session.getComponentInstance("sessionScopedInstance"));
-		
-		DefaultPicoContainer request = new DefaultPicoContainer();
-		Mock httpRequestMock = new Mock(HttpServletRequest.class);
-		
-		composer.composeContainer(request,httpRequestMock.proxy());
-		
-		assertNotNull(request.getComponentInstance("requestScopedInstance"));
-	}
+
+    public void testThatProperConfigurationIsRead() throws Exception {
+        XStreamContainerComposer composer = new XStreamContainerComposer();
+
+        DefaultPicoContainer application = new DefaultPicoContainer();
+        Mock servletContextMock = new Mock(ServletContext.class);
+
+        composer.composeContainer(application, servletContextMock.proxy());
+
+        assertNotNull(application.getComponentInstance("applicationScopedInstance"));
+
+        DefaultPicoContainer session = new DefaultPicoContainer();
+        Mock httpSessionMock = new Mock(HttpSession.class);
+
+        composer.composeContainer(session, httpSessionMock.proxy());
+
+        assertNotNull(session.getComponentInstance("sessionScopedInstance"));
+
+        DefaultPicoContainer request = new DefaultPicoContainer();
+        Mock httpRequestMock = new Mock(HttpServletRequest.class);
+
+        composer.composeContainer(request, httpRequestMock.proxy());
+
+        assertNotNull(request.getComponentInstance("requestScopedInstance"));
+    }
 }
 

826b95e25abd9db3bec3a9901bedcd0ee2c8af3d
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-05-18 21:07:47 +0000

    Make snippet out of hierarchy example from 5 min. intro

--- java/picocontainer/src/test/org/picocontainer/doc/tutorial/interfaces/InterfacesTestCase.java
+++ java/picocontainer/src/test/org/picocontainer/doc/tutorial/interfaces/InterfacesTestCase.java
@@ -5,7 +5,7 @@ import org.picocontainer.MutablePicoContainer;
 import org.picocontainer.defaults.DefaultPicoContainer;
 
 public class InterfacesTestCase extends TestCase {
-    public void test() {
+    public void testKissing() {
         MutablePicoContainer pico = new DefaultPicoContainer();
         pico.registerComponentImplementation(Boy.class);
         pico.registerComponentImplementation(Girl.class);

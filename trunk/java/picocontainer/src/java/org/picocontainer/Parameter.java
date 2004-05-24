dac7d0f8a92dd2aadab9d95f7a0e6811f4b7aa3b
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-05-24 19:57:40 +0000

    Javadoc/jar upload location changes

--- trunk/java/picocontainer/src/java/org/picocontainer/Parameter.java
+++ trunk/java/picocontainer/src/java/org/picocontainer/Parameter.java
@@ -13,22 +13,19 @@ package org.picocontainer;
 /**
  * This class provides control over the arguments that will be passed to a constructor. It can be used for finer control
  * over what arguments are passed to a particular constructor.
- * 
+ *
  * @author Jon Tirs&eacute;n
  * @author Aslak Helles&oslash;y
  * @version $Revision$
- * @see MutablePicoContainer#registerComponentImplementation(Object,Class,Parameter[]) a method on the {@link
- *      MutablePicoContainer} interface which allows passing in of an array of <code>Parameter</code>s.
- * @see org.picocontainer.defaults.ComponentParameter an implementation of this interface that allows you to specify the
- *      key used for resolving the parameter.
- * @see org.picocontainer.defaults.ConstantParameter an implementation of this interface that allows you to specify a
- *      constant that will be used for resolving the parameter.
+ * @see MutablePicoContainer#registerComponentImplementation(Object,Class,Parameter[]) registerComponentImplementation
+ * @see org.picocontainer.defaults.ComponentParameter
+ * @see org.picocontainer.defaults.ConstantParameter
  * @since 1.0
  */
 public interface Parameter {
     /**
      * Retrieve the component adapter that should be used to find the instance to be passed in for this parameter.
-     * 
+     *
      * @param picoContainer the container from which dependencies are resolved.
      * @param expectedType  the type that the returned adapter needs to provide.
      * @return the component adapter that should be used to find the instance to be passed in for this parameter. Should

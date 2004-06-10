3ff98e91fd6386154b901d6b844bcaf1c2c5af9f
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-06-10 15:08:28 +0000

    more coverage work

---
+++ trunk/java/picocontainer/src/test/org/picocontainer/defaults/ImplementationHidingComponentAdapterTestCase.java
@@ -0,0 +1,66 @@
+package org.picocontainer.defaults;
+
+import junit.framework.TestCase;
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.PicoInitializationException;
+import org.picocontainer.PicoIntrospectionException;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.PicoVerificationException;
+
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+public class ImplementationHidingComponentAdapterTestCase extends TestCase {
+
+    public void testMultipleInterfacesCanBeHidden() {
+        ComponentAdapter ca = new ConstructorInjectionComponentAdapter(new Class[] {ActionListener.class, MouseListener.class},Footle.class);
+        ImplementationHidingComponentAdapter ihca = new ImplementationHidingComponentAdapter(ca);
+        Object comp = ihca.getComponentInstance();
+        assertNotNull(comp);
+        assertTrue(comp instanceof ActionListener);
+        assertTrue(comp instanceof MouseListener);
+    }
+
+    public void testNonInterfaceInArrayCantBeHidden() {
+        ComponentAdapter ca = new ConstructorInjectionComponentAdapter(new Class[] {String.class},Footle.class);
+        ImplementationHidingComponentAdapter ihca = new ImplementationHidingComponentAdapter(ca);
+        try {
+            Object comp = ihca.getComponentInstance();
+            fail("Oh no.");
+        } catch (PicoIntrospectionException e) {
+        }
+    }
+
+    public void testNonInterfaceCantBeHidden() {
+        ComponentAdapter ca = new ConstructorInjectionComponentAdapter("ww",Footle.class);
+        ImplementationHidingComponentAdapter ihca = new ImplementationHidingComponentAdapter(ca);
+        try {
+             Object comp = ihca.getComponentInstance();
+             fail("Oh no.");
+         } catch (PicoIntrospectionException e) {
+         }
+      }
+
+    class Footle implements ActionListener , MouseListener {
+        public void actionPerformed(ActionEvent e) {
+        }
+
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+        }
+
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+    }
+}

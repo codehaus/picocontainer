75b24c63003e620ee3b12b1233f4876f9e2c97c6
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-06-19 17:03:40 +0000

    start of nanostruts

---
+++ java/nanostruts/impl/src/test/org/nanocontainer/struts/PicoTilesRequestProcessorTestCase.java
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.nanocontainer.struts;
+
+import org.apache.struts.action.ActionMapping;
+import org.jmock.Mock;
+import org.jmock.cglib.MockObjectTestCase;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.defaults.DefaultPicoContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author Stephen Molitor
+ */
+public class PicoTilesRequestProcessorTestCase extends AbstractTestCase {
+
+    public void testProcessActionCreate() throws IOException {
+        PicoTilesRequestProcessor requestProcessor = new PicoTilesRequestProcessor();
+        MyAction action = (MyAction) requestProcessor.processActionCreate(request, response, mapping);
+        assertNotNull(action);
+        assertSame(dao, action.getDao());
+    }
+
+}
---
+++ java/nanostruts/sample/src/java/org/nanocontainer/sample/struts/Cheese.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.nanocontainer.sample.struts;
+
+/**
+ * @author Stephen Molitor
+ */
+public class Cheese {
+
+    private String name;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String toString() {
+        return "name: " + getName() + ", " + getCountry();
+    }
+
+}

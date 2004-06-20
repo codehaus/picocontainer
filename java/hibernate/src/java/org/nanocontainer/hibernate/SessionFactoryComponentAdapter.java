eb9a5bf2924cc6cbd26ae430c2479d38ff5a7e8a
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-06-20 01:56:58 +0000

    Made the code backwards compatible with PicoContainer 1.0

--- java/hibernate/src/java/org/nanocontainer/hibernate/SessionFactoryComponentAdapter.java
+++ java/hibernate/src/java/org/nanocontainer/hibernate/SessionFactoryComponentAdapter.java
@@ -9,95 +9,109 @@
  *****************************************************************************/
 package org.nanocontainer.hibernate;
 
-import org.picocontainer.PicoContainer;
-import org.picocontainer.Parameter;
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.SessionFactory;
+import net.sf.hibernate.cfg.Configuration;
 import org.picocontainer.ComponentAdapter;
-
+import org.picocontainer.Parameter;
 import org.picocontainer.PicoInitializationException;
 import org.picocontainer.PicoIntrospectionException;
 import org.picocontainer.PicoVerificationException;
-
+import org.picocontainer.PicoContainer;
+import org.picocontainer.defaults.ComponentParameter;
 import org.picocontainer.defaults.CyclicDependencyException;
 import org.picocontainer.defaults.UnsatisfiableDependenciesException;
-import org.picocontainer.defaults.ComponentParameter;
-import org.picocontainer.defaults.AbstractComponentAdapter;
 
-import net.sf.hibernate.SessionFactory;
-import net.sf.hibernate.cfg.Configuration;
-import net.sf.hibernate.HibernateException;
-
-import java.util.Collections;
-import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
  * component adapter providing  hibernate session factory.
+ *
  * @author konstantin pribluda
  * @version $Revision$
  */
-public class SessionFactoryComponentAdapter extends AbstractComponentAdapter {
-	
-	Parameter configurationParameter = null;
-	ComponentAdapter hibernateConfigurationAdapter = null;
-	boolean verifying = false;
-	
-	/**
-	 * construct adapter with net.sf.hibernate.SessionFactory.class as key 
-	 * and dependecy to net.sf.hibernate.cfg.Configuration.class
-	 */
-	public SessionFactoryComponentAdapter() {
-		this(SessionFactory.class,null);
-	}
-	
-	/**
-	 * construct component adapter with specified key and dependecy to 
-	 * net.sf.hibernate.cfg.Configuration.class
-	 */
-	public SessionFactoryComponentAdapter(Object componentKey) {
-		this(componentKey,null);
-	}
-	
-	/**
-	 * construct component adapter with given key and specified parameter.
-	 * in case null parameter is supplied use dependency to 
-	 * net.sf.hibernate.cfg.Configuration.class
-	 */
-	public SessionFactoryComponentAdapter(Object componentKey,Parameter parameter) {
-		super(componentKey,SessionFactory.class);
-		this.configurationParameter = parameter == null ? new ComponentParameter() : parameter;
-	}
-	
-	
-	/** 
-	 * obtain session factory instance if possible
-	 */
-	public Object getComponentInstance() throws   PicoInitializationException, PicoIntrospectionException {
-		verify();
-		try {
-		return ((Configuration)hibernateConfigurationAdapter.getComponentInstance()).buildSessionFactory();
-		} catch(HibernateException he) {
-			throw new PicoInitializationException(he);
-		}
-	}
+public class SessionFactoryComponentAdapter implements ComponentAdapter {
 
-	public void verify() throws PicoVerificationException {
-		try {
-			if(verifying) {
-				throw new CyclicDependencyException(new Class[] { Configuration.class } );
-			}
-			verifying = true;
-			HashSet unsatisfiableDependencies = new HashSet();
-			unsatisfiableDependencies.add(Configuration.class);
-			
-			hibernateConfigurationAdapter = configurationParameter.resolveAdapter(getContainer(),Configuration.class);
-			
-			if(hibernateConfigurationAdapter == null) {
-				throw new  UnsatisfiableDependenciesException(this,unsatisfiableDependencies);
-			}
+    private final Object componentKey;
+    private PicoContainer container;
+
+    private Parameter configurationParameter = null;
+    private ComponentAdapter hibernateConfigurationAdapter = null;
+    private boolean verifying = false;
+
+    /**
+     * construct adapter with net.sf.hibernate.SessionFactory.class as key
+     * and dependecy to net.sf.hibernate.cfg.Configuration.class
+     */
+    public SessionFactoryComponentAdapter() {
+        this(SessionFactory.class, null);
+    }
+
+    /**
+     * construct component adapter with specified key and dependecy to
+     * net.sf.hibernate.cfg.Configuration.class
+     */
+    public SessionFactoryComponentAdapter(Object componentKey) {
+        this(componentKey, null);
+    }
+
+    /**
+     * construct component adapter with given key and specified parameter.
+     * in case null parameter is supplied use dependency to
+     * net.sf.hibernate.cfg.Configuration.class
+     */
+    public SessionFactoryComponentAdapter(Object componentKey, Parameter parameter) {
+        this.componentKey = componentKey;
+        this.configurationParameter = parameter == null ? new ComponentParameter() : parameter;
+    }
 
 
-		} finally {
-			verifying = false;
-		}
-	}
+    /**
+     * obtain session factory instance if possible
+     */
+    public Object getComponentInstance() throws PicoInitializationException, PicoIntrospectionException {
+        verify();
+        try {
+            return ((Configuration) hibernateConfigurationAdapter.getComponentInstance()).buildSessionFactory();
+        } catch (HibernateException he) {
+            throw new PicoInitializationException(he);
+        }
+    }
+
+    public void verify() throws PicoVerificationException {
+        try {
+            if (verifying) {
+                throw new CyclicDependencyException(new Class[]{Configuration.class});
+            }
+            verifying = true;
+            HashSet unsatisfiableDependencies = new HashSet();
+            unsatisfiableDependencies.add(Configuration.class);
+
+            hibernateConfigurationAdapter = configurationParameter.resolveAdapter(getContainer(), Configuration.class);
+
+            if (hibernateConfigurationAdapter == null) {
+                throw new UnsatisfiableDependenciesException(this, unsatisfiableDependencies);
+            }
+
+
+        } finally {
+            verifying = false;
+        }
+    }
+
+    public Object getComponentKey() {
+        return componentKey;
+    }
+
+    public Class getComponentImplementation() {
+        return SessionFactory.class;
+    }
+
+    public void setContainer(PicoContainer container) {
+        this.container = container;
+    }
+
+    public PicoContainer getContainer() {
+        return container;
+    }
 }
--- java/hibernate/src/test/org/nanocontainer/hibernate/SessionComponentAdapterTestCase.java
+++ java/hibernate/src/test/org/nanocontainer/hibernate/SessionComponentAdapterTestCase.java
@@ -9,90 +9,83 @@
  *****************************************************************************/
 package org.nanocontainer.hibernate;
 
-import net.sf.hibernate.Session;
-import net.sf.hibernate.SessionFactory;
-import net.sf.hibernate.Transaction;
-import net.sf.hibernate.cfg.Configuration;
-import net.sf.hibernate.tool.hbm2ddl.SchemaExport;
-
-
 import junit.framework.TestCase;
-
-import org.picocontainer.defaults.DefaultPicoContainer;
-import org.picocontainer.defaults.ConstantParameter;
+import net.sf.hibernate.Session;
+import net.sf.hibernate.cfg.Configuration;
 import org.picocontainer.defaults.ComponentParameter;
+import org.picocontainer.defaults.DefaultPicoContainer;
 
 /**
  * @author Konstantin Pribluda
  * @version $Revision$
  */
 public class SessionComponentAdapterTestCase extends TestCase {
-	
-	public void testDeafulAdapterConstruction() throws Exception {
-		SessionComponentAdapter adapter = new SessionComponentAdapter();
-		assertEquals(Session.class,adapter.getComponentKey());
-		assertEquals(Session.class,adapter.getComponentImplementation());
-	}
-	
-	
-	/**
-	* test that component adapter gets proper key. 
-	*/ 
-	public void testKeyAdapterConstruction() throws Exception {
-		SessionComponentAdapter adapter =  new SessionComponentAdapter("blurge");
-		assertEquals("blurge",adapter.getComponentKey());
-	}
 
-	/**
-	 * test that default parameter instantiuation finds dependency
-	 */
-	public void testInstantiationWithDefaultParams()  throws Exception {
-		DefaultPicoContainer container = new DefaultPicoContainer();
-		
-		container.registerComponentImplementation(Configuration.class,ConstructableConfiguration.class);
-		container.registerComponent(new SessionFactoryComponentAdapter());
-		
-		container.registerComponent(new SessionComponentAdapter());
-		
-		Session session = (Session)container.getComponentInstanceOfType(Session.class);
-		assertNotNull(session);
-		
-	}
-	
-	
-	/**
-	 * test that keyed instantiation works properly
-	 */ 
-	public void testInstantiationByKey() throws Exception {
-		DefaultPicoContainer container = new DefaultPicoContainer();
-		
-		container.registerComponentImplementation(Configuration.class,ConstructableConfiguration.class);
-		container.registerComponent(new SessionFactoryComponentAdapter());
-		container.registerComponent(new SessionComponentAdapter("glarch"));
-		
-		Session session = (Session) container.getComponentInstance("glarch");
-		assertNotNull(session);
-		
-	}
-	
-	/**
-	 * test that we can find our session factory by key and by class
-	 */ 
-	public void testDependencyByKey() throws Exception {
-		DefaultPicoContainer container = new DefaultPicoContainer();
-		
-		container.registerComponentImplementation(Configuration.class,ConstructableConfiguration.class);
-		container.registerComponent(new SessionFactoryComponentAdapter("blurge"));
-		
-		container.registerComponent(new SessionComponentAdapter("session",new ComponentParameter("blurge")));
-		
-		Session session = (Session) container.getComponentInstance("session");
-		assertNotNull(session);
-		
-		
-		session = (Session)container.getComponentInstanceOfType(Session.class);
-		assertNotNull(session);
-	}
+    public void testDefaultAdapterConstruction() throws Exception {
+        SessionComponentAdapter adapter = new SessionComponentAdapter();
+        assertEquals(Session.class, adapter.getComponentKey());
+        assertEquals(Session.class, adapter.getComponentImplementation());
+    }
+
+
+    /**
+     * test that component adapter gets proper key.
+     */
+    public void testKeyAdapterConstruction() throws Exception {
+        SessionComponentAdapter adapter = new SessionComponentAdapter("blurge");
+        assertEquals("blurge", adapter.getComponentKey());
+    }
+
+    /**
+     * test that default parameter instantiuation finds dependency
+     */
+    public void testInstantiationWithDefaultParams() throws Exception {
+        DefaultPicoContainer container = new DefaultPicoContainer();
+
+        container.registerComponentImplementation(Configuration.class, ConstructableConfiguration.class);
+        container.registerComponent(new SessionFactoryComponentAdapter());
+
+        container.registerComponent(new SessionComponentAdapter());
+
+        Session session = (Session) container.getComponentInstanceOfType(Session.class);
+        assertNotNull(session);
+
+    }
+
+
+    /**
+     * test that keyed instantiation works properly
+     */
+    public void testInstantiationByKey() throws Exception {
+        DefaultPicoContainer container = new DefaultPicoContainer();
+
+        container.registerComponentImplementation(Configuration.class, ConstructableConfiguration.class);
+        container.registerComponent(new SessionFactoryComponentAdapter());
+        container.registerComponent(new SessionComponentAdapter("glarch"));
+
+        Session session = (Session) container.getComponentInstance("glarch");
+        assertNotNull(session);
+
+    }
+
+    /**
+     * test that we can find our session factory by key and by class
+     */
+    public void testDependencyByKey() throws Exception {
+        DefaultPicoContainer container = new DefaultPicoContainer();
+
+        container.registerComponentImplementation(Configuration.class, ConstructableConfiguration.class);
+        container.registerComponent(new SessionFactoryComponentAdapter("blurge"));
+
+        container.registerComponent(new SessionComponentAdapter("session", new ComponentParameter("blurge")));
+
+        Session session = (Session) container.getComponentInstance("session");
+        assertNotNull(session);
+
+
+        session = (Session) container.getComponentInstanceOfType(Session.class);
+        assertNotNull(session);
+    }
 
 }
 

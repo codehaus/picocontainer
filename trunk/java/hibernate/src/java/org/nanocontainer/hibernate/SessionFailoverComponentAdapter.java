4d7d93de037074daf7d123c7586ec75e54a7c17d
ko5tik <ko5tik@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-06-25 14:54:18 +0000

    finished session failover component adapter

--- trunk/java/hibernate/src/java/org/nanocontainer/hibernate/SessionFailoverComponentAdapter.java
+++ trunk/java/hibernate/src/java/org/nanocontainer/hibernate/SessionFailoverComponentAdapter.java
@@ -10,12 +10,18 @@
 package org.nanocontainer.hibernate;
 
 import net.sf.hibernate.Session;
-import net.sf.hibernate.Transaction;
+import net.sf.hibernate.HibernateException;
 
 import org.picocontainer.defaults.DecoratingComponentAdapter;
 
 import org.picocontainer.PicoInitializationException;
 import org.picocontainer.PicoIntrospectionException;
+
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+
 
 /** 
  * component adapter providing transparent session restart and management
@@ -29,9 +35,8 @@ import org.picocontainer.PicoIntrospectionException;
 public class SessionFailoverComponentAdapter extends DecoratingComponentAdapter  {
 
 	Session sessionProxy;
-	Transaction transactionProxy;
 	
-	public SessionFailoverComponentAdapter(SessionFactoryComponentAdapter sfca) {
+	public SessionFailoverComponentAdapter(SessionComponentAdapter sfca) {
 		super(sfca);
 	}
 	
@@ -41,10 +46,37 @@ public class SessionFailoverComponentAdapter extends DecoratingComponentAdapter 
 	 */
 	public Object getComponentInstance() throws PicoInitializationException, PicoIntrospectionException {
 		if(sessionProxy == null) {
+			final Session session = (Session)super.getComponentInstance();
 			
+			sessionProxy = (Session) Proxy.newProxyInstance(Session.class.getClassLoader(),
+				new Class[] { Session.class },
+				new InvocationHandler() {
+					Session target = session;
+					
+					public Object invoke(Object proxy,
+						Method method,Object[] args) throws Throwable {
+						try {
+							if(method.getName().equals("close")) {
+								sessionProxy = null;
+							}
+							return method.invoke(target,args);
+						} catch(InvocationTargetException ite) {
+							// we got hibernate exception kill this session
+							if(ite.getCause() instanceof HibernateException)  {
+								target.clear();
+								target.close();
+								sessionProxy = null;
+							}
+							throw ite;
+						}
+					}
+				});
+				
 		}
 		
 		return sessionProxy;
 	}
+	
+	
 }
 

35c9426eba84df94a57bc240c51a126fb7d67070
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-10 18:16:22 +0000

    diff container and correct starting

--- java/microcontainer/src/bootstrap/microcontainerboot.groovy
+++ java/microcontainer/src/bootstrap/microcontainerboot.groovy
@@ -2,12 +2,11 @@ import org.nanocontainer.script.groovy.NanoGroovyBuilder
 
 builder = new NanoGroovyBuilder()
 
-root = builder.container(parent:parent,class:ImplementationHidingSoftCompositionPicoContainer) {
+pico = builder.container(parent:parent,class:ImplementationHidingSoftCompositionPicoContainer) {
   classpathelement(path:"lib/hidden/microcontainer-impl-0.1-SNAPSHOT.jar");
   component(key:org.microcontainer.Kernel, class:"org.microcontainer.impl.DefaultKernel")
   component(key:org.microcontainer.McaDeployer, class:"org.microcontainer.impl.DefaultMcaDeployer")  
 }
-root.start()
-//org.microcontainer.ResidualController rc = new org.microcontainer.ResidualController(root);
-//rc.start();
+org.microcontainer.ResidualController rc = new org.microcontainer.ResidualController(pico);
+rc.start();
 

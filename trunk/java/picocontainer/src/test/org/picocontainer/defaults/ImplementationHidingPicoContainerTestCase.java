91b3e7831666ddf30b3feeb121c431af734fb098
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-10 11:03:33 +0000

    abstract split

--- trunk/java/picocontainer/src/test/org/picocontainer/defaults/ImplementationHidingPicoContainerTestCase.java
+++ trunk/java/picocontainer/src/test/org/picocontainer/defaults/ImplementationHidingPicoContainerTestCase.java
@@ -8,95 +8,18 @@
  *****************************************************************************/
 package org.picocontainer.defaults;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.io.IOException;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-import org.picocontainer.tck.AbstractPicoContainerTestCase;
 import org.picocontainer.MutablePicoContainer;
 import org.picocontainer.PicoContainer;
-import org.picocontainer.PicoException;
-import org.picocontainer.PicoInitializationException;
-import org.picocontainer.Parameter;
-import org.picocontainer.PicoIntrospectionException;
-import org.picocontainer.ComponentAdapter;
 
-public class ImplementationHidingPicoContainerTestCase extends AbstractPicoContainerTestCase {
+public class ImplementationHidingPicoContainerTestCase extends AbstractImplementationHidingPicoContainerTestCase {
+
+    protected MutablePicoContainer createImplementationHidingPicoContainer() {
+        return new ImplementationHidingPicoContainer();
+    }
 
     protected MutablePicoContainer createPicoContainer(PicoContainer parent) {
         return new ImplementationHidingPicoContainer(parent);
     }
 
-    public void testInstanceIsNotAutomaticallyHidden() {
-        ImplementationHidingPicoContainer pc = new ImplementationHidingPicoContainer();
-        pc.registerComponentInstance(Map.class, new HashMap());
-        Map map = (Map) pc.getComponentInstance(Map.class);
-        assertNotNull(map);
-        assertTrue(map instanceof HashMap);
-    }
-
-    public void testImplementaionIsAutomaticallyHidden() {
-        ImplementationHidingPicoContainer pc = new ImplementationHidingPicoContainer();
-        pc.registerComponentImplementation(Map.class, HashMap.class);
-        Map map = (Map) pc.getComponentInstance(Map.class);
-        assertNotNull(map);
-        assertFalse(map instanceof HashMap);
-    }
-
-    public void testNonInterfaceImplementaionIsAutomaticallyHidden() {
-        ImplementationHidingPicoContainer pc = new ImplementationHidingPicoContainer();
-        pc.registerComponentImplementation(HashMap.class, HashMap.class);
-        Map map = (Map) pc.getComponentInstance(HashMap.class);
-        assertNotNull(map);
-        assertTrue(map instanceof HashMap);
-    }
-
-    public void testNonInterfaceImplementaionWithParametersIsAutomaticallyHidden() {
-        ImplementationHidingPicoContainer pc = new ImplementationHidingPicoContainer();
-        pc.registerComponentImplementation(HashMap.class, HashMap.class, new Parameter[0]);
-        Map map = (Map) pc.getComponentInstance(HashMap.class);
-        assertNotNull(map);
-        assertTrue(map instanceof HashMap);
-    }
-
-
-    public void testImplementaionWithParametersIsAutomaticallyHidden() {
-        ImplementationHidingPicoContainer pc = new ImplementationHidingPicoContainer();
-        pc.registerComponentImplementation(Map.class, HashMap.class, new Parameter[0]);
-        Map map = (Map) pc.getComponentInstance(Map.class);
-        assertNotNull(map);
-        assertFalse(map instanceof HashMap);
-    }
-
-    public void testSerializedContainerCanRetrieveImplementation() throws PicoException, PicoInitializationException,
-            IOException, ClassNotFoundException {
-        try {
-            super.testSerializedContainerCanRetrieveImplementation();
-            fail("The ImplementationHidingPicoContainer should not be able to retrieve the component impl");
-        } catch (ClassCastException cce) {
-            // expected.
-        }
-    }
-
-    public void testExceptionThrowingFromHiddenComponent() {
-        ImplementationHidingPicoContainer pc = new ImplementationHidingPicoContainer();
-        pc.registerComponentImplementation(ActionListener.class, Burp.class);
-        try {
-            ActionListener ac = (ActionListener) pc.getComponentInstance(ActionListener.class);
-            ac.actionPerformed(null);
-            fail("Oh no.");
-        } catch (RuntimeException e) {
-            assertEquals("woohoo", e.getMessage());
-        }
-    }
-
-    public static class Burp implements ActionListener {
-
-        public void actionPerformed(ActionEvent e) {
-            throw new RuntimeException("woohoo");
-        }
-    }
 
 }

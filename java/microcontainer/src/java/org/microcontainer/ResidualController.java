e52a26d55df8c5580bf83e5cfb5f58386c9f8f35
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-14 19:04:16 +0000

    copyright

--- java/microcontainer/src/java/org/microcontainer/ResidualController.java
+++ java/microcontainer/src/java/org/microcontainer/ResidualController.java
@@ -1,3 +1,13 @@
+/*****************************************************************************
+ * Copyright (C) MicroContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Paul Hammant                                             *
+ *****************************************************************************/
+
 package org.microcontainer;
 
 import org.picocontainer.Startable;

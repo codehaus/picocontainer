61c4090b13b465741a6d420e11b51a1a7fa42c43
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-14 20:20:16 +0000

    BeanShell console

--- trunk/java/microcontainer/src/bootstrap/microcontainerboot.groovy
+++ trunk/java/microcontainer/src/bootstrap/microcontainerboot.groovy
@@ -6,7 +6,9 @@ pico = builder.container(parent:parent,class:ImplementationHidingSoftComposition
   classpathelement(path:"lib/hidden/microcontainer-impl-0.1-SNAPSHOT.jar");
   component(key:org.microcontainer.Kernel, class:"org.microcontainer.impl.DefaultKernel")
   component(key:org.microcontainer.McaDeployer, class:"org.microcontainer.impl.DefaultMcaDeployer")  
+  component(class:"org.microcontainer.impl.MicroBeanShellConsole")
 }
+pico.start();
 org.microcontainer.ResidualController rc = new org.microcontainer.ResidualController(pico);
 rc.start();
 

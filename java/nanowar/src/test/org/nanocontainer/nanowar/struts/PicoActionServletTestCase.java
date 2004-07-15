114b3b1ec964bc4483c63d50887c046458abe11f
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-15 08:13:48 +0000

    Start of NanoWar I

---
+++ java/nanowar/src/test/org/nanocontainer/nanowar/struts/PicoActionServletTestCase.java
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.nanocontainer.nanowar.struts;
+
+import org.nanocontainer.nanowar.struts.PicoActionServlet;
+import org.nanocontainer.nanowar.struts.AbstractTestCase;
+import org.nanocontainer.nanowar.struts.MyAction;
+
+import java.io.IOException;
+
+/**
+ * @author Stephen Molitor
+ */
+public class PicoActionServletTestCase extends AbstractTestCase {
+
+    public void testProcessActionCreate() throws IOException {
+        PicoActionServlet servlet = new PicoActionServlet();
+        MyAction action = (MyAction) servlet.processActionCreate(mapping, request);
+        assertNotNull(action);
+        assertSame(dao, action.getDao());
+        assertSame(servlet, action.getServlet());
+    }
+
+}

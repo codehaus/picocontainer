ca172a84b541beb147e74ad266d387733ccde42a
steve_molitor <steve_molitor@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-18 00:50:27 +0000

    Changed ActionFactory to only instantiate the actions container once per request. Thus actions will only get instantiated once per request (a good thing). Without this fix there could be subtle issues in requests that forward back to the same action.
    Also, renamed KeyConstants.ACTION_CONTAINER to ACTIONS_CONTAINER
    for consistency with the variable names elsewhere for this container.
    
    Finally, various formatting changes.

--- java/nanowar/src/java/org/nanocontainer/nanowar/struts/ActionFactory.java
+++ java/nanowar/src/java/org/nanocontainer/nanowar/struts/ActionFactory.java
@@ -8,6 +8,8 @@
  *****************************************************************************/
 package org.nanocontainer.nanowar.struts;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionServlet;
@@ -15,49 +17,47 @@ import org.picocontainer.MutablePicoContainer;
 import org.picocontainer.PicoInitializationException;
 import org.picocontainer.PicoIntrospectionException;
 import org.picocontainer.defaults.DefaultPicoContainer;
-import org.nanocontainer.nanowar.struts.KeyConstants;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
- * Uses Pico to produce Actions and inject dependencies into them.  This class does the bulk of the
- * work in nano-struts.  If you have your own <code>RequestProcessor</code> implementation, you can use
- * an <code>ActionFactory</code> in your <code>RequestProcessor.processActionCreate</code> method to
- * Picofy your Actions.
- *
+ * Uses Pico to produce Actions and inject dependencies into them. This class
+ * does the bulk of the work in nano-struts. If you have your own
+ * <code>RequestProcessor</code> implementation, you can use an
+ * <code>ActionFactory</code> in your
+ * <code>RequestProcessor.processActionCreate</code> method to Picofy your
+ * Actions.
+ * 
  * @author Stephen Molitor
  */
 public class ActionFactory {
 
     /**
-     * Gets the <code>Action</code> specified by the mapping type from
-     * a Pico container.  The action will be instantiated if
-     * necessary, and its dependencies will be injected.  The action
-     * will be instantiated via a special Pico container that just
-     * contains actions.  If this container already exists in the
-     * request attribute, this method will use it.  If no such
-     * container exists, this method will create a new Pico container
-     * and place it in the request.  The parent container will either
-     * be the request container, or if that container can not be found
-     * the session container, or if that container can not be found,
-     * the application container.  If no parent container can be
-     * found, a <code>PicoInitializationException</code> will be
-     * thrown.
-     * <p/>
-     * The action path specified in the mapping is used as the
-     * component key for the action.
-     *
-     * @param request the HTTP servlet request.
-     * @param mapping the Struts mapping object.  The type property tells us what Action class is required.
-     * @param servlet the Struts <code>ActionServlet.  The action is configured with this servlet.
+     * Gets the <code>Action</code> specified by the mapping type from a Pico
+     * container. The action will be instantiated if necessary, and its
+     * dependencies will be injected. The action will be instantiated via a
+     * special Pico container that just contains actions. If this container
+     * already exists in the request attribute, this method will use it. If no
+     * such container exists, this method will create a new Pico container and
+     * place it in the request. The parent container will either be the request
+     * container, or if that container can not be found the session container,
+     * or if that container can not be found, the application container. If no
+     * parent container can be found, a <code>PicoInitializationException</code>
+     * will be thrown. <p/>The action path specified in the mapping is used as
+     * the component key for the action.
+     * 
+     * @param request
+     *            the HTTP servlet request.
+     * @param mapping
+     *            the Struts mapping object. The type property tells us what
+     *            Action class is required.
+     * @param servlet
+     *            the Struts
+     *            <code>ActionServlet.  The action is configured with this servlet.
      * @return the <code>Action</code> instance.
      * @throws PicoIntrospectionException  if the mapping type does not specify a valid action.
      * @throws PicoInitializationException if no request, session, or application scoped Pico container
      *                                     can be found.
      */
-    public Action getAction(HttpServletRequest request,
-                            ActionMapping mapping,
-                            ActionServlet servlet)
+    public Action getAction(HttpServletRequest request, ActionMapping mapping, ActionServlet servlet)
             throws PicoIntrospectionException, PicoInitializationException {
 
         MutablePicoContainer actionsContainer = getActionsContainer(request);
@@ -75,10 +75,10 @@ public class ActionFactory {
     }
 
     private MutablePicoContainer getActionsContainer(HttpServletRequest request) {
-        MutablePicoContainer actionsContainer = (MutablePicoContainer)
-                request.getAttribute(KeyConstants.ACTION_CONTAINER);
+        MutablePicoContainer actionsContainer = (MutablePicoContainer) request.getAttribute(KeyConstants.ACTIONS_CONTAINER);
         if (actionsContainer == null) {
             actionsContainer = new DefaultPicoContainer(getNanoServletContainer(request));
+            request.setAttribute(KeyConstants.ACTIONS_CONTAINER, actionsContainer);
         }
         return actionsContainer;
     }
@@ -89,11 +89,12 @@ public class ActionFactory {
             container = (MutablePicoContainer) request.getSession().getAttribute(KeyConstants.SESSION_CONTAINER);
         }
         if (container == null) {
-            container = (MutablePicoContainer) request.getSession().getServletContext().getAttribute(KeyConstants.APPLICATION_CONTAINER);
+            container = (MutablePicoContainer) request.getSession().getServletContext().getAttribute(
+                    KeyConstants.APPLICATION_CONTAINER);
         }
         if (container == null) {
-            throw new PicoInitializationException("Could not find request, session or application container.  " +
-                    "Please make sure you have nano-servlet configured properly");
+            throw new PicoInitializationException("Could not find request, session or application container.  "
+                    + "Please make sure you have nano-servlet configured properly");
         }
         return container;
     }
@@ -102,8 +103,8 @@ public class ActionFactory {
         try {
             return Class.forName(className);
         } catch (ClassNotFoundException e) {
-            throw new PicoIntrospectionException("Action class + '" + className + "' not found.  " +
-                    "Check the spelling of the 'type' element of the action mapping.");
+            throw new PicoIntrospectionException("Action class + '" + className + "' not found.  "
+                    + "Check the spelling of the 'type' element of the action mapping.");
         }
     }
 

716d494e08b37268ea818ffa6daa3136c0b83763
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-18 07:18:40 +0000

    mauro's patch for commons cli - missing class

---
+++ java/nanocontainer/src/test/org/nanocontainer/StandaloneTestCase.java
@@ -0,0 +1,75 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+
+package org.nanocontainer;
+
+import junit.framework.TestCase;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+
+
+/**
+ * @author Mauro Talevi
+ */
+public class StandaloneTestCase extends TestCase {
+
+    public void testCommandLineWithNoArgs() throws Exception {
+        try {
+            Standalone.getCommandLine(new String[]{});
+        } catch ( ParseException e ){
+            assertEquals("Exception message", "No arguments specified", e.getMessage() );
+        }
+    }
+    
+    public void testCommandLineWithHelp() throws Exception {
+        CommandLine cl = Standalone.getCommandLine(new String[]{"-h"});
+        assertTrue(cl.hasOption('h'));
+        assertFalse(cl.hasOption('v'));
+        assertNull(cl.getOptionValue('c'));
+        assertFalse(cl.hasOption('q'));
+        assertFalse(cl.hasOption('n'));
+    }
+
+    public void testCommandLineWithVersion() throws Exception {
+        CommandLine cl = Standalone.getCommandLine(new String[]{"-v"});
+        assertFalse(cl.hasOption('h'));
+        assertTrue(cl.hasOption('v'));
+        assertNull(cl.getOptionValue('c'));
+        assertFalse(cl.hasOption('q'));
+        assertFalse(cl.hasOption('n'));
+    }
+    
+    public void testCommandLineWithCompostion() throws Exception {
+        CommandLine cl = Standalone.getCommandLine(new String[]{"-cpath"});
+        assertFalse(cl.hasOption('h'));
+        assertFalse(cl.hasOption('v'));
+        assertEquals("path",cl.getOptionValue('c'));
+        assertFalse(cl.hasOption('q'));
+        assertFalse(cl.hasOption('n'));
+    }
+
+    public void testCommandLineWithCompositionAndQuiet() throws Exception {
+        CommandLine cl = Standalone.getCommandLine(new String[]{"-cpath","-q"});
+        assertFalse(cl.hasOption('h'));
+        assertFalse(cl.hasOption('v'));
+        assertEquals("path",cl.getOptionValue('c'));
+        assertTrue(cl.hasOption('q'));
+        assertFalse(cl.hasOption('n'));
+    }
+
+    public void testCommandLineWithCompositionAndQuietAndNowait() throws Exception {
+        CommandLine cl = Standalone.getCommandLine(new String[]{"-cpath","-q","-n"});
+        assertFalse(cl.hasOption('h'));
+        assertFalse(cl.hasOption('v'));
+        assertEquals("path",cl.getOptionValue('c'));
+        assertTrue(cl.hasOption('q'));
+        assertTrue(cl.hasOption('n'));
+    }
+    
+}

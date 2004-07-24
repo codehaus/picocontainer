1b7e949e44b9acfb06d6f2c36480e9defe9c6d49
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-24 11:49:33 +0000

    Fix format.

--- trunk/java/ejb/src/test/org/nanocontainer/ejb/rmi/mock/EJBHomeMock.java
+++ trunk/java/ejb/src/test/org/nanocontainer/ejb/rmi/mock/EJBHomeMock.java
@@ -1,24 +1,14 @@
 /*****************************************************************************
-
  * Copyright (c) PicoContainer Organization. All rights reserved.            *
-
  * ------------------------------------------------------------------------- *
-
  * The software in this package is published under the terms of the BSD      *
-
  * style license a copy of which has been included with this distribution in *
-
  * the LICENSE.txt file.                                                     *
-
  *                                                                           *
-
  * Original code by Joerg Schaible                                           *
-
  *****************************************************************************/
 
 package org.nanocontainer.ejb.rmi.mock;
-
-
 
 import javax.ejb.EJBHome;
 import javax.ejb.EJBMetaData;
@@ -26,76 +16,41 @@ import javax.ejb.Handle;
 import javax.ejb.HomeHandle;
 
 
-
 /**
-
  * Mock object for an EJBHome interface.
-
  * @author J&ouml;rg Schaible
-
  */
 
 public class EJBHomeMock
-
         implements EJBHome {
 
-
-
     /**
-
      * @see javax.ejb.EJBHome#remove(java.lang.Object)
-
      */
-
-    public void remove(Object arg0) /* throws RemoteException, RemoveException */ {
-
+    public void remove(Object arg0) /* throws RemoteException, RemoveException */{
         // do nothing
-
     }
 
-
-
     /**
-
      * @see javax.ejb.EJBHome#getEJBMetaData()
-
      */
-
-    public EJBMetaData getEJBMetaData() /* throws RemoteException */ {
-
+    public EJBMetaData getEJBMetaData() /* throws RemoteException */{
         return null;
-
     }
 
-
-
     /**
-
      * @see javax.ejb.EJBHome#remove(javax.ejb.Handle)
-
      */
-
-    public void remove(Handle arg0) /* throws RemoteException, RemoveException */ {
-
+    public void remove(Handle arg0) /* throws RemoteException, RemoveException */{
         // do nothing
-
     }
-
-
 
     /**
-
      * @see javax.ejb.EJBHome#getHomeHandle()
-
      */
-
-    public HomeHandle getHomeHandle() /* throws RemoteException */ {
-
+    public HomeHandle getHomeHandle() /* throws RemoteException */{
         return null;
-
     }
-
-
 
 }
 
--- trunk/java/ejb/src/test/org/nanocontainer/ejb/rmi/mock/PortableRemoteObjectDelegateMock.java
+++ trunk/java/ejb/src/test/org/nanocontainer/ejb/rmi/mock/PortableRemoteObjectDelegateMock.java
@@ -1,24 +1,14 @@
 /*****************************************************************************
-
  * Copyright (c) PicoContainer Organization. All rights reserved.            *
-
  * ------------------------------------------------------------------------- *
-
  * The software in this package is published under the terms of the BSD      *
-
  * style license a copy of which has been included with this distribution in *
-
  * the LICENSE.txt file.                                                     *
-
  *                                                                           *
-
  * Original code by Joerg Schaible                                           *
-
  *****************************************************************************/
 
 package org.nanocontainer.ejb.rmi.mock;
-
-
 
 import javax.ejb.EJBHome;
 import javax.rmi.CORBA.PortableRemoteObjectDelegate;
@@ -29,176 +19,96 @@ import java.rmi.Remote;
 import java.util.Map;
 
 
-
 /**
-
- * Mock class for a PortableRemoteObjectDelegate.
-
- * This class substitutes the implementation of the JDK narrowing an EJB home interface.
-
- * The class is registered in a file orb.properties located in the user.home directory.
-
- * For unit tests just set user.home to the proper directory.
-
+ * Mock class for a PortableRemoteObjectDelegate. This class substitutes the
+ * implementation of the JDK narrowing an EJB home interface. The class is
+ * registered in a file orb.properties located in the user.home directory. For
+ * unit tests just set user.home to the proper directory.
  * @author J&ouml;rg Schaible
-
  */
 
 public class PortableRemoteObjectDelegateMock
-
         implements PortableRemoteObjectDelegate {
-
-    
 
     static private Map s_narrowMap = null;
 
-    
-
     /**
-
-     * Set the narrow {@link Map}.
-
-     * The key is the looked-up element, the value the constructor of the home interface.
-
-     * @param narrowMap The map with the lookup values 
-
+     * Set the narrow {@link Map}. The key is the looked-up element, the value
+     * the constructor of the home interface.
+     * @param narrowMap The map with the lookup values
      * @since elsag-test 0.1
-
      */
-
     public static void setNarrowMap(final Map narrowMap) {
-
         s_narrowMap = narrowMap;
-
     }
 
-
-
     /**
-
      * Constructs a PortableRemoteObjectDelegateMock
-
      */
-
     public PortableRemoteObjectDelegateMock() {
-
         super();
-
     }
 
-
-
     /**
-
      * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#exportObject(java.rmi.Remote)
-
      */
-
-    public void exportObject(Remote obj) /*throws RemoteException*/ {
-
+    public void exportObject(Remote obj) /* throws RemoteException */{
         return;
-
     }
 
-
-
     /**
-
      * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#unexportObject(java.rmi.Remote)
-
      */
-
-    public void unexportObject(Remote obj) /*throws NoSuchObjectException*/ {
-
+    public void unexportObject(Remote obj) /* throws NoSuchObjectException */{
         return;
-
     }
 
-
-
     /**
-
      * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#toStub(java.rmi.Remote)
-
      */
-
-    public Remote toStub(Remote obj) /*throws NoSuchObjectException*/ {
-
+    public Remote toStub(Remote obj) /* throws NoSuchObjectException */{
         return null;
-
     }
 
-
-
     /**
-
-     * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#connect(java.rmi.Remote, java.rmi.Remote)
-
+     * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#connect(java.rmi.Remote,
+     *      java.rmi.Remote)
      */
-
-    public void connect(Remote target, Remote source) /*throws RemoteException*/ {
-
+    public void connect(Remote target, Remote source) /* throws RemoteException */{
         return;
-
     }
 
-
-
     /**
-
-     * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#narrow(java.lang.Object, java.lang.Class)
-
+     * @see javax.rmi.CORBA.PortableRemoteObjectDelegate#narrow(java.lang.Object,
+     *      java.lang.Class)
      */
-
     public Object narrow(Object narrowFrom, Class narrowTo) throws ClassCastException {
-
         final Constructor ctor = (Constructor)s_narrowMap.get(narrowFrom);
-
         if (ctor != null) {
-
-            final ClassCastException cce = new ClassCastException(ctor.getDeclaringClass().getName());
-
+            final ClassCastException cce = new ClassCastException(ctor.getDeclaringClass()
+                    .getName());
             try {
-
                 if (!Modifier.isInterface(narrowTo.getModifiers())) {
-
-                    cce.initCause(new IllegalArgumentException(narrowTo.getName() + " should be an interface"));
-
+                    cce.initCause(new IllegalArgumentException(narrowTo.getName()
+                            + " should be an interface"));
                 } else if (EJBHome.class.isAssignableFrom(narrowTo)
-
                         && narrowTo.isAssignableFrom(ctor.getDeclaringClass())) {
-
                     return ctor.newInstance(null);
-
                 }
-
             } catch (IllegalArgumentException e) {
-
                 cce.initCause(e);
-
             } catch (InstantiationException e) {
-
                 cce.initCause(e);
-
             } catch (IllegalAccessException e) {
-
                 cce.initCause(e);
-
             } catch (InvocationTargetException e) {
-
                 cce.initCause(e);
-
             }
 
             throw cce;
-
         }
 
         return null;
-
     }
-
-
-
 }
 

a807d1b3a98160f60ed0908027e7972af22e1167
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-07-24 11:49:33 +0000

    Fix format.

--- java/ejb/src/test/org/nanocontainer/ejb/rmi/test/PortableRemoteObjectDelegateMockTest.java
+++ java/ejb/src/test/org/nanocontainer/ejb/rmi/test/PortableRemoteObjectDelegateMockTest.java
@@ -1,24 +1,14 @@
 /*****************************************************************************
-
  * Copyright (c) PicoContainer Organization. All rights reserved.            *
-
  * ------------------------------------------------------------------------- *
-
  * The software in this package is published under the terms of the BSD      *
-
  * style license a copy of which has been included with this distribution in *
-
  * the LICENSE.txt file.                                                     *
-
  *                                                                           *
-
  * Original code by Joerg Schaible                                           *
-
  *****************************************************************************/
 
 package org.nanocontainer.ejb.rmi.test;
-
-
 
 import junit.framework.TestCase;
 import org.nanocontainer.ejb.rmi.mock.EJBHomeMock;
@@ -31,188 +21,119 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-
 /**
-
  * Test for the Mock :)
-
  * @author J&ouml;rg Schaible
-
  */
-
 public class PortableRemoteObjectDelegateMockTest
-
         extends TestCase {
 
-
-
     /** Simple EJB interface */
-
-    public static interface Hello extends EJBObject {
-
+    public static interface Hello
+            extends EJBObject {
+        
         /** @return Returns &quot;Hello World!&quot; */
-
         public String getHelloWorld();
-
     }
 
     /** EJB Home interface */
-
-    public static interface HelloHome extends EJBHome {
+    public static interface HelloHome
+            extends EJBHome {
 
         /** @return Returns the EJB */
-
-        public Hello create(); 
-
+        public Hello create();
     }
 
     /** EJB implementation */
-
-    public static class HelloImpl extends EJBObjectMock implements Hello {
+    public static class HelloImpl
+            extends EJBObjectMock
+            implements Hello {
 
         /** @return Returns &quot;Hello World!&quot; */
-
         public String getHelloWorld() {
-
             return "Hello World!";
-
         }
-
     }
 
     /** EJB Home implementation */
-
-    public static class HelloHomeImpl extends EJBHomeMock implements HelloHome {
+    public static class HelloHomeImpl
+            extends EJBHomeMock
+            implements HelloHome {
 
         /** Default ctor. */
-
         public HelloHomeImpl() {
-
             super();
-
         }
 
-        /** 
-
-         * Non-valid ctor with arg. 
-
-         * @param foo An argument. 
-
-         * */
-
+        /**
+         * Non-valid ctor with arg.
+         * @param foo An argument.
+         */
         public HelloHomeImpl(final String foo) {
-
             super();
-
-            if (foo == null);
-
+            if (foo == null)
+            ;
         }
 
         /** @return Returns the EJB */
-
         public Hello create() {
-
             return new HelloImpl();
-
         }
-
     }
 
     /** EJB Home iinterface */
-
-    public static interface FooHome extends EJBHome {
-
+    public static interface FooHome
+            extends EJBHome {
         /** @return Returns the EJB */
-
         public Hello create();
-
     }
 
-
-
     /**
-
      * Test the narrow method.
-
      * @throws Exception
-
      */
 
     public final void testNarrow() throws Exception {
-
         final Map narrowMap = new HashMap();
-
         narrowMap.put("Hello", HelloHomeImpl.class.getConstructor(null));
-
         PortableRemoteObjectDelegateMock.setNarrowMap(narrowMap);
-
-        final PortableRemoteObjectDelegateMock delegate = new PortableRemoteObjectDelegateMock(); 
-
+        final PortableRemoteObjectDelegateMock delegate = new PortableRemoteObjectDelegateMock();
+        
         final HelloHome helloHome = (HelloHome)delegate.narrow("Hello", HelloHome.class);
-
         assertNotNull(helloHome);
-
         final Hello hello = helloHome.create();
-
         assertEquals("Hello World!", hello.getHelloWorld());
-
+        
         try {
-
             delegate.narrow("Hello", FooHome.class);
-
             fail("Should have thrown.");
-
         } catch (ClassCastException e) {
-
             assertNull(e.getCause());
-
         }
 
         try {
-
             delegate.narrow("Hello", HelloHomeImpl.class);
-
             fail("Should have thrown.");
-
         } catch (ClassCastException e) {
-
             assertTrue(e.getCause() instanceof IllegalArgumentException);
-
         }
 
-        narrowMap.put("Hello", HelloHomeImpl.class.getConstructor(new Class[] { String.class }));
-
+        narrowMap.put("Hello", HelloHomeImpl.class.getConstructor(new Class[]{String.class}));
         try {
-
             delegate.narrow("Hello", HelloHome.class);
-
             fail("Should have thrown.");
-
         } catch (ClassCastException e) {
-
             assertTrue(e.getCause() instanceof IllegalArgumentException);
-
         }
-
         assertNull(delegate.narrow("foo", HelloHome.class));
-
+        
         narrowMap.put("Hello", String.class.getConstructor(null));
-
         try {
-
             delegate.narrow("Hello", HelloHome.class);
-
             fail("Should have thrown.");
-
         } catch (ClassCastException e) {
-
             assertNull(e.getCause());
-
         }
-
     }
-
-
-
 }
 

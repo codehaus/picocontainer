113cbacaf1d98fb52eddd9d2b641011dc4b6c9d9
steve_molitor <steve_molitor@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-08-05 05:29:43 +0000

    removed some gunk files

---
+++ trunk/java/nanoaop/src/test/org/nanocontainer/nanoaop/script/groovy/AopNanoGroovyBuilderTestCase2.groovy
@@ -0,0 +1,9 @@
+package org.nanocontainer.nanoaop.script.groovy
+
+class AopNanoGroovyBuilderTestCase2 extends GroovyTestCase {
+
+    // TODO:  write tests!
+    public void testFoo() {
+    }
+
+}

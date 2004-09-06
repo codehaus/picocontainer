fc78aefc2efff8423c3d17134841523ffb2c5d3c
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-09-06 22:28:45 +0000

    Dead

--- java/nanocontainer/src/java/org/nanocontainer/aop/NullPointcutsFactory.java
+++
@@ -1,9 +0,0 @@
-package org.nanocontainer.aop;
-
-/**
- * @author Aslak Helles&oslash;y
- * @author Paul Hammant
- * @version $Revision$
- */
-public class NullPointcutsFactory { //implements PointcutsFactory {
-}

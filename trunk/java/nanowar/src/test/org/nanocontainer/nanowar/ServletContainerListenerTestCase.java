5291101a7afa6a3be3ee686c6d88308ee99154c8
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-09-12 13:06:25 +0000

    Renamed XMLContainerComposer to ScopedContainerComposer and refactored it to be a configurable composer for any nano script. Made ServletContainerListener support composer configuration.

--- trunk/java/nanowar/src/test/org/nanocontainer/nanowar/ServletContainerListenerTestCase.java
+++ trunk/java/nanowar/src/test/org/nanocontainer/nanowar/ServletContainerListenerTestCase.java
@@ -9,24 +9,25 @@
  *****************************************************************************/
 package org.nanocontainer.nanowar;
 
-import org.jmock.Mock;
-import org.jmock.MockObjectTestCase;
-import org.nanocontainer.script.groovy.GroovyContainerBuilder;
-import org.picocontainer.MutablePicoContainer;
-import org.picocontainer.PicoContainer;
-import org.picocontainer.defaults.DefaultPicoContainer;
-
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
+import java.util.Vector;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionBindingListener;
 import javax.servlet.http.HttpSessionEvent;
-import java.io.StringBufferInputStream;
-import java.io.StringReader;
-import java.util.Vector;
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.nanocontainer.integrationkit.DefaultLifecycleContainerBuilder;
+import org.nanocontainer.script.groovy.GroovyContainerBuilder;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.defaults.DefaultPicoContainer;
 
 /**
  * @author Aslak Helles&oslash;y
+ * @author Mauro Talevi
  * @version $Revision$
  */
 public class ServletContainerListenerTestCase extends MockObjectTestCase implements KeyConstants {
@@ -36,6 +37,15 @@ public class ServletContainerListenerTestCase extends MockObjectTestCase impleme
             "pico = builder.container(parent:parent) { \n" +
             "  component(StringBuffer)\n" +
             "}";
+    private String composerScript =
+        "<container>" +
+        "<component-implementation class='org.nanocontainer.nanowar.ScopedContainerConfigurator'>"+
+        "	   <parameter><string>org.nanocontainer.script.xml.XMLContainerBuilder</string></parameter>"+
+        "      <parameter><string>nanowar-application.xml,nanowar/application.xml</string></parameter> "+
+        "      <parameter><string>nanowar-session.xml,nanowar/session.xml</string></parameter>        "+
+        "      <parameter><string>nanowar-request.xml,nanowar/request.xml</string></parameter> "+
+        "</component-implementation>" +    						
+        "</container>";
 
     public void testApplicationScopeContainerIsCreatedWhenServletContextIsInitialisedWithInlinedScript() {
         ServletContainerListener listener = new ServletContainerListener();
@@ -116,6 +126,64 @@ public class ServletContainerListenerTestCase extends MockObjectTestCase impleme
                 .with(eq(SESSION_CONTAINER), isA(PicoContainer.class));
 
         listener.sessionCreated(new HttpSessionEvent((HttpSession) httpSessionMock.proxy()));
+    }
+
+    public void testContainerComposerIsCreatedWithNoConfiguration() {
+        ServletContainerListener listener = new ServletContainerListener();
+
+        Mock servletContextMock = mock(ServletContext.class);
+        final Vector initParams = new Vector();
+        initParams.add(ServletContainerListener.CONTAINER_COMPOSER);
+        servletContextMock.expects(once())
+                .method("getInitParameterNames")
+                .withNoArguments()
+                .will(returnValue(initParams.elements()));
+        servletContextMock.expects(once())
+                .method("getInitParameter")
+                .with(eq(ServletContainerListener.CONTAINER_COMPOSER))
+                .will(returnValue(ScopedContainerComposer.class.getName()));
+        servletContextMock.expects(once())
+        		.method("getInitParameter")
+        		.with(eq(ServletContainerListener.CONTAINER_COMPOSER_CONFIGURATION))
+        		.will(returnValue(null));
+        servletContextMock.expects(once())
+        		.method("setAttribute")
+        		.with(eq(BUILDER), isA(DefaultLifecycleContainerBuilder.class));
+        servletContextMock.expects(once())
+        		.method("setAttribute")
+        		.with(eq(APPLICATION_CONTAINER), isA(PicoContainer.class));
+        listener.contextInitialized(new ServletContextEvent((ServletContext) servletContextMock.proxy()));
+    }
+    
+    public void testContainerComposerIsCreatedWithConfiguration() {
+        ServletContainerListener listener = new ServletContainerListener();
+
+        Mock servletContextMock = mock(ServletContext.class);
+        final Vector initParams = new Vector();
+        initParams.add(ServletContainerListener.CONTAINER_COMPOSER);
+        servletContextMock.expects(once())
+                .method("getInitParameterNames")
+                .withNoArguments()
+                .will(returnValue(initParams.elements()));
+        servletContextMock.expects(once())
+                .method("getInitParameter")
+                .with(eq(ServletContainerListener.CONTAINER_COMPOSER))
+                .will(returnValue(ScopedContainerComposer.class.getName()));
+        servletContextMock.expects(once())
+        		.method("getInitParameter")
+        		.with(eq(ServletContainerListener.CONTAINER_COMPOSER_CONFIGURATION))
+        		.will(returnValue("nanowar/composer-config.xml"));
+        servletContextMock.expects(once())
+        		.method("getResourceAsStream")
+        		.with(eq("nanowar/composer-config.xml"))
+        		.will(returnValue(new StringBufferInputStream(composerScript)));
+        servletContextMock.expects(once())
+				.method("setAttribute")
+				.with(eq(BUILDER), isA(DefaultLifecycleContainerBuilder.class));
+        servletContextMock.expects(once())
+				.method("setAttribute")
+				.with(eq(APPLICATION_CONTAINER), isA(PicoContainer.class));
+        listener.contextInitialized(new ServletContextEvent((ServletContext) servletContextMock.proxy()));
     }
 
 }

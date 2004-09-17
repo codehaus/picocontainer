28d5481ccc301bba4c80717483c816629544d063
ko5tik <ko5tik@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-09-17 15:31:53 +0000

    added attribute for calling default constructor

--- java/nanocontainer/src/test/org/nanocontainer/script/xml/TestBean.java
+++ java/nanocontainer/src/test/org/nanocontainer/script/xml/TestBean.java
@@ -12,7 +12,19 @@ package org.nanocontainer.script.xml;
 public class TestBean {
     private int foo;
     private String bar;
-
+    private String constructorCalled;
+    
+    public TestBean() {
+        constructorCalled="default";
+    }
+    
+    public TestBean(String greedy) {
+         constructorCalled="greedy";
+    }
+    
+    public String getConstructorCalled() {
+        return constructorCalled;
+    }
     public int getFoo() {
         return foo;
     }

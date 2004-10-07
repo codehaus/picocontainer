931733ee55d65c9e536d002481fa62fa0aeaae60
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-10-07 03:46:25 +0000

    getting ready for damage control

---
+++ trunk/book/damage-control.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+export PATH=$PATH:/home/projects/picocontainer/txt2tags-2.0
+./build.sh
+mv cdi.html /home/projects/picocontainer/public_html/cdi-book.html
--- trunk/book/deploy.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-./build.sh
-cp cdi.html /home/projects/picocontainer/public_html

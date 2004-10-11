885e4ff544cd40cce2d3a3ce415fe10171540607
mward <mward@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-10-11 21:32:35 +0000

    updated bootstrap files with latest jars and fixed groovy script

--- trunk/java/microcontainer/src/bootstrap/microcontainerboot.groovy
+++ trunk/java/microcontainer/src/bootstrap/microcontainerboot.groovy
@@ -1,14 +1,13 @@
 import org.nanocontainer.script.groovy.NanoContainerBuilder
 
-builder = new NanoGroovyBuilder()
+builder = new NanoContainerBuilder()
 
-pico = builder.container(parent:parent,class:ImplementationHidingSoftCompositionPicoContainer) {
+nano = builder.container(parent:parent, class:"org.picocontainer.defaults.DefaultPicoContainer") {
   classpathelement(path:"lib/hidden/microcontainer-impl-0.1-SNAPSHOT.jar");
   component(key:org.microcontainer.Kernel, class:"org.microcontainer.impl.DefaultKernel")
-  component(key:org.microcontainer.McaDeployer, class:"org.microcontainer.impl.DefaultMcaDeployer")  
+  component(key:org.microcontainer.McaDeployer, class:"org.microcontainer.impl.DefaultMcaDeployer")
   component(class:"org.microcontainer.impl.MicroBeanShellConsole")
 }
-pico.start();
+pico = nano.getPico();
 org.microcontainer.ResidualController rc = new org.microcontainer.ResidualController(pico);
 rc.start();
-

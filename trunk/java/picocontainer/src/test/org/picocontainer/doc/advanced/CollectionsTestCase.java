38a0bef6786fa523274d2fb78871c41e2862f515
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-10-21 21:38:36 +0000

    Add snippets for Collections.

---
+++ trunk/java/picocontainer/src/test/org/picocontainer/doc/advanced/CollectionsTestCase.java
@@ -0,0 +1,90 @@
+package org.picocontainer.doc.advanced;
+
+import junit.framework.TestCase;
+
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.Parameter;
+import org.picocontainer.defaults.CollectionComponentParameter;
+import org.picocontainer.defaults.DefaultPicoContainer;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * @author J&ouml;rg Schaible
+ * @version $Revision$
+ */
+public class CollectionsTestCase
+        extends TestCase {
+    private MutablePicoContainer pico;
+    private Shark shark;
+    private Cod cod;
+    private Bowl bowl;
+
+    protected void setUp() throws Exception {
+        pico = new DefaultPicoContainer();
+    }
+
+    // START SNIPPET: classes
+
+    public static interface Fish {
+    }
+
+    public static class Cod
+            implements Fish {
+    }
+
+    public static class Shark
+            implements Fish {
+    }
+
+    public static class Bowl {
+        private final LinkedList fishes;
+        private final Collection cods;
+
+        public Bowl(LinkedList fishes, Collection cods) {
+            this.fishes = fishes;
+            this.cods = cods;
+        }
+
+        public Collection getFishes() {
+            return fishes;
+        }
+
+        public Collection getCods() {
+            return cods;
+        }
+    }
+
+    // END SNIPPET: classes
+
+    public void testShouldCreateBowlWithFishCollection() {
+
+        //      START SNIPPET: usage
+
+        pico.registerComponentImplementation(Shark.class);
+        pico.registerComponentImplementation(Cod.class);
+        pico.registerComponentImplementation(Bowl.class);
+
+        pico.registerComponentImplementation(Bowl.class, Bowl.class, new Parameter[]{
+            new CollectionComponentParameter(Fish.class, false),
+            new CollectionComponentParameter(Cod.class, false)
+        });
+        //      END SNIPPET: usage
+
+        shark = (Shark) pico.getComponentInstanceOfType(Shark.class);
+        cod = (Cod) pico.getComponentInstanceOfType(Cod.class);
+        
+        Collection fishes = bowl.getFishes();
+        assertEquals(2, fishes.size());
+        assertTrue(fishes.contains(shark));
+        assertTrue(fishes.contains(cod));
+
+        Collection cods = bowl.getCods();
+        assertEquals(1, cods.size());
+        assertTrue(cods.contains(cod));
+    }
+}

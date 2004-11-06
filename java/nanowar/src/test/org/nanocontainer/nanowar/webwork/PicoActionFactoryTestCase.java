c1ee964f5a614928abd94928ad7b4005a47d4987
ko5tik <ko5tik@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-11-06 16:54:03 +0000

    created tests for webwork stuff

---
+++ java/nanowar/src/test/org/nanocontainer/nanowar/webwork/PicoActionFactoryTestCase.java
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.nanocontainer.nanowar.webwork;
+
+import webwork.action.Action;
+import webwork.action.ServletActionContext;
+import webwork.action.factory.ActionFactory;
+import org.picocontainer.defaults.DefaultPicoContainer;
+import junit.framework.TestCase;
+import org.nanocontainer.nanowar.KeyConstants;
+
+/**
+ * test capabilities of pico action factory
+ * @author Konstantin Pribluda
+ */
+public class PicoActionFactoryTestCase extends TestCase {
+    
+    /**
+    * test that action is instantiated and receives construtor parameters
+    * from container
+    */
+    public void testActionInstantiation() throws Exception {
+        PicoActionFactory factory = new PicoActionFactory();
+        DefaultPicoContainer container = new DefaultPicoContainer();
+        container.registerComponentInstance("foo");
+        (new ActionContextScopeObjectReference(KeyConstants.REQUEST_CONTAINER)).set(container);
+        TestAction action = (TestAction)factory.getActionImpl("org.nanocontainer.nanowar.webwork.TestAction");
+        assertNotNull(action);
+        assertEquals("foo",action.getFoo());
+    }
+}

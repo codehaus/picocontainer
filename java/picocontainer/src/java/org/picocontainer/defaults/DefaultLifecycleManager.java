78fdae8fa7f07efefeb50a0bb638c3c64a1e845b
rinkrank <rinkrank@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2004-11-08 01:29:28 +0000

    Simplified tests Relaxed dependencies Improved Javadoc

--- java/picocontainer/src/java/org/picocontainer/defaults/DefaultLifecycleManager.java
+++ java/picocontainer/src/java/org/picocontainer/defaults/DefaultLifecycleManager.java
@@ -13,13 +13,25 @@ package org.picocontainer.defaults;
 import org.picocontainer.Disposable;
 import org.picocontainer.LifecycleManager;
 import org.picocontainer.PicoContainer;
+import org.picocontainer.PicoVisitor;
 import org.picocontainer.Startable;
 
 import java.io.Serializable;
 import java.lang.reflect.Method;
 
 /**
+ * This class implements the default lifecycle based on
+ * <ul>
+ * <li>{@link org.picocontainer.Startable#start()}</li>
+ * <li>{@link org.picocontainer.Startable#stop()}</li>
+ * <li>{@link org.picocontainer.Disposable#dispose()}</li>
+ * </ul>
+ *
+ * It also allows custom lifecycle strategies to be plugged in via {@link #DefaultLifecycleManager(org.picocontainer.PicoVisitor, org.picocontainer.PicoVisitor, org.picocontainer.PicoVisitor)}.
+ *
  * @author Paul Hammant
+ * @author J&ouml;rg Schaible
+ * @author Aslak Helles&oslash;y
  * @version $Revision$
  */
 public class DefaultLifecycleManager implements LifecycleManager, Serializable {
@@ -27,6 +39,9 @@ public class DefaultLifecycleManager implements LifecycleManager, Serializable {
     private static final Method START;
     private static final Method STOP;
     private static final Method DISPOSE;
+    private PicoVisitor startVisitor;
+    private PicoVisitor stopVisitor;
+    private PicoVisitor disposeVisitor;
 
     static {
         try {
@@ -40,25 +55,51 @@ public class DefaultLifecycleManager implements LifecycleManager, Serializable {
         }
     }
 
-    private ComponentMonitor componentMonitor;
-
+    /**
+     * Creates a lifecycle manager which will invoke lifecycle methods on components implementing:
+     * <ul>
+     * <li>{@link org.picocontainer.Startable#start()}</li>
+     * <li>{@link org.picocontainer.Startable#stop()}</li>
+     * <li>{@link org.picocontainer.Disposable#dispose()}</li>
+     * </ul>
+     *
+     * @param componentMonitor the monitor that will receive lifecycle events.
+     */
     public DefaultLifecycleManager(ComponentMonitor componentMonitor) {
-        this.componentMonitor = componentMonitor;
+        this(new LifecycleVisitor(START, Startable.class, true, componentMonitor),
+                new LifecycleVisitor(STOP, Startable.class, false, componentMonitor),
+                new LifecycleVisitor(DISPOSE, Disposable.class, false, componentMonitor));
     }
 
+    /**
+     * Creates a lifecycle manager using pluggable lifecycle.
+     *
+     * @param startVisitor the visitor to use on start()
+     * @param stopVisitor the visitor to use on stop()
+     * @param disposeVisitor the visitor to use on dispose()
+     */
+    public DefaultLifecycleManager(PicoVisitor startVisitor, PicoVisitor stopVisitor, PicoVisitor disposeVisitor) {
+        this.startVisitor = startVisitor;
+        this.stopVisitor = stopVisitor;
+        this.disposeVisitor = disposeVisitor;
+    }
+
+    /**
+     * Creates a lifecycle manager with default visitors using a {@link NullComponentMonitor}.
+     */
     public DefaultLifecycleManager() {
-        componentMonitor = NullComponentMonitor.getInstance();
+        this(new NullComponentMonitor());
     }
 
     public void start(PicoContainer node) {
-        new LifecycleVisitor(START, Startable.class, true, componentMonitor).traverse(node);
+        startVisitor.traverse(node);
     }
 
     public void stop(PicoContainer node) {
-        new LifecycleVisitor(STOP, Startable.class, false, componentMonitor).traverse(node);
+        stopVisitor.traverse(node);
     }
 
     public void dispose(PicoContainer node) {
-        new LifecycleVisitor(DISPOSE, Disposable.class, false, componentMonitor).traverse(node);
+        disposeVisitor.traverse(node);
     }
 }

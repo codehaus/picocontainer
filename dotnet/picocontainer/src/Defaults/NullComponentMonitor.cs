e8ff3a45acbde15288d8df8893aa35748a525553
mward <mward@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-02-04 05:36:08 +0000

    Initial commit of LifecycleManager code, ported from Java

---
+++ dotnet/picocontainer/src/Defaults/NullComponentMonitor.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Reflection;
+
+namespace PicoContainer.Defaults
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class NullComponentMonitor : IComponentMonitor
+	{
+		public void Instantiating(ConstructorInfo constructor)
+		{
+		}
+
+		public void Instantiated(ConstructorInfo constructor, long beforeTime, long duration)
+		{
+		}
+
+		public void InstantiationFailed(ConstructorInfo constructor, Exception e)
+		{
+		}
+
+		public void Invoking(MethodInfo method, Object instance)
+		{
+		}
+
+		public void Invoked(MethodInfo method, Object instance, long duration)
+		{
+		}
+
+		public void InvocationFailed(MethodInfo method, Object instance, Exception e)
+		{
+		}
+	}
+}

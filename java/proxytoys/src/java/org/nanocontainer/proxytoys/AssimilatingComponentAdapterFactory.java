b1f266073b7bf1d42275060f53a38dd8a8fdf322
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-02-18 20:33:55 +0000

    Fix copyright header.

--- java/proxytoys/src/java/org/nanocontainer/proxytoys/AssimilatingComponentAdapterFactory.java
+++ java/proxytoys/src/java/org/nanocontainer/proxytoys/AssimilatingComponentAdapterFactory.java
@@ -1,7 +1,13 @@
-/*
- * Copyright (C) 2005 Jörg Schaible
- * Created on 18.02.2005 by Jörg Schaible
- */
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Joerg Schaibe                                            *
+ *****************************************************************************/
+
 package org.nanocontainer.proxytoys;
 
 import com.thoughtworks.proxy.ProxyFactory;

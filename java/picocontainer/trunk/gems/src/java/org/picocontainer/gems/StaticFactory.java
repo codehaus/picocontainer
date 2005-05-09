929c6b5a09377dc582f10f1bffeb86da8378443e
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-05-09 10:48:23 +0000

    Copy picogems to new location.

---
+++ java/picocontainer/trunk/gems/src/java/org/picocontainer/gems/StaticFactory.java
@@ -0,0 +1,23 @@
+package org.picocontainer.gems;
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Leo Simmons & Jörg Schaible                              *
+ *****************************************************************************/
+/**
+ * Interface for a static factory wrapper used by the 
+ * {@link org.picocontainer.defaults.StaticFactoryComponentAdapter}.
+ * @author J&ouml;rg Schaible
+ * @author Leo Simmons
+ * @since 1.1
+ */
+public interface StaticFactory {
+    /**
+     * @return Returns the instance created by the factory.
+     */
+    public Object get();
+}

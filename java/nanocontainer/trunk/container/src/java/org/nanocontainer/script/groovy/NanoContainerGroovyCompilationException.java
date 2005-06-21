ee77ea1a9c1e2e707a9ca83f87aaa07fb3259a32
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-06-21 09:11:51 +0000

    Upgraded to groovy-jsr-02 (NANO-141)

--- java/nanocontainer/trunk/container/src/java/org/nanocontainer/script/groovy/NanoContainerGroovyCompilationException.java
+++ java/nanocontainer/trunk/container/src/java/org/nanocontainer/script/groovy/NanoContainerGroovyCompilationException.java
@@ -1,10 +1,11 @@
 package org.nanocontainer.script.groovy;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.control.messages.ExceptionMessage;
 import org.nanocontainer.script.NanoContainerMarkupException;
-
-import java.util.List;
 
 /**
  * @author Paul Hammant
@@ -21,17 +22,23 @@ public class NanoContainerGroovyCompilationException extends NanoContainerMarkup
     public String getMessage() {
         StringBuffer sb = new StringBuffer();
         sb.append(super.getMessage() + "\n");
-        List errors = compilationFailedException.getUnit().getErrors();
-        // errors can definitely be null:
-        if (errors != null) {
-            for (int i = 0; i < errors.size(); i++) {
-                Object o = errors.get(i);
-                if (o instanceof ExceptionMessage) {
-                    ExceptionMessage em = (ExceptionMessage) o;
-                    sb.append(em.getCause().getMessage() + "\n");
-                }
+        List errors = getErrors(compilationFailedException);
+        for (int i = 0; i < errors.size(); i++) {
+            Object o = errors.get(i);
+            if (o instanceof ExceptionMessage) {
+                ExceptionMessage em = (ExceptionMessage) o;
+                sb.append(em.getCause().getMessage() + "\n");
             }
         }
         return sb.toString();
+    }
+
+    private List getErrors(CompilationFailedException e) {
+        List errors = e.getUnit().getErrorCollector().getErrors();
+        // errors can definitely be null:
+        if ( errors == null ){
+            return Collections.EMPTY_LIST;                
+        }
+        return errors;
     }
 }

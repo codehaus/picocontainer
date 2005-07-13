ee0e010285bf8e8f5bd076d0860e942322297e39
juze <juze@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-07-13 15:28:58 +0000

    Removing an unused import.

--- java/nanocontainer-persistence/trunk/persistence/src/test/org/nanocontainer/persistence/hibernate/classic/FailoverSessionDelegatorTestCase.java
+++ java/nanocontainer-persistence/trunk/persistence/src/test/org/nanocontainer/persistence/hibernate/classic/FailoverSessionDelegatorTestCase.java
@@ -18,7 +18,6 @@ import net.sf.hibernate.HibernateException;
 import net.sf.hibernate.Interceptor;
 import net.sf.hibernate.Session;
 import net.sf.hibernate.SessionFactory;
-import net.sf.hibernate.tool.hbm2ddl.SchemaExport;
 import net.sf.hibernate.type.Type;
 
 import org.nanocontainer.persistence.hibernate.pojo.Pojo;

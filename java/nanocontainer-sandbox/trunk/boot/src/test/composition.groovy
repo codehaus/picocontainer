318cb84347135ff4c899a6933a53058b127cef47
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-08-02 18:15:18 +0000

    shorter

--- java/nanocontainer-sandbox/trunk/boot/src/test/composition.groovy
+++ java/nanocontainer-sandbox/trunk/boot/src/test/composition.groovy
@@ -1,22 +1,16 @@
 import org.nanocontainer.reflection.ImplementationHidingNanoPicoContainer
-import org.nanocontainer.ClassNameKey
-import java.net.SocketPermission
 
-builder = new org.nanocontainer.script.groovy.NanoContainerBuilder()
-
-parent = builder.container(parent:parent, class:ImplementationHidingNanoPicoContainer) {
+pico = builder.container(parent:parent, class:ImplementationHidingNanoPicoContainer) {
     component(instance:org.nanocontainer.DefaultNanoContainer) // a class defn
     classPathElement(path:"comps/api.jar")
-
     classLoader {
         classPathElement(path:"comps/honeyimpl.jar")
         component(classNameKey:"org.nanocontainer.boot.Honey", class:"org.nanocontainer.boot.BeeHiveHoney")
     }
     classLoader {
         classPathElement(path:"comps/bearimpl.jar") {
-             grant(new SocketPermission("yahoo.com:80", "connect"))
+             grant(new java.net.SocketPermission("yahoo.com:80", "connect"))
         }
         component(class:"org.nanocontainer.boot.BrownBear")
     }
 }
-pico = parent.getPico()

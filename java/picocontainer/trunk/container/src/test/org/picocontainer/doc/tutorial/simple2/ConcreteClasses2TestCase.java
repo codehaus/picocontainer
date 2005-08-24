a2ac4197da07aefa7c52ed7269368fd0d9336580
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-08-24 21:49:01 +0000

    compiling again

--- java/picocontainer/trunk/container/src/test/org/picocontainer/doc/tutorial/simple2/ConcreteClasses2TestCase.java
+++ java/picocontainer/trunk/container/src/test/org/picocontainer/doc/tutorial/simple2/ConcreteClasses2TestCase.java
@@ -1,22 +1,20 @@
-package org.picocontainer.doc.tutorial.simple;
+package org.picocontainer.doc.tutorial.simple2;
 
 import junit.framework.TestCase;
 import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.doc.tutorial.interfaces.Kissable;
+import org.picocontainer.doc.tutorial.simple.Girl;
 import org.picocontainer.defaults.DefaultPicoContainer;
 
-public class ConcreteClassesTestCase extends TestCase {
+public class ConcreteClasses2TestCase extends TestCase {
 
     public void testAssembleComponentsAndInstantiateAndUseThem() {
         // START SNIPPET: assemble
         MutablePicoContainer pico = new DefaultPicoContainer();
-        pico.registerComponentImplementation(Boy.class);
+        pico.registerComponentImplementation(Kissable.class, Boy.class);
         pico.registerComponentImplementation(Girl.class);
         // END SNIPPET: assemble
 
-        // START SNIPPET: instantiate-and-use
-        Girl girl = (Girl) pico.getComponentInstance(Girl.class);
-        girl.kissSomeone();
-        // END SNIPPET: instantiate-and-use
     }
 
 

cfad244fdb047270cb2846ad03abaa9cfc5ce289
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-03 23:19:46 +0000

    Fixed quotes

--- java/picocontainer/trunk/container/src/java/org/picocontainer/monitors/AbstractComponentMonitor.java
+++ java/picocontainer/trunk/container/src/java/org/picocontainer/monitors/AbstractComponentMonitor.java
@@ -23,12 +23,12 @@ import org.picocontainer.ComponentMonitor;
  */
 public abstract class AbstractComponentMonitor implements ComponentMonitor, Serializable {
 
-    public final static String INSTANTIATING = "PicoContainer: instantiating '{0}'";
-    public final static String INSTANTIATED = "PicoContainer: instantiated '{0}' [{1} ms]";
-    public final static String INSTANTIATION_FAILED = "PicoContainer: instantiation failed: '{0}', reason: '{1}'";
-    public final static String INVOKING = "PicoContainer: invoking '{0}' on '{1}'";
-    public final static String INVOKED = "PicoContainer: invoked '{0}' on '{1}' [{2} ms]";
-    public final static String INVOCATION_FAILED = "PicoContainer: invocation failed: '{0}' on '{1}', reason: {2}";
+    public final static String INSTANTIATING = "PicoContainer: instantiating {0}";
+    public final static String INSTANTIATED = "PicoContainer: instantiated {0} [{1} ms]";
+    public final static String INSTANTIATION_FAILED = "PicoContainer: instantiation failed: {0}, reason: {1}";
+    public final static String INVOKING = "PicoContainer: invoking {0} on {1}";
+    public final static String INVOKED = "PicoContainer: invoked {0} on {1} [{2} ms]";
+    public final static String INVOCATION_FAILED = "PicoContainer: invocation failed: {0} on {1}, reason: {2}";
 
     public static String format(String template, Object[] arguments) {
         return MessageFormat.format(template, arguments);

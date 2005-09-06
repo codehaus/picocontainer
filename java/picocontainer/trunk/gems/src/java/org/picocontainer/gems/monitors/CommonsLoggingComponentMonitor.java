3d4ab1a799c1e5e7ba7aac899e76cbdec8970e54
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-06 16:44:58 +0000

    Improve logging implementation of CLM.

--- java/picocontainer/trunk/gems/src/java/org/picocontainer/gems/monitors/CommonsLoggingComponentMonitor.java
+++ java/picocontainer/trunk/gems/src/java/org/picocontainer/gems/monitors/CommonsLoggingComponentMonitor.java
@@ -10,6 +10,7 @@
 
 package org.picocontainer.gems.monitors;
 
+import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
@@ -18,39 +19,39 @@ import org.apache.commons.logging.LogFactory;
 import org.picocontainer.ComponentMonitor;
 import org.picocontainer.monitors.AbstractComponentMonitor;
 
+
 /**
- * A {@link ComponentMonitor} which writes to a Commons Logging {@link Log} instance. 
+ * A {@link ComponentMonitor} which writes to a Commons Logging {@link Log} instance.
  * 
  * @author Mauro Talevi
  * @version $Revision: $
  */
-public class CommonsLoggingComponentMonitor extends AbstractComponentMonitor {
+public class CommonsLoggingComponentMonitor extends AbstractComponentMonitor implements Serializable {
 
     private Log log;
-    
+
     /**
-     * Creates a CommonsLoggingComponentMonitor using the 
-     * CommonsLoggingComponentMonitor class instance.
+     * Creates a CommonsLoggingComponentMonitor using the CommonsLoggingComponentMonitor class instance.
      */
     public CommonsLoggingComponentMonitor() {
         this(CommonsLoggingComponentMonitor.class);
     }
 
     /**
-     * Creates a CommonsLoggingComponentMonitor with a given Log instance class.
-     * The class name is used to retrieve the Log instance.
+     * Creates a CommonsLoggingComponentMonitor with a given Log instance class. The class name is used to retrieve the
+     * Log instance.
      * 
-     * @param logClass the class of the Log 
+     * @param logClass the class of the Log
      */
     public CommonsLoggingComponentMonitor(Class logClass) {
         this(logClass.getName());
     }
 
     /**
-     * Creates a CommonsLoggingComponentMonitor with a given Log instance name.
-     * It uses the {@link LogFactory LogFactory} to create the Log instance.
+     * Creates a CommonsLoggingComponentMonitor with a given Log instance name. It uses the
+     * {@link LogFactory LogFactory} to create the Log instance.
      * 
-     * @param logName the name of the Log 
+     * @param logName the name of the Log
      */
     public CommonsLoggingComponentMonitor(String logName) {
         this(LogFactory.getLog(logName));
@@ -58,35 +59,47 @@ public class CommonsLoggingComponentMonitor extends AbstractComponentMonitor {
 
     /**
      * Creates a CommonsLoggingComponentMonitor with a given Log instance
-     *
+     * 
      * @param log the Log to write to
      */
     public CommonsLoggingComponentMonitor(Log log) {
         this.log = log;
     }
-    
+
     public void instantiating(Constructor constructor) {
-        log.debug(format(INSTANTIATING, new Object[]{constructor}));
+        if (log.isDebugEnabled()) {
+            log.debug(format(INSTANTIATING, new Object[]{constructor}));
+        }
     }
 
     public void instantiated(Constructor constructor, long duration) {
-        log.debug(format(INSTANTIATED, new Object[]{constructor, new Long(duration)}));
+        if (log.isDebugEnabled()) {
+            log.debug(format(INSTANTIATED, new Object[]{constructor, new Long(duration)}));
+        }
     }
 
     public void instantiationFailed(Constructor constructor, Exception e) {
-        log.warn(format(INSTANTIATION_FAILED, new Object[]{constructor, e.getMessage()}));
+        if (log.isWarnEnabled()) {
+            log.warn(format(INSTANTIATION_FAILED, new Object[]{constructor, e.getMessage()}));
+        }
     }
 
     public void invoking(Method method, Object instance) {
-        log.debug(format(INVOKING, new Object[]{method, instance}));
+        if (log.isDebugEnabled()) {
+            log.debug(format(INVOKING, new Object[]{method, instance}));
+        }
     }
 
     public void invoked(Method method, Object instance, long duration) {
-        log.debug(format(INVOKED, new Object[]{method, instance, new Long(duration)}));
+        if (log.isDebugEnabled()) {
+            log.debug(format(INVOKED, new Object[]{method, instance, new Long(duration)}));
+        }
     }
 
     public void invocationFailed(Method method, Object instance, Exception e) {
-        log.warn(format(INVOCATION_FAILED, new Object[]{method, instance, e.getMessage()}));
+        if (log.isWarnEnabled()) {
+            log.warn(format(INVOCATION_FAILED, new Object[]{method, instance, e.getMessage()}));
+        }
     }
 
 }

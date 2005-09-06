873b344723e531b6fe39ef8e46649b6c1a7d7b68
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-06 18:37:15 +0000

    Added lifecycle state flags

--- java/picocontainer/trunk/container/src/java/org/picocontainer/defaults/CachingComponentAdapter.java
+++ java/picocontainer/trunk/container/src/java/org/picocontainer/defaults/CachingComponentAdapter.java
@@ -23,14 +23,20 @@ import org.picocontainer.PicoIntrospectionException;
  * <p>
  * This adapter is also a {@link LifecycleManager lifecycle manager} which will apply
  * the delegate's {@link LifecycleStrategy lifecycle strategy} to the cached component instance.
+ * The lifecycle state is maintained so that the component instance behaves in the expected way,
+ * eg it can't be started if already started, it can't be started or stopped if disposed, it can't
+ * be stopped if not started, it can't be disposed if already disposed.
  * </p>
  * 
+ * @author Mauro Talevi
  * @version $Revision$
  */
 public class CachingComponentAdapter extends DecoratingComponentAdapter implements LifecycleManager {
 
     private ObjectReference instanceReference;
-
+    private boolean disposed = false;
+    private boolean started = false;
+    
     public CachingComponentAdapter(ComponentAdapter delegate) {
         this(delegate, new SimpleReference());
     }
@@ -39,22 +45,33 @@ public class CachingComponentAdapter extends DecoratingComponentAdapter implemen
         super(delegate);
         this.instanceReference = instanceReference;
     }
-
-    public CachingComponentAdapter(ComponentAdapter delegate, ObjectReference instanceReference, LifecycleStrategy lifecycleStrategy) {
-        super(delegate);
-        this.instanceReference = instanceReference;
-    }
     
     public Object getComponentInstance(PicoContainer container)
             throws PicoInitializationException, PicoIntrospectionException, AssignabilityRegistrationException, NotConcreteRegistrationException {
         if (instanceReference.get() == null) {
             instanceReference.set(super.getComponentInstance(container));
+            //TODO decide if component should always be started upon initialisation
+            //start(container);
         }
         return instanceReference.get();
     }
 
+    /**
+     * Flushes the cache. 
+     * If the component instance is started is will stop and dispose it before 
+     * flushing the cache.
+     */
     public void flush() {
+        Object instance = instanceReference.get();
+        if ( instance != null && delegateSupportsLifecycle() && started ) {
+            stop(instance);
+            dispose(instance);
+        }
         instanceReference.set(null);
+    }
+
+    private boolean delegateSupportsLifecycle() {
+        return getDelegate() instanceof LifecycleStrategy;
     }
 
     /**
@@ -62,7 +79,12 @@ public class CachingComponentAdapter extends DecoratingComponentAdapter implemen
      * {@inheritDoc}
      */
     public void start(PicoContainer container) {
-        start(getComponentInstance(container));
+        if ( delegateSupportsLifecycle() ){
+            if (disposed) throw new IllegalStateException("Already disposed");
+            if (started) throw new IllegalStateException("Already started");
+            start(getComponentInstance(container));
+            started = true;        
+        }
     }
 
     /**
@@ -70,7 +92,14 @@ public class CachingComponentAdapter extends DecoratingComponentAdapter implemen
      * {@inheritDoc}
      */
     public void stop(PicoContainer container) {
-        stop(getComponentInstance(container));
+        if ( delegateSupportsLifecycle() ){
+            if (disposed)
+                throw new IllegalStateException("Already disposed");
+            if (!started)
+                throw new IllegalStateException("Not started");
+            stop(getComponentInstance(container));
+            started = false;
+        }
     }
 
     /**
@@ -78,8 +107,11 @@ public class CachingComponentAdapter extends DecoratingComponentAdapter implemen
      * {@inheritDoc}
      */
     public void dispose(PicoContainer container) {
-        dispose(getComponentInstance(container));
+        if ( delegateSupportsLifecycle() ){
+            if (disposed) throw new IllegalStateException("Already disposed");
+            dispose(getComponentInstance(container));
+            disposed = true;
+        }
     }
-
     
 }
---
+++ java/picocontainer/trunk/container/src/test/org/picocontainer/defaults/CachingComponentAdapterTestCase.java
@@ -0,0 +1,148 @@
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *****************************************************************************/
+package org.picocontainer.defaults;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.testmodel.SimpleTouchable;
+import org.picocontainer.testmodel.Touchable;
+
+/**
+ * @author Mauro Talevi
+ * @version $Revision: $
+ */
+public class CachingComponentAdapterTestCase extends MockObjectTestCase {
+
+    public void testCannotStartDisposedComponent() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(false, false, true, true));
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.dispose(pico);
+        try {
+            adapter.start(pico);
+            fail("IllegalStateException expected");
+        } catch (Exception e) {
+            assertEquals("Already disposed", e.getMessage());
+        }
+    }
+
+    public void testCannotRestartComponent() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(true, false, false, true));
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.start(pico);
+        try {
+            adapter.start(pico);
+            fail("IllegalStateException expected");
+        } catch (Exception e) {
+            assertEquals("Already started", e.getMessage());
+        }
+    }
+    
+    public void testCannotStopDisposedComponent() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(false, false, true, true));
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.dispose(pico);
+        try {
+            adapter.stop(pico);
+            fail("IllegalStateException expected");
+        } catch (Exception e) {
+            assertEquals("Already disposed", e.getMessage());
+        }
+    }
+
+    public void testCannotRestopComponent() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(true, true, false, true));
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.start(pico);
+        adapter.stop(pico);
+        try {
+            adapter.stop(pico);
+            fail("IllegalStateException expected");
+        } catch (Exception e) {
+            assertEquals("Not started", e.getMessage());
+        }
+    }
+
+    public void testCannotRedisposeComponent() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(true, true, true, true));
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.start(pico);
+        adapter.stop(pico);
+        adapter.dispose(pico);
+        try {
+            adapter.dispose(pico);
+            fail("IllegalStateException expected");
+        } catch (Exception e) {
+            assertEquals("Already disposed", e.getMessage());
+        }
+    }
+
+    public void testComponentIsStoppedAndDisposedIfStartedWhenFlushed() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(true, true, true, true));
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.start(pico);
+        adapter.flush();
+    }
+
+    public void testComponentIsNotStoppedAndDisposedWhenFlushedIfNotStarted() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterSupportingLifecycleStrategy(false, false, false, false));
+        adapter.flush();
+    }
+
+    public void testComponentIsNotStoppedAndDisposedWhenFlushedIfDelegateDoesNotSupportLifecycle() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterNotSupportingLifecycleStrategy());
+        adapter.flush();
+    }
+    
+    public void testLifecycleIsIgnoredIfDelegateDoesNotSupportIt() {
+        CachingComponentAdapter adapter = new CachingComponentAdapter(
+                mockComponentAdapterNotSupportingLifecycleStrategy());
+        PicoContainer pico = new DefaultPicoContainer();
+        adapter.start(pico);
+        adapter.stop(pico);
+        adapter.dispose(pico);
+    }
+    
+    private ComponentAdapter mockComponentAdapterNotSupportingLifecycleStrategy() {
+        Mock mock = mock(ComponentAdapter.class);
+        return (ComponentAdapter)mock.proxy();
+    }
+    
+    private ComponentAdapter mockComponentAdapterSupportingLifecycleStrategy(
+            boolean start, boolean stop, boolean dispose, boolean getInstance) {
+        Mock mock = mock(ComponentAdapterSupportingLifecycleStrategy.class);
+        if ( start ){
+            mock.expects(once()).method("start").with(isA(Touchable.class));
+        }
+        if ( stop ) {
+            mock.expects(once()).method("stop").with(isA(Touchable.class));
+        }
+        if ( dispose ) {
+            mock.expects(once()).method("dispose").with(isA(Touchable.class));
+        }
+        if ( getInstance ) {
+            mock.expects(once()).method("getComponentInstance").with(
+                    isA(PicoContainer.class)).will(
+                    returnValue(new SimpleTouchable()));
+        }
+        return (ComponentAdapter) mock.proxy();
+    }
+    
+    static interface ComponentAdapterSupportingLifecycleStrategy extends
+            ComponentAdapter, LifecycleStrategy {
+    }
+}

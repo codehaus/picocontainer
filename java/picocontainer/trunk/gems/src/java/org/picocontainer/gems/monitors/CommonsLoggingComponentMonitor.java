bc7b02cdbad197208cd49c6c9131dd23e6283385
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-11 06:28:19 +0000

    thread safe

--- java/picocontainer/trunk/gems/src/java/org/picocontainer/gems/monitors/CommonsLoggingComponentMonitor.java
+++ java/picocontainer/trunk/gems/src/java/org/picocontainer/gems/monitors/CommonsLoggingComponentMonitor.java
@@ -68,42 +68,42 @@ public class CommonsLoggingComponentMonitor extends AbstractComponentMonitor imp
     }
 
     public void instantiating(Constructor constructor) {
-        log = getLog(constructor);
+        Log log = getLog(constructor);
         if (log.isDebugEnabled()) {
             log.debug(format(INSTANTIATING, new Object[]{constructor}));
         }
     }
 
     public void instantiated(Constructor constructor, long duration) {
-        log = getLog(constructor);
+        Log log = getLog(constructor);
         if (log.isDebugEnabled()) {
             log.debug(format(INSTANTIATED, new Object[]{constructor, new Long(duration)}));
         }
     }
 
     public void instantiationFailed(Constructor constructor, Exception e) {
-        log = getLog(constructor);
+        Log log = getLog(constructor);
         if (log.isWarnEnabled()) {
             log.warn(format(INSTANTIATION_FAILED, new Object[]{constructor, e.getMessage()}),e);
         }
     }
 
     public void invoking(Method method, Object instance) {
-        log = getLog(method);
+        Log log = getLog(method);
         if (log.isDebugEnabled()) {
             log.debug(format(INVOKING, new Object[]{method, instance}));
         }
     }
 
     public void invoked(Method method, Object instance, long duration) {
-        log = getLog(method);
+        Log log = getLog(method);
         if (log.isDebugEnabled()) {
             log.debug(format(INVOKED, new Object[]{method, instance, new Long(duration)}));
         }
     }
 
     public void invocationFailed(Method method, Object instance, Exception e) {
-        log = getLog(method);
+        Log log = getLog(method);
         if (log.isWarnEnabled()) {
             log.warn(format(INVOCATION_FAILED, new Object[]{method, instance, e.getMessage()}),e);
         }

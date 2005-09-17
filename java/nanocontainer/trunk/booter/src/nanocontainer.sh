ffd4d7a6c94eb231f58073a6b34dcbf9f22544fa
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-17 20:27:35 +0000

    version number back in files.

--- java/nanocontainer/trunk/booter/src/nanocontainer.sh
+++ java/nanocontainer/trunk/booter/src/nanocontainer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-EXEC="$JAVA_HOME/bin/java -Djava.security.manager -Djava.security.policy=file:lib/booter.policy -jar lib/nanocontainer-booter.jar $@"
+# NanoContainer Booter script v @VER@
+# www.nanocontainer.org
+
+EXEC="$JAVA_HOME/bin/java -Djava.security.manager -Djava.security.policy=file:lib/booter.policy -jar lib/nanocontainer-booter-@VER@.jar $@"
 echo $EXEC
 $EXEC
 

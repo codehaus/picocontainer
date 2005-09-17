f700119543f51380eeb615d9f0b3d2f3bfc40b16
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-17 19:13:12 +0000

    remove debugging

--- java/nanocontainer/trunk/booter/src/nanocontainer.sh
+++ java/nanocontainer/trunk/booter/src/nanocontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXEC="$JAVA_HOME/bin/java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Djava.security.manager -Djava.security.policy=file:lib/booter.policy -jar lib/nanocontainer-booter.jar $@"
+EXEC="$JAVA_HOME/bin/java -Djava.security.manager -Djava.security.policy=file:lib/booter.policy -jar lib/nanocontainer-booter.jar $@"
 echo $EXEC
 $EXEC
 

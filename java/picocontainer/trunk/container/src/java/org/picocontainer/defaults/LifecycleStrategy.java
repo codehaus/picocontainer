4a57db503191e57fb452c4694f5286f18e187517
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-23 03:36:02 +0000

    'public' redundant

--- java/picocontainer/trunk/container/src/java/org/picocontainer/defaults/LifecycleStrategy.java
+++ java/picocontainer/trunk/container/src/java/org/picocontainer/defaults/LifecycleStrategy.java
@@ -26,20 +26,20 @@ public interface LifecycleStrategy {
      * 
      * @param component the instance of the component to start
      */
-    public void start(Object component);
+    void start(Object component);
     
     /**
      * Invoke the "stop" method on the component instance if this is Startable
      * 
      * @param component the instance of the component to stop
      */
-    public void stop(Object component);
+    void stop(Object component);
 
     /**
      * Invoke the "dispose" method on the component instance if this is Disposable
      * 
      * @param component the instance of the component to dispose
      */
-    public void dispose(Object component);
+    void dispose(Object component);
 
 }

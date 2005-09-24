726578ae1ccbe20cccbe23be0a51e125672e5e66
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-09-24 09:52:58 +0000

    Added removed mocker as deprecated for backward compat with RC-1. Should be removed before final release.

---
+++ java/nanocontainer-nanowar/trunk/nanowar/src/java/org/nanocontainer/nanowar/TestContainerMocker.java
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by                                                          *
+ *****************************************************************************/
+package org.nanocontainer.nanowar;
+
+/**
+ * @deprecated Use ContainerComposerMocker
+ */
+public class TestContainerMocker extends ContainerComposerMocker {
+
+    public TestContainerMocker(Class containerComposerClass) {
+        super(containerComposerClass);
+    }
+
+}

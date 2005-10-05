9db19329eb5f833bd2cbd71d1c76c9ce0a242681
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-10-05 12:11:17 +0000

    Added javadocs. Fixed license.

--- java/nanocontainer/trunk/container/src/java/org/nanocontainer/integrationkit/ContainerPopulator.java
+++ java/nanocontainer/trunk/container/src/java/org/nanocontainer/integrationkit/ContainerPopulator.java
@@ -1,19 +1,33 @@
-/*
- * Copyright 2004 by Lastminute.com, plc. 4 Buckingham Gate, London SW1E 6JP,
- * United Kingdom. All rights reserved.
- * 
- * This software is the confidential and proprietary information of lastminute.com plc
- * ("Confidential Information"). You shall not disclose such confidential information
- * and shall use it only in accordance with the terms of the license agreements you 
- * entered into with lastminute.com
- */
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
 package org.nanocontainer.integrationkit;
 
 import org.picocontainer.MutablePicoContainer;
 
 /**
+ * <p>
+ * Interface for populating a container.  The concern here is only on the
+ * container to populate and not on the composition or the building.
+ * </p>
+ * <p>
+ * An example of its use can be found in <a href="http://nanocontainer.org/nanowar">NanoWar</a>
+ * for container population from multiple web scopes.
+ * </p>
+ * 
  * @author Mauro Talevi
  */
 public interface ContainerPopulator {
+    
+    /**
+     * Populate a container 
+     * @param container the MutablePicoContainer to populate
+     */
     public void populateContainer(MutablePicoContainer container);
+
 }

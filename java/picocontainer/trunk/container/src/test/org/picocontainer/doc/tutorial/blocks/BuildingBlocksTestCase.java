3934c6a5f01c402e49bb84b12abd10ebcf681e38
joehni <joehni@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-10-12 20:49:53 +0000

    Tutorial work.

--- java/picocontainer/trunk/container/src/test/org/picocontainer/doc/tutorial/blocks/BuildingBlocksTestCase.java
+++ java/picocontainer/trunk/container/src/test/org/picocontainer/doc/tutorial/blocks/BuildingBlocksTestCase.java
@@ -4,15 +4,22 @@ import junit.framework.TestCase;
 
 import org.picocontainer.MutablePicoContainer;
 import org.picocontainer.defaults.CachingComponentAdapter;
+import org.picocontainer.defaults.CachingComponentAdapterFactory;
 import org.picocontainer.defaults.ConstructorInjectionComponentAdapter;
 import org.picocontainer.defaults.DefaultPicoContainer;
 import org.picocontainer.defaults.InstanceComponentAdapter;
+import org.picocontainer.defaults.SetterInjectionComponentAdapter;
+import org.picocontainer.defaults.SetterInjectionComponentAdapterFactory;
+import org.picocontainer.defaults.SynchronizedComponentAdapter;
+import org.picocontainer.defaults.SynchronizedComponentAdapterFactory;
 import org.picocontainer.doc.introduction.Apple;
 import org.picocontainer.doc.introduction.Juicer;
 import org.picocontainer.doc.introduction.Peeler;
 
+
 /**
  * Test case for the snippets used in "Component Adapters and Factories"
+ * 
  * @author J&ouml;rg Schaible
  */
 public class BuildingBlocksTestCase extends TestCase {
@@ -27,21 +34,39 @@ public class BuildingBlocksTestCase extends TestCase {
         picoContainer.registerComponent(new InstanceComponentAdapter("Another Apple", new Apple()));
         // END SNIPPET: register-direct
     }
-    
+
     public void testRegisterEquivalentConvenient() {
         MutablePicoContainer picoContainer = new DefaultPicoContainer();
         // START SNIPPET: register-equivalent-convenient
         picoContainer.registerComponentImplementation(Juicer.class);
         // END SNIPPET: register-equivalent-convenient
     }
-    
+
     public void testRegisterEquivalentAtLength() {
         MutablePicoContainer picoContainer = new DefaultPicoContainer();
         // START SNIPPET: register-equivalent-at-length
         picoContainer.registerComponent(
                 new CachingComponentAdapter(
-                        new ConstructorInjectionComponentAdapter(
-                                Juicer.class, Juicer.class, null)));
+                        new ConstructorInjectionComponentAdapter(Juicer.class, Juicer.class, null)));
         // END SNIPPET: register-equivalent-at-length
+    }
+
+    public void testRegisterDifferentComponentAdapterFactory() {
+        // START SNIPPET: register-different-caf
+        MutablePicoContainer picoContainer = new DefaultPicoContainer(
+                new SynchronizedComponentAdapterFactory(new CachingComponentAdapterFactory(
+                        new SetterInjectionComponentAdapterFactory())));
+        // END SNIPPET: register-different-caf
+    }
+
+    public void testRegisterEquivalentAtLength2() {
+        MutablePicoContainer picoContainer = new DefaultPicoContainer();
+        // START SNIPPET: register-equivalent-at-length2
+        picoContainer.registerComponent(
+                new SynchronizedComponentAdapter(
+                        new CachingComponentAdapter(
+                                new SetterInjectionComponentAdapter(
+                                        JuicerBean.class, JuicerBean.class, null))));
+        // END SNIPPET: register-equivalent-at-length2
     }
 }

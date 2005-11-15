e49ce32a0bd4aba0f2ad6b2e243d2c584cd0e88f
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-11-15 04:46:27 +0000

    offline and online master build

--- java/build-trunk-offline.sh
+++ java/build-trunk-offline.sh
@@ -5,37 +5,37 @@ maven multiproject:install
 cd -
 echo ** NanoContainer
 cd nanocontainer/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** NanoContainer - NanoWar
 cd nanocontainer-nanowar/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** NanoContainer - Persistence
 cd nanocontainer-persistence/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** NanoContainer - Remoting
 cd nanocontainer-remoting/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** NanoContainer - IoC
 cd nanocontainer-ioc/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** NanoContainer - Tools
 cd nanocontainer-tools/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** NanoContainer - Sandbox
 cd nanocontainer-sandbox/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** MicroContainer
 cd microcontainer/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -
 echo ** Demos
 cd demos/trunk
-maven multiproject:install
+maven -o multiproject:install
 cd -

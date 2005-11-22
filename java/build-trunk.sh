26622d3cc56bdc4564e1d6aa6ec0f8cd78ec1eef
gjoseph <gjoseph@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2005-11-22 16:55:45 +0000

    echo * was printing the directory contents, now escaping

--- java/build-trunk.sh
+++ java/build-trunk.sh
@@ -1,41 +1,41 @@
-echo ** Building PicoContainer, NanoContainer and Microcontainer trunks
-echo ** PicoContainer
+echo \*\* Building PicoContainer, NanoContainer and Microcontainer trunks
+echo \*\* PicoContainer
 cd picocontainer/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer
+echo \*\* NanoContainer
 cd nanocontainer/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer - NanoWar
+echo \*\* NanoContainer - NanoWar
 cd nanocontainer-nanowar/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer - Persistence
+echo \*\* NanoContainer - Persistence
 cd nanocontainer-persistence/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer - Remoting
+echo \*\* NanoContainer - Remoting
 cd nanocontainer-remoting/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer - IoC
+echo \*\* NanoContainer - IoC
 cd nanocontainer-ioc/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer - Tools
+echo \*\* NanoContainer - Tools
 cd nanocontainer-tools/trunk
 maven multiproject:install
 cd -
-echo ** NanoContainer - Sandbox
+echo \*\* NanoContainer - Sandbox
 cd nanocontainer-sandbox/trunk
 maven multiproject:install
 cd -
-echo ** MicroContainer
+echo \*\* MicroContainer
 cd microcontainer/trunk
 maven multiproject:install
 cd -
-echo ** Demos
+echo \*\* Demos
 cd demos/trunk
 maven multiproject:install
 cd -

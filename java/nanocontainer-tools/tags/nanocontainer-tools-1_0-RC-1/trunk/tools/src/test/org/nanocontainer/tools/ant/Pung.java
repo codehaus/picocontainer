060b107c1eb79db3ddd3724f802f34625ef99cbd
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2006-01-17 23:12:48 +0000

    Created tag java/nanocontainer-tools/tags/nanocontainer-tools-1_0

---
+++ java/nanocontainer-tools/tags/nanocontainer-tools-1_0-RC-1/trunk/tools/src/test/org/nanocontainer/tools/ant/Pung.java
@@ -0,0 +1,25 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by                                                          *
+ *****************************************************************************/
+package org.nanocontainer.tools.ant;
+
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public class Pung {
+    public String text;
+    public Pang pang;
+
+    public Pung(Pang pang, String text) {
+        this.pang = pang;
+        this.text = text;
+    }
+}

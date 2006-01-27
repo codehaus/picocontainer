e3fb8bff9ff32a89de957511299eeaece0b226fd
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2006-01-27 14:06:39 +0000

    another go at the doc

--- site/trunk/site/docs/merge.sh
+++ site/trunk/site/docs/merge.sh
@@ -2,5 +2,5 @@
 
 rm two.pdf
 rm PicoContainerDocumentation-1.2.pdf
-pdfmeld -pages 4-300 downloaded.pdf two.pdf
+pdfmeld -pages 3-300 downloaded.pdf two.pdf
 pdfmeld  front-page.pdf,two.pdf PicoContainerDocumentation-1.2.pdf

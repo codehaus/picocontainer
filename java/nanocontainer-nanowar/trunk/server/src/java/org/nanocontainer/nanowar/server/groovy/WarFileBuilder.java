9d14036eca77abd5e5f954454bccba43b2985911
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2006-08-07 03:38:16 +0000

    moved to main container

--- java/nanocontainer-nanowar/trunk/server/src/java/org/nanocontainer/nanowar/server/groovy/WarFileBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.nanocontainer.nanowar.server.groovy;
-
-import groovy.util.NodeBuilder;
-import org.nanocontainer.nanowar.server.PicoWebAppContext;
-
-public class WarFileBuilder extends NodeBuilder {
-    public WarFileBuilder(PicoWebAppContext context) {
-    }
-}

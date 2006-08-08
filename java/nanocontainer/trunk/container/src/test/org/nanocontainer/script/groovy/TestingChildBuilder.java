b4831ae70b8822ea0670d44fd821d6738aa0aef1
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2006-08-08 02:35:17 +0000

    flip to NodeBuilder design

--- java/nanocontainer/trunk/container/src/test/org/nanocontainer/script/groovy/TestingChildBuilder.java
+++ java/nanocontainer/trunk/container/src/test/org/nanocontainer/script/groovy/TestingChildBuilder.java
@@ -10,39 +10,21 @@
 
 package org.nanocontainer.script.groovy;
 
-import groovy.util.BuilderSupport;
+import groovy.util.NodeBuilder;
 import org.picocontainer.MutablePicoContainer;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author Paul Hammant
  * @version $Revision$
  */
-public class TestingChildBuilder extends BuilderSupport {
+public class TestingChildBuilder extends NodeBuilder {
 
     MutablePicoContainer toOperateOn;
 
     public TestingChildBuilder(MutablePicoContainer toOperateOn) {
         this.toOperateOn = toOperateOn;
-    }
-
-    protected void setParent(Object o, Object o1) {
-    }
-
-    protected Object createNode(Object name) {
-        return createNode(name, Collections.EMPTY_MAP);
-    }
-
-    protected Object createNode(Object name, Object value) {
-        if (value instanceof Class) {
-            Map attributes = new HashMap();
-            attributes.put("class", value);
-            return createNode(name, attributes);
-        }
-        return createNode(name);
     }
 
     protected Object createNode(Object name, Map map) {
@@ -53,7 +35,4 @@ public class TestingChildBuilder extends BuilderSupport {
         }
     }
 
-    protected Object createNode(Object o, Map map, Object o1) {
-        return new Object();
-    }
 }

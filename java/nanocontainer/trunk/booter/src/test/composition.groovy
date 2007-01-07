d1071f51daae8ab2bd64a34d9d7b67bce267930c
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-01-07 19:04:01 +0000

    Fixed booter integration test.

--- java/nanocontainer/trunk/booter/src/test/composition.groovy
+++ java/nanocontainer/trunk/booter/src/test/composition.groovy
@@ -1,13 +1,13 @@
 import org.nanocontainer.reflection.ImplementationHidingNanoPicoContainer
 
 pico = builder.container(parent:parent, class:ImplementationHidingNanoPicoContainer) {
-    classPathElement(path:"lib/hidden/api.jar")
+    classPathElement(path:"test-comps/api.jar")
     classLoader {
-        classPathElement(path:"lib/hidden/honeyimpl.jar")
+        classPathElement(path:"test-comps/honeyimpl.jar")
         component(classNameKey:"org.nanocontainer.booter.Honey", class:"org.nanocontainer.booter.BeeHiveHoney")
     }
     classLoader {
-        classPathElement(path:"lib/hidden/bearimpl.jar") {
+        classPathElement(path:"test-comps/bearimpl.jar") {
              grant(new java.net.SocketPermission("yahoo.com:80", "connect"))
         }
         component(class:"org.nanocontainer.booter.BrownBear")

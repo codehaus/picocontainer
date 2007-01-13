bf20b3ed86c69844bbc252480dbbcf8e17b1265d
mauro <mauro@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-01-13 13:06:06 +0000

    Created tags/nanocontainer-nanowar-1_1-RC-1

---
+++ java/nanocontainer/tags/nanocontainer-nanowar-1_1-RC-1/nanowar-nanoweb/src/test/test.groovy
@@ -0,0 +1,18 @@
+package org.nanocontainer.dynaweb;
+
+/**
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public class MyGroovyAction {
+
+    int year
+
+    public doit() {
+        if(year > 2003) {
+            return "success";
+        } else {
+            return "error";
+        }
+    }
+}
---
+++ java/nanocontainer/tags/nanocontainer-nanowar-1_1-RC-1/nanowar/src/java/org/nanocontainer/nanowar/RequestScopeObjectReference.java
@@ -0,0 +1,39 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.nanocontainer.nanowar;
+
+import org.picocontainer.defaults.ObjectReference;
+
+import javax.servlet.ServletRequest;
+
+/**
+ * References an object that lives as an attribute of the
+ * ServletRequest.
+ *
+ * @author <a href="mailto:joe@thoughtworks.net">Joe Walnes</a>
+ */
+public class RequestScopeObjectReference implements ObjectReference {
+
+    private ServletRequest request;
+    private String key;
+
+    public RequestScopeObjectReference(ServletRequest request, String key) {
+        this.request = request;
+        this.key = key;
+    }
+
+    public void set(Object item) {
+        request.setAttribute(key, item);
+    }
+
+    public Object get() {
+        return request.getAttribute(key);
+    }
+
+}

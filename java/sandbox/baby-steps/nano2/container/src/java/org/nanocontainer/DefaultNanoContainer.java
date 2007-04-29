4321024ee41119a47995eb51cda21fa4fc98250e
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-04-29 22:59:28 +0000

    missing file

---
+++ java/sandbox/baby-steps/nano2/container/src/java/org/nanocontainer/DefaultNanoContainer.java
@@ -0,0 +1,113 @@
+/*****************************************************************************
+ * Copyright (C) NanoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by Paul Hammant                                             *
+ *****************************************************************************/
+
+package org.nanocontainer;
+
+import java.io.Serializable;
+import org.picocontainer.ComponentMonitor;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.defaults.ComponentAdapterFactory;
+import org.picocontainer.defaults.ComponentMonitorStrategy;
+import org.picocontainer.componentadapters.CachingAndConstructorComponentAdapterFactory;
+import org.picocontainer.defaults.DefaultPicoContainer;
+import org.picocontainer.defaults.LifecycleStrategy;
+
+/**
+ * This is a MutablePicoContainer that also supports soft composition. i.e. assembly by class name rather that class
+ * reference.
+ * <p>
+ * In terms of implementation it adopts the behaviour of DefaultPicoContainer and DefaultNanoContainer.</p>
+ *
+ * @author Paul Hammant
+ * @author Mauro Talevi
+ * @author Michael Rimov
+ * @version $Revision$
+ */
+public class DefaultNanoContainer extends AbstractNanoContainer implements NanoPicoContainer, Serializable,
+    ComponentMonitorStrategy {
+
+    public DefaultNanoContainer(ClassLoader classLoader, ComponentAdapterFactory caf, PicoContainer parent) {
+        super(new DefaultPicoContainer(caf, parent), classLoader);
+    }
+
+    public DefaultNanoContainer(ClassLoader classLoader, PicoContainer parent) {
+        super(new DefaultPicoContainer(new CachingAndConstructorComponentAdapterFactory(), parent), classLoader);
+    }
+
+    public DefaultNanoContainer(ClassLoader classLoader, PicoContainer parent, ComponentMonitor componentMonitor) {
+        super(new DefaultPicoContainer(new CachingAndConstructorComponentAdapterFactory(componentMonitor), parent), classLoader);
+    }
+
+    public DefaultNanoContainer(ComponentAdapterFactory caf) {
+        super(new DefaultPicoContainer(caf, null), DefaultNanoContainer.class.getClassLoader());
+    }
+
+    public DefaultNanoContainer(PicoContainer pc) {
+        super(new DefaultPicoContainer(pc), DefaultNanoContainer.class.getClassLoader());
+    }
+
+    public DefaultNanoContainer(ClassLoader classLoader) {
+        super(new DefaultPicoContainer(), classLoader);
+    }
+
+    public DefaultNanoContainer() {
+        super(new DefaultPicoContainer(), DefaultNanoContainer.class.getClassLoader());
+    }
+
+
+    /**
+     * Constructor that provides the same control over the nanocontainer lifecycle strategies
+     * as {@link DefaultPicoContainer(ComponentAdapterFactory, LifecycleStrategy, PicoContainer)}.
+     * @param componentAdapterFactory ComponentAdapterFactory
+     * @param lifecycleStrategyForInstanceRegistrations LifecycleStrategy
+     * @param parent PicoContainer may be null if there is no parent.
+     * @param cl the Classloader to use.  May be null, in which case DefaultNanoPicoContainer.class.getClassLoader()
+     * will be called instead.
+     */
+    public DefaultNanoContainer(ComponentAdapterFactory componentAdapterFactory,
+        LifecycleStrategy lifecycleStrategyForInstanceRegistrations, PicoContainer parent, ClassLoader cl) {
+
+        super(new DefaultPicoContainer(componentAdapterFactory,
+            lifecycleStrategyForInstanceRegistrations, parent),
+            //Use a default classloader if none is specified.
+            (cl != null) ? cl : DefaultNanoContainer.class.getClassLoader());
+    }
+
+    /**
+     * Copy Constructor.  Makes a new DefaultNanoPicoContainer with the same
+     * attributes  ClassLoader, child PicoContainer type, ComponentAdapterFactory
+     * as the parent.
+     * <p><tt>Note:</tt> This constructor is protected because are existing scripts
+     * that call <tt>new DefaultNanoPicoContainer(PicoContainer)</tt>, and they get this
+     * constructor instead (which has different behavior).</p>
+     * @param parent  The object to copy.
+     */
+    protected DefaultNanoContainer(final DefaultNanoContainer parent) {
+        super(parent.getDelegate().makeChildContainer(),  parent.getComponentClassLoader());
+        MutablePicoContainer parentDelegate = parent.getDelegate();
+        parentDelegate.removeChildContainer(getDelegate());
+        parentDelegate.addChildContainer(this);
+    }
+
+
+    protected AbstractNanoContainer createChildContainer() {
+        return new DefaultNanoContainer(this);
+     }
+
+    public void changeMonitor(ComponentMonitor monitor) {
+        ((ComponentMonitorStrategy)getDelegate()).changeMonitor(monitor);
+    }
+
+    public ComponentMonitor currentMonitor() {
+        return ((ComponentMonitorStrategy)getDelegate()).currentMonitor();
+    }
+
+}

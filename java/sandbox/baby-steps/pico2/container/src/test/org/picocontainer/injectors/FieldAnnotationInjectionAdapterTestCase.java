98dbb33a36b3999cd8e4701e067193820d61bc9c
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-06-12 05:48:33 +0000

    edge cases

--- java/sandbox/baby-steps/pico2/container/src/test/org/picocontainer/injectors/FieldAnnotationInjectionAdapterTestCase.java
+++ java/sandbox/baby-steps/pico2/container/src/test/org/picocontainer/injectors/FieldAnnotationInjectionAdapterTestCase.java
@@ -33,4 +33,17 @@ public class FieldAnnotationInjectionAdapterTestCase extends TestCase {
         assertNotNull(chopper.pogo);
     }
 
+    public void testFieldDeosNotHappenWithoutRightInjector() {
+        MutablePicoContainer pico = new DefaultPicoContainer();
+        pico.addAdapter(new SetterInjector(Helicopter.class, Helicopter.class, null,
+                                                    new ConsoleComponentMonitor(), NullLifecycleStrategy.getInstance()));
+        pico.addComponent(PogoStick.class, new PogoStick());
+        Helicopter chopper = pico.getComponent(Helicopter.class);
+        assertNotNull(chopper);
+        assertNull(chopper.pogo);
+    }
+
+
+
+
 }

aaec02aac5b1c55217f78c2eb30044f5b3c8d5bc
ko5tik <ko5tik@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2007-11-20 08:34:38 +0000

    tandem charge implementation introduced

---
+++ java/sandbox/parameter-refactoring/pico/container/src/java/org/picocontainer/parameters/Lookup.java
@@ -0,0 +1,34 @@
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Original code by                                                          *
+ *****************************************************************************/
+package org.picocontainer.parameters;
+
+import java.util.Collection;
+
+import org.picocontainer.ComponentAdapter;
+import org.picocontainer.PicoContainer;
+
+/**
+ * lookup is responsible for retrieval of collection of
+ * component adapters out of container. this collection will be 
+ * later processed by convert. 
+ * 
+ * @author Konstantin Pribluda
+ *
+ */
+public interface Lookup {
+	
+	/**
+	 * retrieve collection of component adapters 
+	 * from container. 
+	 * @param container
+	 * @return
+	 */
+	Collection<ComponentAdapter<?>> lookup(PicoContainer container);
+}

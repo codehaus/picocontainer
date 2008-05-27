e34070f5e2b756c09359eb3ace4e3cd1414b8406
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2008-05-27 15:08:00 +0000

    work on struts2 example

--- java/sandbox/picocontainer-web/examples/struts2-webapp/src/java/org/nanocontainer/nanowar/sample/struts2/ExampleWebWork2PicoServletContainerListener.java
+++ java/sandbox/picocontainer-web/examples/struts2-webapp/src/java/org/nanocontainer/nanowar/sample/struts2/ExampleWebWork2PicoServletContainerListener.java
@@ -1,7 +1,7 @@
-package org.nanocontainer.nanowar.sample.webwork2;
+package org.nanocontainer.nanowar.sample.struts2;
 
 import org.nanocontainer.nanowar.PicoServletContainerListener;
-import org.nanocontainer.nanowar.webwork2.WebWork2PicoServletContainerListener;
+import org.nanocontainer.nanowar.struts2.WebWork2PicoServletContainerListener;
 import org.nanocontainer.nanowar.sample.dao.simple.MemoryCheeseDao;
 import org.nanocontainer.nanowar.sample.dao.CheeseDao;
 import org.nanocontainer.nanowar.sample.service.defaults.DefaultCheeseService;

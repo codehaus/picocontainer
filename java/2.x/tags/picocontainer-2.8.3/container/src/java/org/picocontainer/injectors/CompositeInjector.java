d1248f3443ca11eb793f8b7885a64ecb949eb7f4
paul <paul@ac66bb80-72f5-0310-8d68-9f556cfffb23>
2009-06-18 17:22:42 +0000

    [maven-release-plugin]  copy for tag picocontainer-2.8.3

---
+++ java/2.x/tags/picocontainer-2.8.3/container/src/java/org/picocontainer/injectors/CompositeInjector.java
@@ -0,0 +1,80 @@
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ *****************************************************************************/
+package org.picocontainer.injectors;
+
+import java.lang.reflect.Type;
+
+import org.picocontainer.ComponentMonitor;
+import org.picocontainer.Injector;
+import org.picocontainer.LifecycleStrategy;
+import org.picocontainer.Parameter;
+import org.picocontainer.PicoCompositionException;
+import org.picocontainer.PicoContainer;
+import org.picocontainer.PicoVisitor;
+import org.picocontainer.ComponentAdapter;
+
+@SuppressWarnings("serial")
+public class CompositeInjector<T> extends AbstractInjector<T> {
+
+    private final Injector<T>[] injectors;
+
+    public CompositeInjector(Object componentKey, Class<?> componentImplementation, Parameter[] parameters, ComponentMonitor monitor, LifecycleStrategy lifecycleStrategy,
+                             boolean useNames, Injector... injectors) {
+        super(componentKey, componentImplementation, parameters, monitor, lifecycleStrategy, useNames);
+        this.injectors = injectors;
+    }
+
+
+    public T getComponentInstance(PicoContainer container) throws PicoCompositionException {
+        return getComponentInstance(container, NOTHING.class);
+    }
+
+    public T getComponentInstance(PicoContainer container, Type into) throws PicoCompositionException {
+        T instance = null;
+        for (int i = 0; i < injectors.length; i++) {
+            Injector<T> injector = injectors[i];
+            if (instance == null) {
+                instance = injector.getComponentInstance(container, ComponentAdapter.NOTHING.class);
+            } else {
+                injector.decorateComponentInstance(container, into, instance);
+            }
+        }
+        return (T) instance;
+    }
+
+
+    /**
+     * @return the object returned is the result of the last of the injectors delegated to
+     */
+    @Override
+    public Object decorateComponentInstance(PicoContainer container, Type into, T instance) {
+        Object result = null;
+        for (int i = 0; i < injectors.length; i++) {
+            result = injectors[i].decorateComponentInstance(container, into, instance);
+        }        
+        return result;
+    }
+
+    public void verify(PicoContainer container) throws PicoCompositionException {
+        for (int i = 0; i < injectors.length; i++) {
+            injectors[i].verify(container);
+        }
+    }
+
+    public final void accept(PicoVisitor visitor) {
+        super.accept(visitor);
+        for (int i = 0; i < injectors.length; i++) {
+            injectors[i].accept(visitor);
+        }
+    }
+
+    public String getDescriptor() {
+        return "CompositeInjector";
+    }
+}
---
+++ java/2.x/tags/picocontainer-2.8.3/container/src/test/org/picocontainer/injectors/ConstructorInjectionTestCase.java
@@ -0,0 +1,112 @@
+/*****************************************************************************
+ * Copyright (C) PicoContainer Organization. All rights reserved.            *
+ * ------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the BSD      *
+ * style license a copy of which has been included with this distribution in *
+ * the LICENSE.txt file.                                                     *
+ *                                                                           *
+ * Idea by Rachel Davies, Original code by Aslak Hellesoy and Paul Hammant   *
+ *****************************************************************************/
+package org.picocontainer.injectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.picocontainer.Characteristics.USE_NAMES;
+
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.picocontainer.ComponentFactory;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.monitors.NullComponentMonitor;
+import org.picocontainer.parameters.ConstantParameter;
+import org.picocontainer.tck.AbstractComponentFactoryTest;
+import org.picocontainer.tck.AbstractComponentAdapterTest.RecordingLifecycleStrategy;
+import org.picocontainer.testmodel.NullLifecycle;
+import org.picocontainer.testmodel.RecordingLifecycle;
+import org.picocontainer.testmodel.RecordingLifecycle.One;
+
+/**
+ * @author Mauro Talevi
+ */
+public class ConstructorInjectionTestCase extends AbstractComponentFactoryTest {
+
+	@Before
+    public void setUp() throws Exception {
+        picoContainer = new DefaultPicoContainer(createComponentFactory());
+    }
+
+    protected ComponentFactory createComponentFactory() {
+        return new ConstructorInjection();
+    }
+
+    @Test public void testCustomLifecycleCanBeInjected() throws NoSuchMethodException {
+        RecordingLifecycleStrategy strategy = new RecordingLifecycleStrategy(new StringBuffer());
+        ConstructorInjection componentFactory =
+            new ConstructorInjection();
+        ConstructorInjector cica =  (ConstructorInjector)
+        componentFactory.createComponentAdapter(new NullComponentMonitor(), strategy, new Properties(), NullLifecycle.class, NullLifecycle.class);
+        One one = new RecordingLifecycle.One(new StringBuffer());
+        cica.start(one);
+        cica.stop(one);        
+        cica.dispose(one);
+        assertEquals("<start<stop<dispose", strategy.recording());
+    }
+
+    public static class ClassA {
+        private int x;
+        public ClassA(int x) {
+            this.x = x;
+        }
+    }
+    @Test public void testAutoConversionOfIntegerParam() {
+        picoContainer.as(USE_NAMES).addComponent(ClassA.class);
+        picoContainer.addComponent("x", "12");
+        assertNotNull(picoContainer.getComponent(ClassA.class));
+        assertEquals(12,picoContainer.getComponent(ClassA.class).x);
+    }
+
+    public static class ClassB {
+        private float x;
+        public ClassB(float x) {
+            this.x = x;
+        }
+    }
+    @Test public void testAutoConversionOfFloatParam() {
+        picoContainer.as(USE_NAMES).addComponent(ClassB.class);
+        picoContainer.addComponent("x", "1.2");
+        assertNotNull(picoContainer.getComponent(ClassB.class));
+        assertEquals(1.2,picoContainer.getComponent(ClassB.class).x, 0.0001);
+    }
+    
+    
+    /**
+     * Test class to verify the CICA can handle
+     * a constant parameter class type. 
+     *
+     */
+    @SuppressWarnings({"unchecked", "unused"})
+    public static class ClassAsConstructor {
+		private final Class type;
+
+		public ClassAsConstructor(Class type) {
+			this.type = type;    		
+    	}
+    }
+    
+	@Test 
+    @SuppressWarnings("unchecked")
+    public void allowClassTypesForComponentAdapter() {
+        ConstructorInjection componentFactory = new ConstructorInjection();
+        
+        ConstructorInjector cica =  (ConstructorInjector)
+        componentFactory.createComponentAdapter(new NullComponentMonitor(), null, new Properties(), ClassAsConstructor.class, ClassAsConstructor.class, new ConstantParameter(String.class));
+        
+        ClassAsConstructor instance = (ClassAsConstructor) cica.getComponentInstance(picoContainer);
+        assertNotNull(instance);
+    	
+    }
+
+
+}
